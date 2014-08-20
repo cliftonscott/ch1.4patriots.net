dacaed37e30173bf099686f8a78379c65b5aded5
Brian Gibbins <brian@briangibbins.com>
2014-08-20 15:58:18 -0400

    FFP-152 update product file w/ vals for food

--- libraries/Product.php
+++ libraries/Product.php
@@ -30,169 +30,82 @@ class Product {
 		$productObj->productId = intval($productId);
 		switch($productObj->productId) {
 			
-			case 162: //main product - Patriot Power Generator
-				$productObj->pmaSku = null;
-				$productObj->price = 1997;
-				$productObj->originalPrice = 1997;
-				$productObj->shippingIdDomestic = 26;
-				$productObj->shippingIdInternational = null; //no international sales
-				$productObj->shippingCostDomestic = 0;
-				$productObj->shippingCostInternational = 0; //no international sales
-				$productObj->mpsId = null; //used for MPS
-				$productObj->campaignId = 16;
-				$productObj->netRevenueEach = 615;
-				$productObj->taxable = TRUE;
-				$productObj->listId = 55;
-				$productObj->tags = "ppg1500";
-				$productObj->googleProductName = "Patriot Power Generator 1500" . self::VSL_VERSION;
-				$productObj->metaTitle = "Patriot Power Generator 1500";
-				$productObj->metaDescription = "Patriot Power Generator 1500 - Plus Bonuses";
-				$productObj->defaultQuantity = 1;
-				$productObj->hasBonuses = TRUE; //set this to trigger any bonuses for this id
-				$productObj->bonusIds = array (170); //set this as a single integer in an array (1) or a string of integers (123,456)
-				$productObj->nextPage = "/checkout/oto/ppg-platinum.php";
-				break;
-			case 170: //PPG Bonuses
-				$productObj->pmaSku = null;
-				$productObj->price = 0;
-				$productObj->originalPrice = 0;
-				$productObj->shippingIdDomestic = 7;
-				$productObj->shippingIdInternational = null; //no international sales
-				$productObj->shippingCostDomestic = 0;
-				$productObj->shippingCostInternational = 0; //no international sales
-				$productObj->mpsId = null; //used for MPS
-				$productObj->campaignId = 18;
-				$productObj->netRevenueEach = 0;
-				$productObj->taxable = TRUE;
-				$productObj->listId = null;
-				$productObj->tags = null;
-				$productObj->googleProductName = "PPG Bonuses " . self::VSL_VERSION;
-				$productObj->metaTitle = "Patriot Power Generator 1500 - Bonuses";
-				$productObj->metaDescription = "Patriot Power Generator 1500 - Bonuses";
-				$productObj->defaultQuantity = 1;
-				$productObj->isBonus = TRUE;
-				$productObj->nextPage = null; //bonuses don't use a next page
-				break;
-			case 164: //Patriot Power Generator Deluxe Upgrade
-				$productObj->pmaSku = null;
-				$productObj->price = 997;
-				$productObj->originalPrice = 997;
-				$productObj->shippingIdDomestic = 26;
-				$productObj->shippingIdInternational = null; //no international sales
-				$productObj->shippingCostDomestic = 0;
-				$productObj->shippingCostInternational = 0; //no international sales
-				$productObj->mpsId = null; //used for MPS
-				$productObj->campaignId = 16;
-				$productObj->netRevenueEach = 546;
-				$productObj->taxable = TRUE;
-				$productObj->listId = 56;
-				$productObj->tags = "ppgDeluxe";
-				$productObj->googleProductName = "PPG Platinum Upgrade " . self::VSL_VERSION;
-				$productObj->metaTitle = "PPG Platinum Upgrade";
-				$productObj->metaDescription = "Patriot Power Generator Platinum Upgrade";
-				$productObj->defaultQuantity = 1;
-				$productObj->nextPage = "/checkout/oto/ppg-1year.php";
-				break;
-			case 166: //PPG Deluxe Upgrade - 2 Easy Payment Plan
+			case 19: //main product - 3 Month Kit
 				$productObj->pmaSku = null;
 				$productObj->price = 497;
 				$productObj->originalPrice = 497;
-				$productObj->shippingIdDomestic = 26;
-				$productObj->shippingIdInternational = null; //no international sales
-				$productObj->shippingCostDomestic = 0;
-				$productObj->shippingCostInternational = 0; //no international sales
-				$productObj->mpsId = null; //used for MPS
-				$productObj->campaignId = 16;
-				$productObj->netRevenueEach = 448;
-				$productObj->taxable = TRUE;
-				$productObj->listId = 57;
-				$productObj->tags = "ppgDeluxe2pay";
-				$productObj->googleProductName = "PPG Platinum 2Pay " . self::VSL_VERSION;
-				$productObj->metaTitle = "PPG Platinum Upgrade - 2 Easy Payment Plan";
-				$productObj->metaDescription = "PPG Platinum Upgrade - 2 Easy Payment Plan";
-				$productObj->defaultQuantity = 1;
-				$productObj->nextPage = "/checkout/oto/ppg-1year.php";
-				break;
-			case 168: //PPG Deluxe Upgrade - 2nd Payment (NO SHIP)
-				$productObj->pmaSku = null;
-				$productObj->price = 497;
-				$productObj->originalPrice = 497;
-				$productObj->shippingIdDomestic = 26;
-				$productObj->shippingIdInternational = null; //no international sales
-				$productObj->shippingCostDomestic = 0;
-				$productObj->shippingCostInternational = 0; //no international sales
-				$productObj->mpsId = null; //used for MPS
-				$productObj->campaignId = 16;
-				$productObj->netRevenueEach = 0;
-				$productObj->taxable = TRUE;
-				$productObj->listId = null;
-				$productObj->tags = null;
-				$productObj->googleProductName = "PPG Platinum 2Pay2 " . self::VSL_VERSION;
-				$productObj->metaTitle = "PPG Platinum Upgrade - 2nd Payment (NO SHIP)";
-				$productObj->metaDescription = "PPG Platinum Upgrade - 2nd Payment (NO SHIP)";
-				$productObj->defaultQuantity = 1;
-				$productObj->nextPage = "/checkout/oto/ppg-platinum.php";
-				break;
-			case 40: //Food4Patriots 1 Year Food Kit
-				$productObj->pmaSku = null;
-				$productObj->price = 1997;
-				$productObj->originalPrice = 1997;
-				$productObj->shippingIdDomestic = 7;
-				$productObj->shippingIdInternational = null; //no international sales
-				$productObj->shippingCostDomestic = 0;
-				$productObj->shippingCostInternational = 0; //no international sales
-				$productObj->mpsId = null; //used for MPS
-				$productObj->campaignId = 9;
-				$productObj->netRevenueEach = 952;
-				$productObj->taxable = TRUE;
-				$productObj->listId = 58;
-				$productObj->tags = "ppg1YrKit";
-				$productObj->googleProductName = "F4P 1YR " . self::VSL_VERSION;
-				$productObj->metaTitle = "Food4Patriots 1 Year Food Kit";
-				$productObj->metaDescription = "Food4Patriots 1 Year Food Kit";
-				$productObj->defaultQuantity = 1;
-				$productObj->nextPage = "/checkout/thankyou.php";
-				break;
-			case 120: //Food4Patriots 1 Year Food Kit w/ payments
-				$productObj->pmaSku = null;
-				$productObj->price = 597;
-				$productObj->originalPrice = 597;
-				$productObj->shippingIdDomestic = 7;
-				$productObj->shippingIdInternational = null; //no international sales
-				$productObj->shippingCostDomestic = 0;
-				$productObj->shippingCostInternational = 0; //no international sales
-				$productObj->mpsId = null; //used for MPS
-				$productObj->campaignId = 9;
-				$productObj->netRevenueEach = 952;
-				$productObj->taxable = TRUE;
-				$productObj->listId = 60;
-				$productObj->tags = "ppg1YrKitPmt";
-				$productObj->googleProductName = "F4P 1YR Payments" . self::VSL_VERSION;
-				$productObj->metaTitle = "Food4Patriots 1 Year Food Kit";
-				$productObj->metaDescription = "Food4Patriots 1 Year Food Kit";
-				$productObj->defaultQuantity = 1;
-				$productObj->nextPage = "/checkout/thankyou.php";
-				break;
-			case 148: //Power4Patriots Platinum Hard Copy Book Version 2.0
-				$productObj->pmaSku = "P4PHCBK";
-				$productObj->price = 97;
-				$productObj->originalPrice = 97;
 				$productObj->shippingIdDomestic = 8;
 				$productObj->shippingIdInternational = 6;
 				$productObj->shippingCostDomestic = null;
 				$productObj->shippingCostInternational = null;
-				$productObj->mpsId = null; //used for MPS
-				$productObj->campaignId = 1;
-				$productObj->netRevenueEach = 84;
+				$productObj->mpsId = 19;
+				$productObj->campaignId = 9;
+				$productObj->netRevenueEach = 245;
 				$productObj->taxable = TRUE;
-				$productObj->listId = 14;
-				$productObj->tags = "p4pplatinum";
-				$productObj->googleProductName = "P4P Platinum " . self::VSL_VERSION;
-				$productObj->metaTitle = "Power4Patriots Platinum Hardcopy Package";
-				$productObj->metaDescription = "Power4Patriots Platinum Hardcopy Package";
+				$productObj->listId = 35;
+				$productObj->tags = "3MKIT";
+				$productObj->googleProductName = "F4P-3MK";
+				$productObj->googleProductSKU = "PID19";
+				$productObj->googleProductCategory = "1-PAY-497";
+				$productObj->metaTitle = "Food4Patriots 3 Month Food Supply";
+				$productObj->metaDescription = "Food4Patriots 3 Month Food Supply";
 				$productObj->defaultQuantity = 1;
-				$productObj->nextPage = "/checkout/thankyou-p4p-platinum.php";
-				break;		}
+				$productObj->isBonus = FALSE;
+				$productObj->hasBonuses = FALSE; //set this to trigger any bonuses for this id
+				$productObj->bonusIds = array (0); //set this as a single integer in an array (1) or a string of integers (123,456)
+				$productObj->nextPage = "/checkout/oto/f4p-oto-1year.php";
+				break;
+			case 18: //additional product
+				$productObj->pmaSku = null;
+				$productObj->price = 197;
+				$productObj->originalPrice = 197;
+				$productObj->shippingIdDomestic = 8;
+				$productObj->shippingIdInternational = 6;
+				$productObj->shippingCostDomestic = null;
+				$productObj->shippingCostInternational = null;
+				$productObj->mpsId = 18;
+				$productObj->campaignId = 9;
+				$productObj->netRevenueEach = 110;
+				$productObj->taxable = TRUE;
+				$productObj->listId = 35;
+				$productObj->tags = "4WKIT";
+				$productObj->googleProductName = "F4P-4WK";
+				$productObj->googleProductSKU = "PID18";
+				$productObj->googleProductCategory = "1-PAY-197";
+				$productObj->metaTitle = "Food4Patriots 4 Week Food Supply";
+				$productObj->metaDescription = "Food4Patriots 4 Week Food Supply";
+				$productObj->defaultQuantity = 1;
+				$productObj->isBonus = FALSE;
+				$productObj->hasBonuses = FALSE; //set this to trigger any bonuses for this id
+				$productObj->bonusIds = array (0); //set this as a single integer in an array (1) or a string of integers (123,456)
+				$productObj->nextPage = "/checkout/oto/f4p-oto-b.php";
+				break;
+			case 17: //additional product
+				$productObj->pmaSku = null;
+				$productObj->price = 27;
+				$productObj->originalPrice = 27;
+				$productObj->shippingIdDomestic = 14;
+				$productObj->shippingIdInternational = 19;
+				$productObj->shippingCostDomestic = null;
+				$productObj->shippingCostInternational = null;
+				$productObj->mpsId = 17;
+				$productObj->campaignId = 9;
+				$productObj->netRevenueEach = 17;
+				$productObj->taxable = TRUE;
+				$productObj->listId = 35;
+				$productObj->tags = "72HKIT";
+				$productObj->googleProductName = "F4P-72HRK";
+				$productObj->googleProductSKU = "PID17";
+				$productObj->googleProductCategory = "1-PAY-27";
+				$productObj->metaTitle = "Food4Patriots 72 Hour Food Supply";
+				$productObj->metaDescription = "Food4Patriots 72 Hour Food Supply";
+				$productObj->defaultQuantity = 1;
+				$productObj->isBonus = FALSE;
+				$productObj->hasBonuses = FALSE; //set this to trigger any bonuses for this id
+				$productObj->bonusIds = array (0); //set this as a single integer in an array (1) or a string of integers (123,456)
+				$productObj->nextPage = "/checkout/oto/f4p-oto-a.php";
+				break;
+		}
 		return $productObj;	
 	}
 	
