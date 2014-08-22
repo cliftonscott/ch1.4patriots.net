c1b9ec5bfc76ef26ffdc3aeb5079d12cae7f8400
Brian Gibbins <brian@briangibbins.com>
2014-08-22 12:52:28 -0400

    FFP-152 updated product file with additional products for this site

--- libraries/Product.php
+++ libraries/Product.php
@@ -18,6 +18,8 @@ class Product {
 	}
 	
 	function getProduct($productId) {
+
+		// is #21 new? isn't in old process file
 		
 		$productObj = new stdClass();
 		
@@ -29,31 +31,31 @@ class Product {
 		//TODO ensure there is a variable here
 		$productObj->productId = intval($productId);
 		switch($productObj->productId) {
-			
-			case 19: //main product - 3 Month Kit
+
+			case 17: //additional product
 				$productObj->pmaSku = null;
-				$productObj->price = 497;
-				$productObj->originalPrice = 497;
-				$productObj->shippingIdDomestic = 8;
-				$productObj->shippingIdInternational = 6;
+				$productObj->price = 27;
+				$productObj->originalPrice = 27;
+				$productObj->shippingIdDomestic = 14;
+				$productObj->shippingIdInternational = 19;
 				$productObj->shippingCostDomestic = null;
 				$productObj->shippingCostInternational = null;
-				$productObj->mpsId = 19;
+				$productObj->mpsId = 17;
 				$productObj->campaignId = 9;
-				$productObj->netRevenueEach = 245;
+				$productObj->netRevenueEach = 17;
 				$productObj->taxable = TRUE;
 				$productObj->listId = 35;
-				$productObj->tags = "3MKIT";
-				$productObj->googleProductName = "F4P-3MK";
-				$productObj->googleProductSKU = "PID19";
-				$productObj->googleProductCategory = "1-PAY-497";
-				$productObj->metaTitle = "Food4Patriots 3 Month Food Supply";
-				$productObj->metaDescription = "Food4Patriots 3 Month Food Supply";
+				$productObj->tags = "72HKIT";
+				$productObj->googleProductName = "F4P-72HRK";
+				$productObj->googleProductSKU = "PID17";
+				$productObj->googleProductCategory = "1-PAY-27";
+				$productObj->metaTitle = "Food4Patriots 72 Hour Food Supply";
+				$productObj->metaDescription = "Food4Patriots 72 Hour Food Supply";
 				$productObj->defaultQuantity = 1;
 				$productObj->isBonus = FALSE;
 				$productObj->hasBonuses = FALSE; //set this to trigger any bonuses for this id
 				$productObj->bonusIds = array (0); //set this as a single integer in an array (1) or a string of integers (123,456)
-				$productObj->nextPage = "/checkout/oto/f4p-oto-1year.php";
+				$productObj->nextPage = "/checkout/oto/f4p-oto-a.php";
 				break;
 			case 18: //additional product
 				$productObj->pmaSku = null;
@@ -80,31 +82,210 @@ class Product {
 				$productObj->bonusIds = array (0); //set this as a single integer in an array (1) or a string of integers (123,456)
 				$productObj->nextPage = "/checkout/oto/f4p-oto-b.php";
 				break;
-			case 17: //additional product
+			case 19: //main product - 3 Month Kit
 				$productObj->pmaSku = null;
-				$productObj->price = 27;
-				$productObj->originalPrice = 27;
-				$productObj->shippingIdDomestic = 14;
-				$productObj->shippingIdInternational = 19;
+				$productObj->price = 497;
+				$productObj->originalPrice = 497;
+				$productObj->shippingIdDomestic = 8;
+				$productObj->shippingIdInternational = 6;
 				$productObj->shippingCostDomestic = null;
 				$productObj->shippingCostInternational = null;
-				$productObj->mpsId = 17;
+				$productObj->mpsId = 19;
 				$productObj->campaignId = 9;
-				$productObj->netRevenueEach = 17;
+				$productObj->netRevenueEach = 245;
 				$productObj->taxable = TRUE;
 				$productObj->listId = 35;
-				$productObj->tags = "72HKIT";
-				$productObj->googleProductName = "F4P-72HRK";
-				$productObj->googleProductSKU = "PID17";
-				$productObj->googleProductCategory = "1-PAY-27";
-				$productObj->metaTitle = "Food4Patriots 72 Hour Food Supply";
-				$productObj->metaDescription = "Food4Patriots 72 Hour Food Supply";
+				$productObj->tags = "3MKIT";
+				$productObj->googleProductName = "F4P-3MK";
+				$productObj->googleProductSKU = "PID19";
+				$productObj->googleProductCategory = "1-PAY-497";
+				$productObj->metaTitle = "Food4Patriots 3 Month Food Supply";
+				$productObj->metaDescription = "Food4Patriots 3 Month Food Supply";
 				$productObj->defaultQuantity = 1;
 				$productObj->isBonus = FALSE;
 				$productObj->hasBonuses = FALSE; //set this to trigger any bonuses for this id
 				$productObj->bonusIds = array (0); //set this as a single integer in an array (1) or a string of integers (123,456)
-				$productObj->nextPage = "/checkout/oto/f4p-oto-a.php";
+				$productObj->nextPage = "/checkout/oto/f4p-oto-1year.php";
 				break;
+			case 21:
+				//is this id new?
+				break;
+			case 22: // 4WEEK DISCOUNT KIT
+				//process file
+				$productObj->campaignId = 9;
+				$productObj->nextPage = "/checkout/oto/f4p-rutgers.php";
+				$productObj->listId = 35;
+				$productObj->tags = "4WKITUP";
+				$productObj->shippingIdDomestic = 8;
+				$productObj->shippingIdInternational = 6;
+				$productObj->shippingCostDomestic = null;
+				$productObj->shippingCostInternational = null;
+				//TODO add something for skip rutgers nextpage
+				$productObj->mpsId = 18;
+				//Limelight
+				$productObj->price = 147;
+				$productObj->originalPrice = 147;
+				//GA Naming Wiki
+				$productObj->netRevenueEach = 65;
+				$productObj->googleProductName = "F4P-4WK";
+				$productObj->googleProductSKU = "PID22";
+				$productObj->googleProductCategory = "1-PAY-147-DISCOUNT";
+				$productObj->metaTitle = "Additional Food4Patriots 4 Week Food Supply $50 Off";
+				$productObj->metaDescription = "Additional Food4Patriots 4 Week Food Supply $50 Off";
+				//Other
+				$productObj->pmaSku = null;
+				$productObj->taxable = TRUE;
+				$productObj->defaultQuantity = 1;
+				$productObj->isBonus = FALSE;
+				$productObj->hasBonuses = FALSE; //set this to trigger any bonuses for this id
+				$productObj->bonusIds = array (0); //set this as a single integer in an array (1) or a string of integers (123,456)
+				break;
+			case 23: // 3MONTH DISCOUNT KIT
+				//process file
+				$productObj->campaignId = 9;
+				$productObj->nextPage = "/checkout/registration/thankyou.php";
+				$productObj->listId = 35;
+				$productObj->tags = "3MKITUP";
+				$productObj->shippingIdDomestic = 8;
+				$productObj->shippingIdInternational = 6;
+				$productObj->shippingCostDomestic = null;
+				$productObj->shippingCostInternational = null;
+				$productObj->mpsId = 18;
+				//Limelight
+				$productObj->price = 397;
+				$productObj->originalPrice = 397;
+				//GA Naming Wiki
+				$productObj->netRevenueEach = 150;
+				$productObj->googleProductName = "F4P-3MK";
+				$productObj->googleProductSKU = "PID23";
+				$productObj->googleProductCategory = "1-PAY-397-DISCOUNT";
+				$productObj->metaTitle = "Additional Food4Patriots 3 Month Food Supply $100 Off";
+				$productObj->metaDescription = "Additional Food4Patriots 3 Month Food Supply $100 Off";
+				//Other
+				$productObj->pmaSku = null;
+				$productObj->taxable = TRUE;
+				$productObj->defaultQuantity = 1;
+				$productObj->isBonus = FALSE;
+				$productObj->hasBonuses = FALSE; //set this to trigger any bonuses for this id
+				$productObj->bonusIds = array (0); //set this as a single integer in an array (1) or a string of integers (123,456)
+				break;
+			case 27: // 3MONTH DISCOUNT KIT
+				//process file
+				$productObj->campaignId = 9;
+				$productObj->nextPage = "/checkout/oto/f4p-oto-d.php";
+				$productObj->listId = 42;
+				$productObj->tags = "FREEFOOD";
+				$productObj->shippingIdDomestic = 20;
+				$productObj->shippingIdInternational = 21;
+				$productObj->shippingCostDomestic = null;
+				$productObj->shippingCostInternational = null;
+				$productObj->mpsId = 27;
+				//Limelight
+				$productObj->price = 0;
+				$productObj->originalPrice = 0;
+				//GA Naming Wiki
+				$productObj->netRevenueEach = 0;
+				$productObj->googleProductName = "F4P-72HRK";
+				$productObj->googleProductSKU = "PID27";
+				$productObj->googleProductCategory = "FREE-PLUS-SHIPPING";
+				$productObj->metaTitle = "Food4Patriots FREE Survival Food Offer";
+				$productObj->metaDescription = "Food4Patriots FREE Survival Food Offer";
+				//Other
+				$productObj->pmaSku = null;
+				$productObj->taxable = TRUE;
+				$productObj->defaultQuantity = 1;
+				$productObj->isBonus = FALSE;
+				$productObj->hasBonuses = FALSE; //set this to trigger any bonuses for this id
+				$productObj->bonusIds = array (0); //set this as a single integer in an array (1) or a string of integers (123,456)
+				break;
+			case 40: // 1YEAR KIT
+				//process file
+				$productObj->campaignId = 9;
+				$productObj->nextPage = "/checkout/registration/thankyou.php";
+				$productObj->listId = 35;
+				$productObj->tags = "1YRKIT";
+				$productObj->shippingIdDomestic = 8;
+				$productObj->shippingIdInternational = 6;
+				$productObj->shippingCostDomestic = null;
+				$productObj->shippingCostInternational = null;
+				$productObj->mpsId = 40;
+				//Limelight
+				$productObj->price = 1997;
+				$productObj->originalPrice = 1997;
+				//GA Naming Wiki
+				$productObj->netRevenueEach = 952;
+				$productObj->googleProductName = "F4P-1YK";
+				$productObj->googleProductSKU = "PID40";
+				$productObj->googleProductCategory = "1-PAY-1997";
+				$productObj->metaTitle = "Food4Patriots 1 Year Food Supply";
+				$productObj->metaDescription = "Food4Patriots 1 Year Food Supply";
+				//Other
+				$productObj->pmaSku = null;
+				$productObj->taxable = TRUE;
+				$productObj->defaultQuantity = 1;
+				$productObj->isBonus = FALSE;
+				$productObj->hasBonuses = FALSE; //set this to trigger any bonuses for this id
+				$productObj->bonusIds = array (0); //set this as a single integer in an array (1) or a string of integers (123,456)
+				break;
+			case 120: // 1YEAR KIT 3 PAYMENTS $597
+				//process file
+				$productObj->campaignId = 9;
+				$productObj->nextPage = "/checkout/registration/thankyou.php";
+				$productObj->listId = 35;
+				$productObj->tags = "1YRKIT3PAYMENT";
+				$productObj->shippingIdDomestic = 8;
+				$productObj->shippingIdInternational = 6;
+				$productObj->shippingCostDomestic = null;
+				$productObj->shippingCostInternational = null;
+				$productObj->mpsId = 40;
+				//Limelight
+				$productObj->price = 597;
+				$productObj->originalPrice = 597;
+				//GA Naming Wiki
+				$productObj->netRevenueEach = 411;
+				$productObj->googleProductName = "F4P-1YK";
+				$productObj->googleProductSKU = "PID120";
+				$productObj->googleProductCategory = "3-PAY-597";
+				$productObj->metaTitle = "Food4Patriots 1 Year Food Supply 3 Payments";
+				$productObj->metaDescription = "Food4Patriots 1 Year Food Supply 3 Payments";
+				//Other
+				$productObj->pmaSku = null;
+				$productObj->taxable = TRUE;
+				$productObj->defaultQuantity = 1;
+				$productObj->isBonus = FALSE;
+				$productObj->hasBonuses = FALSE; //set this to trigger any bonuses for this id
+				$productObj->bonusIds = array (0); //set this as a single integer in an array (1) or a string of integers (123,456)
+				break;
+			case 128: // FRUIT VEGGIE SNACK KIT
+				//process file
+				$productObj->campaignId = 9;
+				$productObj->nextPage = "/checkout/registration/thankyou-snack.php";
+				$productObj->listId = 35;
+				$productObj->tags = "3MKITSNACK";
+				$productObj->shippingIdDomestic = 7;
+				$productObj->shippingIdInternational = 6;
+				$productObj->shippingCostDomestic = null;
+				$productObj->shippingCostInternational = null;
+				$productObj->mpsId = 128;
+				//Limelight
+				$productObj->price = 97;
+				$productObj->originalPrice = 97;
+				//GA Naming Wiki
+				$productObj->netRevenueEach = 35;
+				$productObj->googleProductName = "F4P-FVSK";
+				$productObj->googleProductSKU = "PID128";
+				$productObj->googleProductCategory = "1-PAY-97";
+				$productObj->metaTitle = "Food4Patriots Fruit, Veggie, And Snack Kit";
+				$productObj->metaDescription = "Food4Patriots Fruit, Veggie, And Snack Kit";
+				//Other
+				$productObj->pmaSku = null;
+				$productObj->taxable = TRUE;
+				$productObj->defaultQuantity = 1;
+				$productObj->isBonus = FALSE;
+				$productObj->hasBonuses = FALSE; //set this to trigger any bonuses for this id
+				$productObj->bonusIds = array (0); //set this as a single integer in an array (1) or a string of integers (123,456)
+				break;
+
 		}
 		return $productObj;	
 	}
