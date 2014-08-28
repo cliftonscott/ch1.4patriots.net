52ed9b7ad7f04a99b7677f7be6ddcb2c294adc95
Brian Gibbins <brian@briangibbins.com>
2014-08-28 12:56:16 -0400

    FFP-188 new semi-universal js file to handle shipping and tax calculations based on customers state and the quantity selected.

---
+++ js/set-state-tax-multi-pid.js
@@ -0,0 +1,78 @@
+/*
+
+This is used for upsell forms that require taxes to be shown
+ex: http://dev.sf4p.4patriots.net/checkout/oto/f4p-choose-3m-4w-kit.php
+
+It handles shipping as well as taxes, hiding the entire rows if not applicable
+
+This function allows a developer to set the stateTax based on a productId
+passed to the function and the current values set in on the form in a series
+of named fields that end with _ProductId for each of the productIds on the page
+
+There are a number of To Dos that should be completed to make this multi form
+handler better.
+
+See the inline notes
+
+*/
+function setStateTax(productId) {
+
+	//TODO skip this whole thing if the country isn't US, will need to return "" to the calling function
+
+    taxState = document.getElementById('taxState_' + productId).value;
+	quantity = document.getElementById('quantity_' + productId).value;
+
+	formSubTotal = document.getElementById('subTotal_' + productId);
+	formTaxAmount = document.getElementById('taxAmount_' + productId);
+	formTotalAmount = document.getElementById('totalAmount_' + productId);
+	formTaxRow = document.getElementById('taxRow_' + productId);
+    formShippingAmount = document.getElementById('shippingAmount_' + productId);
+	formShippingRow = document.getElementById('shippingRow_' + productId);
+
+    //get product data
+    //TODO get product data from Product.php via ajax
+    //TODO FFP-232 - refactor set-state-tax-multi-pid.js to pull data from Product.php
+    //TODO remove the on-form-page JSON product descriptions when above is completed
+	jsProductSrc = document.getElementById('productData[' + productId + ']');
+	jsProductData = jsProductSrc.value;
+	jsProductData = jsProductData.replace(/'/g,"\"");
+	jsProductObj = JSON.parse(jsProductData);
+
+	shippingPerItem = parseFloat(jsProductObj.shipping);
+    shippingCost = parseFloat(shippingPerItem * quantity);
+    formShippingAmount.innerHTML = "$" + shippingCost.toFixed(2);
+    if(shippingPerItem > 0) {
+        formShippingRow.setAttribute("style", "display:block;visibility:visible;");
+    } else {
+        formShippingRow.setAttribute("style", "display:none;visibility:hidden;");
+    }
+
+	productPrice = parseFloat(jsProductObj.price);
+	productSubTotal = productPrice * quantity;
+
+    //TODO get state tax rates from Tax.php library via ajax
+    //TODO FFP-233 - refactor set-state-tax-multi-pid.js to get state taxes from Tax.php
+	switch(taxState) {
+        case "tennessee":
+            stateTax = (productSubTotal + shippingCost) * .0925;
+            formTaxAmount.innerHTML = "$" + stateTax.toFixed(2);
+            formSubTotal.innerHTML = "$" + productSubTotal.toFixed(2);
+            formTotalAmount.innerHTML = "$" + (productSubTotal + stateTax + shippingCost).toFixed(2) + " USD (One Time)";
+            formTaxRow.setAttribute("style", "display:block;visibility:visible;");
+            break;
+        case "arizona":
+            stateTax = productSubTotal * .0810;
+            formTaxAmount.innerHTML = "$" + stateTax.toFixed(2);
+            formSubTotal.innerHTML = "$" + productSubTotal.toFixed(2);
+            formTotalAmount.innerHTML = "$" + (productSubTotal + stateTax + shippingCost).toFixed(2) + " USD (One Time)";
+            formTaxRow.setAttribute("style", "display:block;visibility:visible;");
+            break;
+        default:
+            formTaxAmount.innerHTML = "$0.00";
+            formSubTotal.innerHTML = "$" + productSubTotal.toFixed(2);
+            formTotalAmount.innerHTML = "$" + (productSubTotal + shippingCost).toFixed(2) + " USD (One Time)";
+            formTaxRow.setAttribute("style", "display:none;visibility:hidden;");
+            break;
+	}
+
+}
