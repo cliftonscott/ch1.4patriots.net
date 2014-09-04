b3ebc07db08a96a8ef43d16f65d9306ab09aee36
Brian Gibbins <brian@briangibbins.com>
2014-09-04 12:20:30 -0400

    FFP-241 created simple file to return product information in json format

---
+++ api/product.php
@@ -0,0 +1,9 @@
+<?php
+include_once("Product.php");
+$productObj = new Product();
+$productDataObj = $productObj->getProduct($_REQUEST["pId"]);
+$return["productId"] = $productDataObj->productId;
+$return["price"] = $productDataObj->price;
+$return["shippingCostDomestic"] = $productDataObj->shippingCostDomestic;
+$return["shippingCostInternational"] = $productDataObj->shippingCostInternational;
+echo json_encode($return);
