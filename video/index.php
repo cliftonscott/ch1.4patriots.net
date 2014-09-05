c1f6a950c9be5949805d0d1e95848a40abba0fca
Brian Gibbins <brian@briangibbins.com>
2014-09-05 14:35:09 -0400

    FFP-210 clean up sawbutton script, add new video countdown js script, add new div for countdown timer to populate in html, move mobile detection script earlier in load for better mobile performance

--- video/index.php
+++ video/index.php
@@ -17,6 +17,10 @@ $productDataObj = Product::getProduct($_SESSION["productId"]);
 include_once ('template-top.php');
 include_once ('template-header.php'); /*Add template-header-nav.php to add top menu*/
 ?>
+<script>
+if (isMobile()) {
+	document.location = "<?php echo $productDataObj->mobileLink ?>"; }
+</script>
 <script src="/js/jquery.timers-1.2.js" type="text/javascript"></script>
 <script src="/js/jcookie.js" type="text/javascript"></script>
 <script>
@@ -25,35 +29,25 @@ include_once ('template-header.php'); /*Add template-header-nav.php to add top m
         $(document).ready(function(){
 
 				if ($.cookie("sawbutton")) {
-				
 			 		var hours = 0;
 	                var minutes = 0; 
 	                var seconds = 5;
-				
-				}
-				else {
-
+				} else {
 			 		var hours = 0;
 	                var minutes = 51; 
 	                var seconds = 27;
-				
 				}
 		 	
 		        // Start by converting hours to milliseconds
 		        var time = hours * 60 * 60 * 1000;
-		 
 		        // Add minutes converted to milliseconds and add to total time
 		        time += minutes * 60 * 1000;
-		 
 		        // Add seconds to total time after converting to milliseconds
 		        time += seconds * 1000;
-		               
+
 				if ($.cookie("sawbutton")) {
-				
 					// If return visitor that saw button, show alt button
-
 	                $("#reserve").oneTime(time, function() {
-	 
 	                        $("#reserve").css("display", "block");
 	                        $("#reserve").oneTime(5000, function() {
 									$("#reserve").css("display", "none");
@@ -62,33 +56,47 @@ include_once ('template-header.php'); /*Add template-header-nav.php to add top m
 							   });
 	                        
 	                });
-				
-				}
-				else {
-				
+				} else {
 					// If visitor hasn't seen button yet, show default button
- 
 	                $("#reserve").oneTime(time, function() {
-	 
 	                        $("#reserve").css("display", "block");
 	                        $("#reserve").oneTime(5000, function() {
 									$("#reserve").css("display", "none");
 									$("#buyButton").css("display", "block");
 									$("#buyButton2").css("display", "block");
 							   });
-	                        
 	                });
-	                
 				}
-
-                setTimeout(function(){$.cookie("sawbutton", "1", { expires: 30 });}, 30000);               
-
+                setTimeout(function(){$.cookie("sawbutton", "1", { expires: 30 });}, 30000);
 		});
 
 </script>
-<script> 
-if (isMobile()) {
-	document.location = "<?php echo $productDataObj->mobileLink ?>"; }
+<script>
+/*
+	This is the countdown timer, used for the visual display of the 'clock'.
+	It uses a date object where the last three integers are hours, minutes, seconds.
+	When the timer reaches zero it clears the timer object and calls a function
+	to call the CSR Modal Window.
+
+	This timer requires a block object (div) with an id of 'countDownTimer'.
+ */
+	var jsTimer = setInterval(function(){timerChange()},1000);
+	timerDateObj = new Date(2014, 01, 01, 12, 39, 00);
+	function timerChange() {
+		time = timerDateObj.getTime();
+		newTime = time - 1000;
+		timerDateObj.setTime(newTime);
+		m = timerDateObj.getMinutes();
+		s = timerDateObj.getSeconds();
+		if(s < 10) {
+			s = "0" + s;
+		}
+		$("#countDownTimer").html(m + ":" + s);
+		if(parseInt(s) + parseInt(m) == 0) {
+			clearInterval(jsTimer);
+			showCsrModal();
+		}
+	}
 </script>
 <div class="container subheader"></div>
 <div class="container-main">
@@ -116,7 +124,7 @@ if (isMobile()) {
             	<!-- Button Stuff -->
                 <div id="buyButton2" class="center-block text-center" style="display:none">
                 	<h2 class="darkRed"><strong>Act fast! Your reservation and discount <br> are guaranteed until...</strong></h2>
-               	  <div>(Add Timer Here)</div>
+               	  <div id="countDownTimer"></div>
                     <a href="<?php echo $productDataObj->offerLink; ?>" onClick="ga('send', 'event', 'free-video', 'food-stockpile-buy', 'click-to-accept');"><img class="img-responsive center-block" src="/assets/images/buttons/btn-orange-choose-kit-01.jpg" alt="Order Now!"></a>
                 </div>
             </div>
