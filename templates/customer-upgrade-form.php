a33c6ee438bd1fe110dfa1d1a7edd5e28e6dc5b8
Brian Gibbins <brian@briangibbins.com>
2014-09-18 11:29:27 -0400

    FFP-250 create initial upgrade form with include for generic form

---
+++ templates/customer-upgrade-form.php
@@ -0,0 +1,17 @@
+<div class="row">
+	<div class="col-sm-6 col-md-6">
+		<div class="center-block text-center">
+			<div>
+				<p class="text-success h3">
+					<?php echo $productDataObj->metaTitle; ?>
+				</p>
+				<p>
+					<?php echo $productDataObj->metaDescription; ?>
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-6 col-md-6">
+		<?php include_once ('customer-form.php'); ?>
+	</div>
+</div>
