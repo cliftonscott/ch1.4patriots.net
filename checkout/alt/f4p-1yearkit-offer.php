d2562c93815b64e8ae2e238dc85ec5eed34a7442
jeremy <jeremy@rebootmarketing.com>
2014-09-20 11:28:37 -0500

    FFP-234 1 Year Alt Page Updates

--- checkout/alt/f4p-1yearkit-offer.php
+++ checkout/alt/f4p-1yearkit-offer.php
@@ -28,7 +28,105 @@ include_once ('template-top.php');
 
 <div class="container-main">
 
-<div class="container">
+<div id="content" style="display:block;">
+<div class="container oto-width">
+		<div><h1 class="darkRed text-center">Get The Brand-New 1-Year Food4Patriots Kit For $200.00 Off Plus FREE Shipping &amp; 27 FREE Bonus Gifts!</h1></div>
+		<div class="text-center">
+			<img class="img-responsive center-block" src="/media/images/f4p/f4p-1-year-kit-01.jpg" alt="Food4Patriots 1 Year Kit" style="margin-bottom:20px;">
+			<span class="caption"><i>Save $200.00 when you order a 1 Year Kit,  plus get FREE Shipping, 4 FREE Lifestraw Personal Water Filers, 2 FREE brand-new Survival hard copy books, 4 FREE Liberty Seed Vaults, 4 FREE 11-in-1 Survival Tools and more!</i></span>
+		</div>
+		<div style="margin-top:50px;">
+			<p>Thank you for being a Food4Patriots customer! I hope that you love you love your Food4Patriots kit and have been sleeping easier at night knowing that you are more prepared for whatever may happen.</p>
+			<p>I've got a special "customers only" offer for you...</p>
+			<p>A lot of folks have told me that given the scary state of affairs in this country and the constant threat of natural disasters, they want more food. In fact, we have quite a few folks telling us, &ldquo;I&rsquo;ll take everything you&rsquo;ve got!&rdquo;</p>
+			<p>I want to do everything I can to help you build your food stockpile as quickly and easily as possible, so to thank you for being a customer today, I am offering you an <strong>exclusive $200.00 discount</strong> on a ONE YEAR Food4Patriots kit if you act now. But this special sale offer is ONLY for customers so if you&rsquo;re seeing this, then good news, you qualify!</p>
+			<p>Plus I'll throw in <strong>FREE Shipping and 27 FREE bonus gifts worth $470.00 -- including 4 of the super-popular Lifestraw Personal Water Filters and over 22,000+ heirloom survival seeds</strong> -- just to make this a "no-brainer" for you!</p>
+			<p>Would you like to accelerate your results by getting the 1 Year Food4Patriots Kit at a 1-time discount sale price of $1,997? (That&rsquo;s a $200.00 discount…you&rsquo;ll get 1 year&rsquo;s worth of food for just $1.11 per serving!)</p>
+			<img class="img-responsive center-block" src="/media/images/f4p/f4p-testimonials-10.png" alt="L. Graeser's Testimonial" style="margin-bottom:20px;">
+		</div>
+</div>
+<?php include("f4p-1year-glenbeck.html");?>
+<?php include("f4p-1year-whatsincluded.html");?>
+<div class="container oto-width">
+	<h2 class="darkRed text-center">Check Out The Amazing FREE Bonuses You Can ONLY Get With The 1 Year Kit!</h2>
+	<p>You’re going to get the “mother lode” of special bonuses that are ONLY available with the 1 Year Kit!</p>
+</div>
+<?php include("f4p-1year-bonus-lifestraw.html");?>
+<?php include("f4p-1year-bonus-survivalbooks.html");?>
+<?php //include("f4p-1year-bonus-pam.html");?>
+<div class="container oto-width">
+	<h2 class="darkRed text-center">You Will Also Get FOUR More Sets Of All Of Your Original FREE Bonuses When You Take Advantage Of This Special Upgrade Offer! </h2>
+	<p>Add to your own stockpile, keep at multiple locations or give to friends and loved ones so they can experience the same peace of mind. Your FREE bonuses include:</p>
+</div>
+<?php include("f4p-1year-bonus-seeds.html");?>
+<?php include("f4p-1year-bonus-tool.html");?>
+<?php include("f4p-1year-bonus-reports.html");?>
+
+<div class="container oto-width">
+
+			<h2 class="darkRed text-center">You Are 100% Protected By My Outrageous Double Guarantee.</h2>
+			<div class="outLineBoxDarkBlue">
+				<p><img src="/media/images/misc/seal-guarantee-satisfaction.jpg" alt="Guarantee #1" width="250" height="189" class="pull-left img-responsive media">
+					<h3>Guarantee #1:</h3> This is a 100% money back guarantee. No questions asked. If for any reason, you&rsquo;re not 
+					satisfied with your Food4Patriots kit, just return it within 60 days of purchase and I&rsquo;ll refund 100% of your purchase 
+					price. If you try it and decide it&rsquo;s not as delicious and nutritious as I promised, you can have your money back for 
+					any reason or no reason whatsoever. So there&rsquo;s absolutely no risk for you. You literally can&rsquo;t lose!</p>
+				<div class="clearfix"></div>
+			</div>
+
+			<div class="outLineBoxDarkBlue">
+				<p><img src="/media/images/misc/seal-guarantee-money.jpg" alt="Guarantee #2" width="250" height="192" class="pull-left img-responsive media">
+				<h3>Guarantee #2:</h3> This is an unheard of 300% money back guarantee. It&rsquo;s in addition to guarantee #1. If you open any 
+				of your Food4Patriots meals anytime <strong>in the next 25 years</strong> and find that your food has spoiled or gone bad, you 
+				can return your entire Food4Patriots stockpile and I will <strong>triple</strong> your money back!</p>
+				
+				<p>That&rsquo;s how confident I am that this food will remain just as delicious and nutritious for the next 25 years as it is 
+					on the day you buy it. Some of my friends said I was crazy to offer this double guarantee, but to be honest I&rsquo;m not 
+					really worried about it, because I am so confident you&rsquo;re going to see the value in your Food4Patriots kit as soon 
+					as you have it in your hands.</p>
+			</div>
+
+			<div><img class="img-responsive center-block" src="/media/images/f4p/f4p-testimonials-09.png" width="626" height="269" alt="Stacy's Testimonial"></div>
+
+			<h2 class="darkRed title-max-600 center-block text-center">Get On The ‘Fast Track’ - Claim Your 1 Year 
+				Food4Patriots Kit For $1000.00 Off Right Now!</h2>
+
+			<p>Now I understand that the 1 Year Food4Patriots kit is the right choice for folks who are looking for the ultimate in food security. This kit plus all the bonuses is valued at over $3,000, but because you&rsquo;ve already taken the 1st step, and because I appreciate you putting your trust in us by being a customer, you can get your 1 Year Food4Patriots kit today for just $1,997.</p>
+				
+			<p>You get a year&rsquo;s worth of delicious survival food for $1.11 per serving!</p>
+			
+			<p>Plus I'll throw in 27 FREE bonus gifts worth $800+ -- including 4 of the super-popular Lifestraw Personal Water Filters and over 
+				22,000+ heirloom survival seeds -- just to make this a "no-brainer" for you!</p>
+				
+			<p>I was only able to secure a limited quantity of these 1 Year Food4Patriots kits and it&rsquo;s been one of our most frequent requests, 
+				so I don&rsquo;t know how long I&rsquo;m going to have them available. To make sure that you don&rsquo;t miss out on getting yours, 
+				go ahead and click the big orange &ldquo;Click Here To Accept&rdquo; button below to add the 1 Year Food4Patriots Kit to your order 
+				today!</p>
+				
+			<p>The 1 Year Food4Patriots kit will help secure your stockpile faster and protect you and your family from whatever crisis may come. 
+				You&rsquo;ll be on the &ldquo;fast track&rdquo; to securing the ultimate food stockpile.</p>
+				
+			<p>This is your last chance for this special 1-time discount, so you need to act now. To get your 1 Year 
+				Food4Patriots kit at $1000.00 off, click the big green &ldquo;Add To Cart&rdquo; button below.</p>
+
+			<div>
+				<div><img class="img-responsive center-block" src="/media/images/f4p/f4p-1-year-kit-01.jpg"  alt="Food4Patriots 1 Year Kit"/></div>
+				<div><img class="img-responsive center-block" src="/media/images/f4p/value-chart-3.jpg" alt="Value Chart"/></div>
+				<div class="text-center"><h2 id="save" class="darkRed">Act Today And Save Over $1000</h2></div>
+
+				<a id="accept"></a>
+
+				<div style="text-align:center;">
+					<a href="#" onclick="checkOut();"><img src="/assets/images/buttons/btn-green-add-to-cart-01.jpg" class="img-responsive center-block" alt="Add To Cart"></a>
+				</div>
+				<div style="margin-top:20px;"><img class="img-responsive center-block"  src="/media/images/f4p/f4p-testimonials-07.png" alt="Food4Patriots Testimonial"></div>
+				<div><img class="img-responsive center-block" src="/media/images/f4p/f4p-testimonials-02.png" alt="Food4Patriots Testimonial"></div>
+			</div>
+</div>
+</div>
+
+<!-- START ORDER FORM SECTION -->
+<div id="checkoutForm" class="container" style="display:none;">
 
 	<div class="row">
 	<div class="col-sm-6 col-md-7">
@@ -47,7 +145,7 @@ include_once ('template-top.php');
 					</a>
 					<div id="chooseProductThree" class="panel-collapse collapse in">
 						<div class="panel-body">
-						<img src="/media/images/f4p/f4p-1-year-kit-02.jpg" class="img-responsive center-block">
+						<a href="#info" onclick="showProductModal()"><img src="/media/images/f4p/f4p-1-year-kit-02.jpg" class="img-responsive center-block"></a>
 						<div class="container nopadding">
 							<div class="row">    
 								<div class="col-sm-12 col-md-6 nopadding">
@@ -83,14 +181,10 @@ include_once ('template-top.php');
 								<div class="col-sm-12">
 									<p><strong>When you order Food4Patriots, you get:</strong></p>
 									<ul>
-										<li><strong>Great Tasting Food That&rsquo;s Easy To Prepare</strong><br>
-										Just Add Boiling Water, Simmer, And Serve In Less Than 20 Minutes!</li>
-										<li><strong>&ldquo;Disaster-Proof&rdquo; Packaging</strong><br>
-										Mylar Pouches Keep Out Air, Moisture And Light To Keep The Food Nutritionally Sound And Tasting Great For Up To 25 Years.</li>
-										<li><strong>Nutritional Value</strong><br>
-										Meets The Highest Standard Of The Long-Term Food Reserve Industry. Each Product Serving Is An Entire Cup For Children And Adults Alike. No Half-Size &ldquo;Kid Portions&rdquo; Here!<br></li>
-										<li><strong>Minimal Storage Space</strong><br>
-										Our Kits Are More Compact Than Traditional Food Storage Systems And Allow You To Discreetly Save And Store In Less Space.</li>
+										<li><strong>Great Tasting Food That&rsquo;s Easy To Prepare</strong> - Just Add Boiling Water, Simmer, And Serve In Less Than 20 Minutes!</li>
+										<li><strong>&ldquo;Disaster-Proof&rdquo; Packaging</strong> - Mylar Pouches Keep Out Air, Moisture And Light To Keep The Food Nutritionally Sound And Tasting Great For Up To 25 Years.</li>
+										<li><strong>Nutritional Value</strong> - Meets The Highest Standard Of The Long-Term Food Reserve Industry. Each Product Serving Is An Entire Cup For Children And Adults Alike. No Half-Size &ldquo;Kid Portions&rdquo; Here!<br></li>
+										<li><strong>Minimal Storage Space</strong> - Our Kits Are More Compact Than Traditional Food Storage Systems And Allow You To Discreetly Save And Store In Less Space.</li>
 									</ul>
 								</div>
 							</div>
@@ -126,6 +220,10 @@ include_once ('template-top.php');
 
 </div> 
 <script>
+function checkOut(){
+	document.getElementById("content").style.display='none';
+	document.getElementById("checkoutForm").style.display='block';
+}
 $('#initial').find('.panel-heading').addClass("active-panel");
 $('#accordion > .panel').on('show.bs.collapse', function (e) {
 		$(this).find('.panel-heading').addClass("active-panel");
@@ -157,7 +255,7 @@ $('#accordion > .panel').on('hide.bs.collapse', function (e) {
 		$("#toolPopover").popover({
 			html:true,
 			trigger: 'hover',
-			title:"11-in-1 Survival Tool",
+			title:"4 11-in-1 Survival Tool",
 			content: "<img src=/media/images/f4p/multi-tool-02.jpg>"
 		});
 
@@ -186,7 +284,7 @@ $('#accordion > .panel').on('hide.bs.collapse', function (e) {
 	<div class="modal-dialog modal-lg">
 		<div class="modal-content">
 			<div class="glyphicon glyphicon-remove-circle" style="float:right;cursor:pointer;padding:10px;" onclick="hideProductModal();"></div>
-			<img class="img-responsive center-block" src="/media/images/ppg/ppg-product-checkout-02.jpg">
+			<img class="img-responsive center-block" src="/media/images/f4p/f4p-1-year-kit-03.jpg">
 		</div>
 	</div>
 </div>
