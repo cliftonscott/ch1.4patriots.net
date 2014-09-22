6935d342817cd5c751c6c47fe9202d767e14d0ba
jeremy <jeremy@rebootmarketing.com>
2014-09-22 12:44:01 -0500

    FFP-176 Header Modal

--- video/index.php
+++ video/index.php
@@ -97,7 +97,7 @@ if (isMobile()) {
 		}
 	}
 </script>
-<div class="container subheader"></div>
+<a href="#info" onclick="showProductModal()"><div class="container subheader"></div></a>
 <div class="container-main">
 	<div class="container">
 		<div class="row">
@@ -138,13 +138,61 @@ if (isMobile()) {
 				<!-- Start References -->
 				<?php include("snippets/video-references.html");?>
 				<!-- End References -->
-
-			</div>
-			<div class="col-md-12">
-
 			</div>
 		</div>
 	</div>
 </div>
+<!-- Header Food Images -->
+<div id="productModal" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="glyphicon glyphicon-remove-circle" style="float:right;cursor:pointer;padding:10px;" onclick="hideProductModal();"></div>
+			<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+				<!-- Indicators -->
+				<ol class="carousel-indicators">
+					<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="3"></li>
+				</ol>
+				<!-- Wrapper for slides -->
+				<div class="carousel-inner">
+					<div class="item active">
+					<img src="media/images/f4p/food/f4p-header-almond-coconut-granola.jpg" alt="Patriot Power Generator">
+					<div class="carousel-caption"></div>
+				</div>
+				<div class="item">
+					<img src="media/images/f4p/food/f4p-header-white-cheddar-shells.jpg" alt="Patriot Power Deluxe">
+					<div class="carousel-caption"></div>
+				</div>
+				<div class="item">
+					<img src="media/images/f4p/food/f4p-header-cheesy-chicken-rice-casserole.jpg" alt="Patriot Power Accesories">
+					<div class="carousel-caption"></div>
+				</div>
+				<div class="item">
+					<img src="media/images/f4p/food/f4p-header-chili-and-dumplings.jpg" alt="Patriot Power Accesories">
+					<div class="carousel-caption"></div>
+				</div>
+				</div>
+				<!-- Controls -->
+				<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+				<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+		</div>
+		</div>
+	</div>
+</div>
+<script>
+	function showProductModal() {
+		$('#productModal').modal('show');
+	}
+	function hideProductModal() {
+		$('#productModal').modal('hide');
+	}
+	$(document).ready(function(){
+		$('#mcarousel-example-generic').carousel({
+			interval: 5000
+		})
+	});
+</script>
 <?php
 include_once("template-bottom.php");
