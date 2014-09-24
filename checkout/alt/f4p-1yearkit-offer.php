497ef3bb5391fd567033ab0e6d17e1978aa31e4e
jeremy <jeremy@rebootmarketing.com>
2014-09-24 11:28:08 -0500

    FFP-148 1 Year Updates

--- checkout/alt/f4p-1yearkit-offer.php
+++ checkout/alt/f4p-1yearkit-offer.php
@@ -53,7 +53,7 @@ include_once ('template-top.php');
 </div>
 <?php include("f4p-1year-bonus-lifestraw.html");?>
 <?php include("f4p-1year-bonus-survivalbooks.html");?>
-<?php //include("f4p-1year-bonus-pam.html");?>
+<?php include("f4p-1year-bonus-cards.html");?>
 <div class="container oto-width">
 	<h2 class="darkRed text-center">You Will Also Get FOUR More Sets Of All Of Your Original FREE Bonuses When You Take Advantage Of This Special Upgrade Offer! </h2>
 	<p>Add to your own stockpile, keep at multiple locations or give to friends and loved ones so they can experience the same peace of mind. Your FREE bonuses include:</p>
--- checkout/oto/f4p-1year-kit.php
+++ checkout/oto/f4p-1year-kit.php
@@ -21,22 +21,22 @@ include_once ('template-header.php'); /*Add template-header-nav.php to add top m
 ?>
 <script src="/js/audio.js"></script>
 <script language="javascript">
-    $(document).ready(function() {
+	$(document).ready(function() {
 
 		$("#optin-form").validate({
-    		rules: {
-    			check1: {
+			rules: {
+				check1: {
 					required: true
-    			},
-  			},
-  			messages: { 
+				},
+			},
+			messages: {
 		  check1: '<div class="warning-check"></div>',
-        },
+		},
 		submitHandler: function(form) {
 		 //optIn(); 
 		 form.submit();
 		}
-    	});
+		});
 		
 	});
 </script>
@@ -70,40 +70,40 @@ include_once ('template-header.php'); /*Add template-header-nav.php to add top m
 	}
 </script>
 <div class="container-main">
-    <div class="breadcrumb1">
-        <a>CHECKOUT</a>
-        <a class="current">ORDER CUSTOMIZATION</a>
-        <a>ORDER CONFIRMATION</a>
-    </div>
+	<div class="breadcrumb1">
+		<a>CHECKOUT</a>
+		<a class="current">ORDER CUSTOMIZATION</a>
+		<a>ORDER CONFIRMATION</a>
+	</div>
 <div class="container oto-width">
 		<div><h1 class="darkRed text-center"><?php echo $firstName;?>, Act Now And Save An Additional $1000 (Today Only)</h1>
 		</div>
-        <div id="videobox" class="hidden-xs">
+		<div id="videobox" class="hidden-xs">
 			<iframe src="//fast.wistia.net/embed/iframe/p5l8v3n48p?videoFoam=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" width="640" height="352"></iframe><script src="//fast.wistia.net/assets/external/iframe-api-v1.js"></script>
 		</div>
-        <div style="margin-top:50px;">
+		<div style="margin-top:50px;">
 			<p><?php echo $firstName;?>, congratulations for making the great decision to get the 3-month Food4Patriots kit. </p>
-            <p>You&rsquo;ve taken an important step today to take charge, be more self-reliant and protect your family. I know you&rsquo;re going to sleep easier at night. The folks in our warehouse have reserved your order and they are already busy getting it ready to ship to you in <?php echo $shippingCity;?>.</p>
-          <p>But before you move on, I've got a <strong>special 1-time offer</strong> for you... </p>
-            <p>A lot of folks have told me that while they love having the 3-month kit on hand, they feel that it&rsquo;s simply not enough food… especially given the scary state of affairs in this country and the constant threat of natural disasters. In fact, we have quite a few folks telling us, &ldquo;I&rsquo;ll take everything you&rsquo;ve got!&rdquo;</p>
-            <img class="img-responsive center-block" src="/media/images/f4p//f4p-testimonials-10.png" alt="L. Graeser's Testimonial" style="margin-bottom:20px;">
-        </div>
-        <div>
+			<p>You&rsquo;ve taken an important step today to take charge, be more self-reliant and protect your family. I know you&rsquo;re going to sleep easier at night. The folks in our warehouse have reserved your order and they are already busy getting it ready to ship to you in <?php echo $shippingCity;?>.</p>
+		  <p>But before you move on, I've got a <strong>special 1-time offer</strong> for you... </p>
+			<p>A lot of folks have told me that while they love having the 3-month kit on hand, they feel that it&rsquo;s simply not enough food… especially given the scary state of affairs in this country and the constant threat of natural disasters. In fact, we have quite a few folks telling us, &ldquo;I&rsquo;ll take everything you&rsquo;ve got!&rdquo;</p>
+			<img class="img-responsive center-block" src="/media/images/f4p//f4p-testimonials-10.png" alt="L. Graeser's Testimonial" style="margin-bottom:20px;">
+		</div>
+		<div>
 			<p>I want to do everything I can to help you build your food stockpile as quickly and easily as possible, so to thank you for becoming a customer today, I am offering you an&nbsp;<strong>exclusive $1000.00 discount on a ONE YEAR Food4Patriots kit if you act now</strong>. But this special sale offer is ONLY for customers so if you&rsquo;re seeing this, then good news, you qualify!</p>
-            <p>Plus I'll throw in <strong>FREE Shipping and 27 FREE bonus gifts worth $470.00</strong> -- including 4 of the super-popular Lifestraw Personal Water Filters and over 22,000+ heirloom survival seeds -- just to make this a "no-brainer" for you!</p>
-            <p><?php echo $firstName;?>, would you like to accelerate your results by adding the 1 Year Food4Patriots Kit to your order at a 1-time discount sale price of $1,997? (That&rsquo;s a $1,000.00 discount with all the free goodies…you&rsquo;ll get 1 year&rsquo;s worth of food for just $1.11 per serving!)</p>
-            
-       	  <p class="text-center read-warning">Please read the rest of the page below and accept or decline the offer at the bottom of the page.</p>
+			<p>Plus I'll throw in <strong>FREE Shipping and 27 FREE bonus gifts worth $470.00</strong> -- including 4 of the super-popular Lifestraw Personal Water Filters and over 22,000+ heirloom survival seeds -- just to make this a "no-brainer" for you!</p>
+			<p><?php echo $firstName;?>, would you like to accelerate your results by adding the 1 Year Food4Patriots Kit to your order at a 1-time discount sale price of $1,997? (That&rsquo;s a $1,000.00 discount with all the free goodies…you&rsquo;ll get 1 year&rsquo;s worth of food for just $1.11 per serving!)</p>
+
+		  <p class="text-center read-warning">Please read the rest of the page below and accept or decline the offer at the bottom of the page.</p>
 	</div>
 </div>
 <?php include("f4p-1year-glenbeck.html");?>
 <?php include("f4p-1year-whatsincluded.html");?>
 <div class="container oto-width">
-    <h2 class="darkRed text-center">Get FREE Shipping & Handling!</h2>
-			  	
+	<h2 class="darkRed text-center">Get FREE Shipping & Handling!</h2>
+
 			<p><img src="/media/images/misc/free-shipping-burst-01.png" alt="FREE Shipping" width="181" height="104" class="pull-left">You&rsquo;ll get FREE Shipping &amp; Handling on your 1 Year Food4Patriots Kit when you upgrade today!</p>
-            <p>Because we're already going to be sending you a 3-month kit, we can add the 1Year kit to the shipping box and save on fulfillment costs. Sure, the fact is that it DOES cost more in postage to ship you a much heavier box, but it&rsquo;s still a lot more efficient than sending 2 separate shipments. Everybody loves FREE Shipping and I want to pass along the savings to YOU to make it even easier to upgrade.</p>
-                
+			<p>Because we're already going to be sending you a 3-month kit, we can add the 1Year kit to the shipping box and save on fulfillment costs. Sure, the fact is that it DOES cost more in postage to ship you a much heavier box, but it&rsquo;s still a lot more efficient than sending 2 separate shipments. Everybody loves FREE Shipping and I want to pass along the savings to YOU to make it even easier to upgrade.</p>
+
 	<h2 class="darkRed text-center">Check Out The Amazing FREE Bonuses<br>You Can ONLY Get With The 1 Year Kit!</h2>
 	
 	<p>You&rsquo;re going to get the &ldquo;mother lode&rdquo; of special bonuses  ONLY available with the 1 Year Kit!</p>
@@ -125,9 +125,9 @@ include_once ('template-header.php'); /*Add template-header-nav.php to add top m
 
 
 <div class="container oto-width">
-           
 
-            <h2 class="darkRed text-center">You Are 100% Protected By My Outrageous Double Guarantee.</h2>
+
+			<h2 class="darkRed text-center">You Are 100% Protected By My Outrageous Double Guarantee.</h2>
 			<div class="outLineBoxDarkBlue">
 				<p><img src="/media/images/misc/seal-guarantee-satisfaction.jpg" alt="Guarantee #1" width="250" height="189" class="pull-left img-responsive media">
 					<h3>Guarantee #1:</h3> This is a 100% money back guarantee. No questions asked. If for any reason, you&rsquo;re not 
@@ -136,7 +136,7 @@ include_once ('template-header.php'); /*Add template-header-nav.php to add top m
 					any reason or no reason whatsoever. So there&rsquo;s absolutely no risk for you. You literally can&rsquo;t lose!</p>
 				<div class="clearfix"></div>
 			</div>
-          
+
 			<div class="outLineBoxDarkBlue">
 				<p><img src="/media/images/misc/seal-guarantee-money.jpg" alt="Guarantee #2" width="250" height="192" class="pull-left img-responsive media">
 				<h3>Guarantee #2:</h3> This is an unheard of 300% money back guarantee. It&rsquo;s in addition to guarantee #1. If you open any 
@@ -148,12 +148,12 @@ include_once ('template-header.php'); /*Add template-header-nav.php to add top m
 					really worried about it, because I am so confident you&rsquo;re going to see the value in your Food4Patriots kit as soon 
 					as you have it in your hands.</p>
 			</div>
-            
+
 			<div><img class="img-responsive center-block" src="/media/images/f4p/f4p-testimonials-09.png" width="626" height="269" alt="Stacy's Testimonial"></div>
-            
-            <h2 class="darkRed title-max-600 center-block text-center"><?php echo $firstName;?>, Get On The ‘Fast Track’ - Claim Your 1 Year 
-            	Food4Patriots Kit For $1000.00 Off Right Now!</h2>
-			          	
+
+			<h2 class="darkRed title-max-600 center-block text-center"><?php echo $firstName;?>, Get On The ‘Fast Track’ - Claim Your 1 Year
+				Food4Patriots Kit For $1000.00 Off Right Now!</h2>
+
 			<p>Now I understand that the 1 Year Food4Patriots kit is the right choice for folks who are looking for the ultimate in food security. This kit plus all the bonuses is valued at over $3,000, but because you&rsquo;ve already taken the 1st step, and because I appreciate you putting your trust in us by being a customer, you can get your 1 Year Food4Patriots kit today for just $1,997.</p>
 				
 			<p>You get a year&rsquo;s worth of delicious survival food for $1.11 per serving!</p>
@@ -173,11 +173,11 @@ include_once ('template-header.php'); /*Add template-header-nav.php to add top m
 				Food4Patriots kit at $1000.00 off, click the big orange &ldquo;Click Here To Accept&rdquo; button below.</p>
 
 			<div>
-                <div><img class="img-responsive center-block" src="/media/images/f4p/f4p-1-year-kit-01.jpg"  alt="Food4Patriots 1 Year Kit"/></div>
-                <div><img class="img-responsive center-block" src="/media/images/f4p/value-chart-3.jpg" alt="Value Chart"/></div>
-                <div class="text-center"><h2 id="save" class="darkRed">Act Today And Save Over $1000</h2></div>
-              
-                <a id="accept"></a>
+				<div><img class="img-responsive center-block" src="/media/images/f4p/f4p-1-year-kit-01.jpg"  alt="Food4Patriots 1 Year Kit"/></div>
+				<div><img class="img-responsive center-block" src="/media/images/f4p/value-chart-3.jpg" alt="Value Chart"/></div>
+				<div class="text-center"><h2 id="save" class="darkRed">Act Today And Save Over $1000</h2></div>
+
+				<a id="accept"></a>
 				<?php
 				if($isUpgrade) {
 					?>
@@ -187,47 +187,47 @@ include_once ('template-header.php'); /*Add template-header-nav.php to add top m
 					<?php
 				} else {
 				?>
-                <form action="/checkout/process.php" method="post" accept-charset="utf-8" id="optin-form">
-                    <div style="text-align:center;">
-                        <input type="image" src="/assets/images/buttons/btn-orange-click-accept-02.jpg" name="submit" class="img-responsive center-block" onClick="ga('send', 'event', 'upsell-2-f4p-1-yr-kit', 'f4p-1-yr-kit-accept', 'click-to-accept-bottom');"/>
+				<form action="/checkout/process.php" method="post" accept-charset="utf-8" id="optin-form">
+					<div style="text-align:center;">
+						<input type="image" src="/assets/images/buttons/btn-orange-click-accept-02.jpg" name="submit" class="img-responsive center-block" onClick="ga('send', 'event', 'upsell-2-f4p-1-yr-kit', 'f4p-1-yr-kit-accept', 'click-to-accept-bottom');"/>
 					</div>
-                    
-                    <div>
-                        <table  style="margin-right:auto;margin-left:auto;" border="0" cellspacing="0" cellpadding="0">
-                            <tr>
-                                <td><div style="text-align: left;margin-right:10px;"><strong>Quantity:</strong></div></td>
-                                <td><span style="text-align:center;padding-top:10px;padding-bottom:10px;">
-                                <select name="quantity" id="quantity" style="width:50px;margin-top:3px;margin-bottom:3px;" onchange="productChange();">
-                                  <?php
-                                for ($i=1; $i<=5; $i++)
-                                  {
-                                  echo "<option>". $i . "</option>";
-                                      }
-                                 ?>
-                                </select>
-                                </span></td>
-                            </tr>
-                        </table>
-                    </div>
-                  
-                    <div style="position:relative;text-align:left;margin-top:10px;max-width:600px;margin-right:auto;margin-left:auto;">
-                        <div style="float:left;margin-right:5px;">
-                            <input type="checkbox" id="check1" name="check1">
-                            <img src="/assets/images/misc/yes-01.jpg" width="74" height="34" alt="Yes">
-                        </div>
-                        <div id="terms">I want to add the 1 Year Food4Patriots Kit to my order at the 1-time discount sale price of $1,997. 
-                            I will get FREE Shipping and 27 FREE Bonus Gifts including 4 of the super-popular Lifestraw Personal Water Filters 
-                            and over 22,000+ heirloom survival seeds
-                        </div>
-                    </div>
-                    <div style="margin-top:20px;">
-                        <img class="img-responsive center-block"  src="/media/images/f4p/f4p-testimonials-07.png" alt="Food4Patriots Testimonial">
-                    </div>
-                    <div>
-                        <img class="img-responsive center-block" src="/media/images/f4p/f4p-testimonials-02.png" alt="Food4Patriots Testimonial">
-                    </div>
-                    <div class="text-center" style="margin-top:20px;"><strong>OR</strong></div>
-                </form>
+
+					<div>
+						<table  style="margin-right:auto;margin-left:auto;" border="0" cellspacing="0" cellpadding="0">
+							<tr>
+								<td><div style="text-align: left;margin-right:10px;"><strong>Quantity:</strong></div></td>
+								<td><span style="text-align:center;padding-top:10px;padding-bottom:10px;">
+								<select name="quantity" id="quantity" style="width:50px;margin-top:3px;margin-bottom:3px;" onchange="productChange();">
+								  <?php
+								for ($i=1; $i<=5; $i++)
+								  {
+								  echo "<option>". $i . "</option>";
+									  }
+								 ?>
+								</select>
+								</span></td>
+							</tr>
+						</table>
+					</div>
+
+					<div style="position:relative;text-align:left;margin-top:10px;max-width:600px;margin-right:auto;margin-left:auto;">
+						<div style="float:left;margin-right:5px;">
+							<input type="checkbox" id="check1" name="check1">
+							<img src="/assets/images/misc/yes-01.jpg" width="74" height="34" alt="Yes">
+						</div>
+						<div id="terms">I want to add the 1 Year Food4Patriots Kit to my order at the 1-time discount sale price of $1,997.
+							I will get FREE Shipping and 27 FREE Bonus Gifts including 4 of the super-popular Lifestraw Personal Water Filters
+							and over 22,000+ heirloom survival seeds
+						</div>
+					</div>
+					<div style="margin-top:20px;">
+						<img class="img-responsive center-block"  src="/media/images/f4p/f4p-testimonials-07.png" alt="Food4Patriots Testimonial">
+					</div>
+					<div>
+						<img class="img-responsive center-block" src="/media/images/f4p/f4p-testimonials-02.png" alt="Food4Patriots Testimonial">
+					</div>
+					<div class="text-center" style="margin-top:20px;"><strong>OR</strong></div>
+				</form>
 					<?php
 					}
 					?>
