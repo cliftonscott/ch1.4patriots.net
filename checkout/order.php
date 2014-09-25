9915e0e8e4834a355d7fa2975bb8abe2b255f12d
jeremy <jeremy@rebootmarketing.com>
2014-09-25 11:41:41 -0500

    F4P-148 Adjusted Title

--- checkout/order.php
+++ checkout/order.php
@@ -25,10 +25,10 @@ if($_SESSION['upgrade'] == TRUE) {
 	$_SESSION["upgrade"] = FALSE;
 	//use only values available in the Product.php file OR use only generic text, non-product specific
 ?>
-					<p class="text-success h3">
+					<p class="text-success h3 title-max-400 center-block">
 						<?php echo $productDataObj->metaTitle; ?>
 					</p>
-					<p>
+					<p class="hidden">
 						<?php echo $productDataObj->metaDescription; ?>
 					</p>
 					<?php
@@ -43,10 +43,10 @@ if($_SESSION['upgrade'] == TRUE) {
 } else {
 ?>
 					<h2 class="text-danger">There was a problem processing your order for the special offer:</h2>
-					<p class="text-success h3">
+					<p class="text-success h3 title-max-400 center-block">
 						<?php echo $productDataObj->metaTitle; ?>
 					</p>
-					<p>
+					<p class="hidden">
 						<?php echo $productDataObj->metaDescription; ?>
 					</p>
 					<p><u><strong>Don't worry!</strong></u><strong> If you have another method for payment, you can enter it and still receive the special offer!</strong></p>
