73fec586986c6fab9ad9cdd905cef6594df4c1ac
Brian Gibbins <brian@briangibbins.com>
2014-09-30 11:48:29 -0400

    FFP-148 update path for images

--- templates/products/F4P-bonus-manuals.php
+++ templates/products/F4P-bonus-manuals.php
@@ -3,22 +3,22 @@
 		<p>Here are all your Food4Patriots bonus manuals. </p>
 	<div>
 		<div class="row">
-			<div class="col-sm-12 col-md-4 text-center"><a target="_blank" href="/media/pdf/F4P-10-Items-After-Crisis.pdf"><img height="231" src="/media/images/f4p/f4p-eBook-10-items-after-crisis.jpg" alt="" /></a></div>
+			<div class="col-sm-12 col-md-4 text-center"><a target="_blank" href="/media/pdf/F4P-10-Items-After-Crisis.pdf"><img height="231" src="/media/images/f4p/F4P-eBook-10-items-after-crisis.jpg" alt="" /></a></div>
 			<div class="col-sm-12 col-md-8 margin-tb-50"><a target="_blank" href="/media/pdf/F4P-10-Items-After-Crisis.pdf">Right Click Here for Top 10 Items Sold Out After Crisis</a><br />
 				This is manual shows you everything you need to stock to survive a disaster.</div>
 		</div>
 		<div class="row">
-			<div class="col-sm-12 col-md-4 text-center"><a target="_blank" href="/media/pdf/F4P-Cut-Grocery-Bills-Half.pdf"><img src="/media/images/f4p/f4p-eBook-cut-grocery-bills-half.jpg" alt=""></a></div>
+			<div class="col-sm-12 col-md-4 text-center"><a target="_blank" href="/media/pdf/F4P-Cut-Grocery-Bills-Half.pdf"><img src="/media/images/f4p/F4P-eBook-cut-grocery-bills-half.jpg" alt=""></a></div>
 			<div class="col-sm-12 col-md-8 margin-tb-50"><a target="_blank" href="/media/pdf/F4P-Cut-Grocery-Bills-Half.pdf">Right Click here for How To Cut Your Grocery Bills In Half Guide</a><br />
 				Here's the step-by-step manual showing you how to save on your grocery bill.</div>
 		</div>
 		<div class="row">
-			<div class="col-sm-12 col-md-4 text-center"><a target="_blank" href="/media/pdf/F4P-10-Items-After-Crisis.pdf"><img height="231" src="/media/images/f4p/f4p-eBook-10-items-after-crisis.jpg" alt="" /></a></div>
+			<div class="col-sm-12 col-md-4 text-center"><a target="_blank" href="/media/pdf/F4P-10-Items-After-Crisis.pdf"><img height="231" src="/media/images/f4p/F4P-eBook-10-items-after-crisis.jpg" alt="" /></a></div>
 			<div class="col-sm-12 col-md-8 margin-tb-50"><a target="_blank" href="/media/pdf/F4P-Survival-Garden-Guide.pdf">Right Click Here For The Survival Garden Guide</a><br />
 				This is the manual showing you the critical steps necessary to plant and grow your survival garden.</div>
 		</div>
 		<div class="row">
-			<div class="col-sm-12 col-md-4 text-center"><a target="_blank" href="/media/pdf/F4P-Water-Survival-Guide.pdf"><img src="/media/images/f4p/f4p-eBook-the-water-survival-guide.jpg" alt=""></a></div>
+			<div class="col-sm-12 col-md-4 text-center"><a target="_blank" href="/media/pdf/F4P-Water-Survival-Guide.pdf"><img src="/media/images/f4p/F4P-eBook-the-water-survival-guide.jpg" alt=""></a></div>
 			<div class="col-sm-12 col-md-8 margin-tb-50"><a target="_blank" href="/media/pdf/F4P-Water-Survival-Guide.pdf"> Right Click Here For Water Survival Manual</a><br />
 				This is the manual showing you the critical steps necessary to survive a water crisis.</div>
 		</div>
