5f7781d1099f4737569735d979b10cbe0d30b496
Brian Gibbins <brian@briangibbins.com>
2014-10-05 09:19:00 -0400

    FFP-267 change order of session start and ssl check

--- includes/AUTO-PREPEND-PROD.php
+++ includes/AUTO-PREPEND-PROD.php
@@ -6,12 +6,15 @@
  * The downside is that changing this file can take the site down. So you have to take as much care changing this
  * file as you would say an apache config file or a php.ini file.
  */
+
+$some_session = session_name("f4p_session");
+session_start();
+
 if((!isset($_SERVER['HTTPS']) || !$_SERVER['HTTPS']) && (strpos($_SERVER["PHP_SELF"],"/checkout/") !== FALSE)) {
 	header("Location: https://secure.food4patriots.com" . $_SERVER["REQUEST_URI"]);
 	exit();
 }
-$some_session = session_name("f4p_session");
-session_start();
+
 ini_set("display_errors", 0);
 const APP_MODE_LOG = FALSE; //options FALSE or DEBUG
 $addPath[] = "/var/www/vhosts/secure.food4patriots.com/includes";
