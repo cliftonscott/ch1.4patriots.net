3b6586d6c691177bc72106a4743132a290168e64
Brian Gibbins <brian@briangibbins.com>
2014-10-06 23:26:53 -0500

    FFP-273 add queryString property from Analytics.php to footer links

--- templates/footer-links.php
+++ templates/footer-links.php
@@ -12,12 +12,12 @@ if(in_array($_SERVER["PHP_SELF"], $targetNewArray)) {
 }
 ?>
 <div>
-	<a href="/privacy.php" class="bottomlinks" target="<?php echo $footerLinkTarget;?>">Privacy Policy</a> | 
-	<a href="/terms.php" class="bottomlinks" target="<?php echo $footerLinkTarget;?>">Terms &amp; Conditions</a> | 
-	<a href="/disclaimer.php" class="bottomlinks" target="<?php echo $footerLinkTarget;?>">Disclaimer</a> | 
-	<a href="/returns.php" class="bottomlinks" target="<?php echo $footerLinkTarget;?>">Returns</a> | 
-	<a href="/faq.php" class="bottomlinks" target="<?php echo $footerLinkTarget;?>">FAQ</a><!-- |
-	<a href="/newsroom.php" class="bottomlinks" target="<?php echo $footerLinkTarget;?>">Newsroom</a> --> | 
-	<a href="/testimonials.php" class="bottomlinks" target="<?php echo $footerLinkTarget;?>">Testimonials</a> | 
-	<a href="/contact.php" class="bottomlinks" target="<?php echo $footerLinkTarget;?>">Contact Us</a>
+	<a href="/privacy.php<?php echo $analyticsObj->queryString;?>" class="bottomlinks" target="<?php echo $footerLinkTarget;?>">Privacy Policy</a> |
+	<a href="/terms.php<?php echo $analyticsObj->queryString;?>" class="bottomlinks" target="<?php echo $footerLinkTarget;?>">Terms &amp; Conditions</a> |
+	<a href="/disclaimer.php<?php echo $analyticsObj->queryString;?>" class="bottomlinks" target="<?php echo $footerLinkTarget;?>">Disclaimer</a> |
+	<a href="/returns.php<?php echo $analyticsObj->queryString;?>" class="bottomlinks" target="<?php echo $footerLinkTarget;?>">Returns</a> |
+	<a href="/faq.php<?php echo $analyticsObj->queryString;?>" class="bottomlinks" target="<?php echo $footerLinkTarget;?>">FAQ</a><!-- |
+	<a href="/newsroom.php<?php echo $analyticsObj->queryString;?>" class="bottomlinks" target="<?php echo $footerLinkTarget;?>">Newsroom</a> --> |
+	<a href="/testimonials.php<?php echo $analyticsObj->queryString;?>" class="bottomlinks" target="<?php echo $footerLinkTarget;?>">Testimonials</a> |
+	<a href="/contact.php<?php echo $analyticsObj->queryString;?>" class="bottomlinks" target="<?php echo $footerLinkTarget;?>">Contact Us</a>
 </div>
