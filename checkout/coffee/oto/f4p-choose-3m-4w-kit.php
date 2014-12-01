bb7fd4248561ba665527d1e43fd670441c21b3e7
Brian Gibbins <brian@briangibbins.com>
2014-12-01 10:22:40 -0500

    FFP-347 add new php template to coffee funnel

---
+++ checkout/coffee/oto/f4p-choose-3m-4w-kit.php
@@ -0,0 +1,23 @@
+<?php
+if($_GET["upgrade"] == 1 ) {
+	$isUpgrade = TRUE;
+}
+if(($isUpgrade !== TRUE) && (!empty($_SESSION["customerDataArray"]["firstName"]))) {
+	$firstName = $_SESSION["customerDataArray"]["firstName"];
+	$_SESSION['upsell'] = TRUE; //must stay a boolean
+} else {
+	$firstName = "Fellow Patriot";
+}
+
+$billingStateName = $_SESSION["customerDataArray"]["billingStateName"];
+// SET PRODUCT ID
+//$_SESSION['productId'] = 164; //please keep as an integer
+//$_SESSION['quantity'] = '1';
+$_SESSION['upsell'] = TRUE; //must stay a boolean
+$_SESSION['pageReturn'] = '/checkout/order.php';
+include_once("Product.php");
+include_once("template-top.php");
+include_once('template-header.php'); /*Add template-header-nav.php to add top menu*/
+include_once("products/offers/f4p-choose-3m-4w-kit.php");
+include_once("template-bottom.php");
+?>
