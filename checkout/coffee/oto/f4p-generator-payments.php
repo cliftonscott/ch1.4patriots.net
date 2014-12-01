8cf88d1b647b606873a8049867b42b4a7c9cffe5
Brian Gibbins <brian@briangibbins.com>
2014-12-01 12:08:39 -0500

    FFP-353 new php template for coffee offer

---
+++ checkout/coffee/oto/f4p-generator-payments.php
@@ -0,0 +1,27 @@
+<?php
+if($_GET["upgrade"] == 1 ) {
+	$isUpgrade = TRUE;
+}
+if(($isUpgrade !== TRUE) && (!empty($_SESSION["customerDataArray"]["firstName"]))) {
+	$firstName = $_SESSION["customerDataArray"]["firstName"];
+	$_SESSION['upsell'] = TRUE; //must stay a boolean
+} else {
+	$firstName = "Fellow Patriot";
+}
+// SET PRODUCT ID
+$_SESSION['productId'] = 174; //please keep as an integer
+$_SESSION['quantity'] = '1';
+$_SESSION['upsell'] = TRUE; //must stay a boolean
+$_SESSION['pageReturn'] = '/checkout/order.php';
+include_once("Product.php");
+$productDataObj = Product::getProduct($_SESSION["productId"]);
+include_once("Inventory.php");
+$inventory = Inventory::hasInventory(162);
+if($inventory->success !== true) {
+	header("Location: " . $productDataObj->soldOutPage);
+	exit;
+}
+include_once("template-top.php");
+include_once('template-header.php'); /*Add template-header-nav.php to add top menu*/
+include_once("products/offers/f4p-generator-payments.php");
+include_once("template-bottom.php");
