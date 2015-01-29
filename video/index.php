4178ee0ea6e104d42df4f0ed2ceab57cb87f5998
jeremy <jeremy@rebootmarketing.com>
2015-01-29 14:07:50 -0600

    FFP-490 Setup split test - control vs Glenn, testimonials, and guarantees

--- video/index.php
+++ video/index.php
@@ -12,6 +12,7 @@ $variantsArray = array (
 	"np-nologo", // No exit pop, and no logos/badges
 	"np", // No exit pop
 	"pu", // Pop Under
+	"b", // Testimonials - FAQ - Guarantee
 );
 if($_GET["v"]) {
 	if(in_array(trim($_GET["v"]),$variantsArray)) {
@@ -39,6 +40,7 @@ $platform->setCsrModalButtons("sample,video,letter");
 if (isMobile()) {
 	document.location = "<?php echo $productDataObj->mobileLink ?>"; }
 </script>
+<script src="/js/audio.js"></script>
 <script src="/js/jquery.timers-1.2.js" type="text/javascript"></script>
 <script src="/js/jcookie.js" type="text/javascript"></script>
 <script>
@@ -131,7 +133,7 @@ if (isMobile()) {
 						echo "<div style='font-size:18pt;'>Special presentation for fans of Glenn Beck and TheBlaze...</div>";
 					}
 					?>
-					<h1><strong>Why Has This Video Been Banned?</strong></h1></div>
+					<h1><strong>Why Was This Video Banned?</strong></h1></div>
 			</div>
 			<div class="col-md-12">
 				<!-- Button Stuff -->
@@ -189,6 +191,59 @@ if (isMobile()) {
 			</div>
 		</div>
 	</div>
+	<?php
+	if($variation == "b") { ?>
+		<!--Testimonials-->
+		<div style="margin: 0 auto 0 auto;"></div>
+		<div id="testimonials-checkout" class="center-block" style="max-width:95%;">
+			<div class="section-header">
+				<div class="section-header-bg">
+					<h4><i class="fa fa-comments"></i>  What People Are Saying About Food4Patriots</h4>
+				</div>
+			</div>
+			<div class="row" style="padding-bottom: 0;background-color: #f7f7f7;">
+				<div class="col-xs-12 col-sm-7">
+					<div style="text-align: center;font-size: 31px;">
+						"Patriot Pantry meals: they make the cut because they are delicious, nutritious and good for 25 years."<br>- Glenn Beck <audio id="beckCheckoutAudioSrc" src="/media/audio/f4p-beck-testimonial-01.mp3" preload="auto"></audio><img id="beckCheckoutAudioControl" class="audioControl" src="/assets/images/misc/speaker_off.gif" onclick="toggleAudio('beckCheckout');">
+						<div class="video-ref-test" style="padding-bottom: 0;">Glenn Beck (host of The Glenn Beck Show on radio, TV, and frequent guest on FoxNews), recommends Patriot Pantry, the survival food in Food4patriots Kits</div>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-5"><img src="/media/images/f4p/f4p-glenn-beck-01.jpg" class="img-responsive"></div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-3"><img src="/media/images/misc/img-testimonial-diana.jpg" class="img-responsive center-block"></div>
+				<div class="col-xs-12 col-sm-9"><i>&quot;I am a new customer and heard about your company from my brother. Praises to your customer service heroes! Their patience, kindness and care with my calls were like talking with my best friend. Thank you!&quot;</i><strong> - Diana L.</strong> </div>
+			</div>
+			<div class="row" style="background-color: #f7f7f7;">
+				<div class="col-xs-12 col-sm-3"><img src="/media/images/misc/img-testimonial-rolf.jpg" class="img-responsive center-block"></div>
+				<div class="col-xs-12 col-sm-9"><i>&quot;Hello Frank – this is Rolf K. and I am one of your very happy customers. The most important thing is your food tastes great. I’ve tasted 2 out of the 4 entrées in one of the small packages and I now have a tub and three other small packages set away…and am feeling pretty good about it. Keep up the good work!&quot;</i><strong> - Rolf K.</strong></div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-3"><img src="/media/images/misc/img-testimonial-lynn.jpg" class="img-responsive center-block"></div>
+				<div class="col-xs-12 col-sm-9"><i>&quot;My purchase of Food4Patriots has indeed brought me peace of mind…I particularly loved the choices in the different price and size ranges for the Food4Patriots product. The fact that it has a shelf life of 25 years, if it is properly stored, is an added plus! I intend to make a second purchase in the near future, as my budget allows because it just makes sense.&quot;</i><strong> - Lynn G.</strong></div>
+			</div>
+			<div class="row" style="background-color: #f7f7f7;">
+				<div class="col-xs-12 col-sm-3"><img src="/media/images/misc/img-testimonial-jeff.jpg" class="img-responsive center-block"></div>
+				<div class="col-xs-12 col-sm-9"><i>&quot;All our orders were received in perfect condition and in containers that make storage and inventory easy to access when needed. Very organized with labeling, highly recommend ordering your product for friends and family and anyone who wants to look ahead and be prepared for the unexpected. Thanks!&quot;</i><strong> - Jeff W.</strong></div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-2"></div>
+				<div class="col-xs-12 col-sm-8 text-center"><a href="/testimonials.php" target="_blank">Click here</a> to see what others are saying about Food4Patriots, including audio and video, on our testimonials page.</div>
+				<div class="col-xs-12 col-sm-2"></div>
+			</div>
+		</div>
+		<!--FAQ-->
+		<hr>
+		<div style="margin: 30px auto 0 auto;max-width:95%;">
+			<h4 class="darkRed">Frequently Asked Questions:</h4>
+			<?php include_once ('snippets/faq-accordian-1wk.html'); ?>
+		</div>
+		<!--Guarantee-->
+		<hr>
+		<?php include_once ('snippets/f4p-guarantees-checkout.html'); ?>
+		<hr>
+		<div class="video-ref-test"><p>LEGAL DISCLAIMER: Although not guaranteed, every effort has been made to accurately represent our products and their potential. In accordance with the latest FTC guidelines, we want to make it explicitly clear that the testimonials and customer letters we have received are exceptional results, don't apply to the average purchaser and are not intended to represent or guarantee that anyone will achieve the same or similar results. The generally expected performance of our products in regards to any specific application has not been scientifically validated and we cannot and will not make any promises in regards to your specific results. Furthermore, all testimonials are real testimonials from real customers though the images have been modified to protect their privacy.</p></div>
+	<?php } ?>
 </div>
 <!-- Header Food Images -->
 <div id="productModal" class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
