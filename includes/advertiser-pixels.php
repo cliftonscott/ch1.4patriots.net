713072df352e38a114abf67b9f6ed0ed8ea9161d
jeremy <jeremy@rebootmarketing.com>
2015-02-18 12:44:59 -0600

    FFP-501 Update DivisionD Conversion Pixel

--- includes/advertiser-pixels.php
+++ includes/advertiser-pixels.php
@@ -6,7 +6,16 @@ $adConversionNewArray = array (
 );
 if(in_array($_SERVER["PHP_SELF"], $adConversionNewArray)) { ?>
 <!-- Start Advertiser Code -->
-<img src="https://ss1.zedo.com/ads2/t?o=562185;h=1768615;z=<?php echo rand(); ?>" width="1" height="1" BORDER="0">//DivD Conversion
+<!-- Begin ZEDO -->
+<script language="JavaScript" type="text/javascript">
+	var zzp=new Image();
+	if (location.protocol == "https:") {
+		zzp.src="https://ss1.zedo.com/ads2/t?o=562185;h=1768615;z="+Math.random();
+	} else {
+		zzp.src="http://xads.zedo.com/ads2/t?o=562185;h=1768615;z="+Math.random();
+	}
+</script>
+<!-- End ZEDO -->//DivD Conversion
 <img src="https://ss7.zedo.com/img/bh.gif?n=305&g=20&a=1488&s=1&l=1&t=e&f=1" width="1" height="1" border="0" > //DivD Opt Out
 <!-- End Advertiser Code -->
 <?php } ?>
