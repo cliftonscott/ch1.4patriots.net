6d3371d31167fb0360c87e0ea00ca8adaddc2864
Brian Gibbins <brian@briangibbins.com>
2015-04-16 16:22:03 -0400

    FFP-548 changes format of log

--- 4/tests/devlog.php
+++ 4/tests/devlog.php
@@ -1,62 +1,8 @@
 <?php
-session_start();
-ini_set("display_errors", TRUE);
-//ini_set("memory_limit", 28M);
-require_once("Dblog.php");
-$devlogs = Dblog::getDblog();
-
-//Util::nicedump($devlogs);
-
-?>
-<style>
-	table tr td {
-		font-family:Courier;
-		padding-bottom:5px;
-		border-bottom:1px solid ##000000;
-		font-size:.8em;
-		vertical-align: top;
-	}
-	table tr td.process {
-		font-style:italic;
-		border-right:1px solid #cccccc;
-		padding-right:10px;
-		color:green;
-	}	
-	table tr td.stamp {
-		font-size:.8em;
-		font-style:italic;
-		border-right:1px solid #cccccc;
-		padding-right:10px;
-	}
-	table tr td.serverId {
-		font-size:.8em;
-		font-style:italic;
-		border-right:1px solid #cccccc;
-		padding-right:10px;
-	}
-	table tr td.visitorId {
-		font-size:.8em;
-		font-style:italic;
-		border-right:1px solid #cccccc;
-		padding-right:10px;
-	}
-</style>
-
-
-<?php
-
-echo ("<table>\n");
-foreach($devlogs->results as $devlog) {
-	echo "<tr>\n";
-//	echo "<td>" . $devlog["logkey"] . "</td>\n";
-	echo "<td class='visitorId'>" . $devlog["visitorId"] . "</td>\n";
-	echo "<td class='serverId'>" . $devlog["serverId"] . "</td>\n";
-	echo "<td class='stamp'>" . date("m-d h:i:s",strtotime($devlog["stamp"])) . "</td>\n";
-	echo "<td class='process'>" . $devlog["process"] . "</td>\n";
-//	echo "<td class='process'>" . $devlog["userIp"] . "</td>\n";
-	echo "<td>" . $devlog["log"] . "</td>\n";
-	echo "</tr>\n";
+include_once("Dblog.php");
+$dblog = new Dblog();
+if(!empty($_GET["v"])) {
+	$dblog->showDblog($_GET["v"]);
+} else {
+	$dblog->showDblog();
 }
-echo "</table>";
-
-echo "<br>===end===";
