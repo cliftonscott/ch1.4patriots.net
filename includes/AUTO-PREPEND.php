a9ef6c62e19f7c0f8c459f64c6739e3bdb6813a0
Brian Gibbins <brian@briangibbins.com>
2015-04-20 08:42:16 -0400

    DEVO-419 update the method to determine dev/stage/production

--- includes/AUTO-PREPEND.php
+++ includes/AUTO-PREPEND.php
@@ -6,21 +6,24 @@
  * The downside is that changing this file can take the site down. So you have to take as much care changing this
  * file as you would say an apache config file or a php.ini file.
  */
-
 $some_session = session_name("f4p_session");
 session_start();
-
-if((!isset($_SERVER['HTTPS']) || !$_SERVER['HTTPS']) && (strpos($_SERVER["PHP_SELF"],"/checkout/") !== FALSE)) {
-	header("Location: https://secure.food4patriots.com" . $_SERVER["REQUEST_URI"]);
-	exit();
+$host = $_SERVER['HTTP_HOST'];
+if (strpos($host, 'dev.') === 0) {
+	putenv("APP_ENV=dev");
+} else if (strpos($host, 'stage.') === 0) {
+	putenv("APP_ENV=stage");
+} else {
+	putenv("APP_ENV=production");
 }
-
-ini_set("display_errors", 0);
+unset($host);
+//ini_set("error_reporting", E_ALL);
+ini_set("display_errors", FALSE);
 const APP_MODE_LOG = FALSE; //options FALSE or DEBUG
-$addPath[] = "/var/www/vhosts/secure.food4patriots.com/includes";
-$addPath[] = "/var/www/vhosts/secure.food4patriots.com/templates";
-$addPath[] = "/var/www/vhosts/secure.food4patriots.com/content";
-$addPath[] = "/var/www/vhosts/secure.food4patriots.com/libraries";
+$addPath[] = $_SERVER["DOCUMENT_ROOT"] . "/includes";
+$addPath[] = $_SERVER["DOCUMENT_ROOT"] . "/templates";
+$addPath[] = $_SERVER["DOCUMENT_ROOT"] . "/content";
+$addPath[] = $_SERVER["DOCUMENT_ROOT"] . "/libraries";
 $appConfig["path"] = implode(PATH_SEPARATOR, $addPath);
 set_include_path(get_include_path() . PATH_SEPARATOR . $appConfig["path"]);
 unset($addPath);
