bc2e43dece8ab5c0c944224d19b885914565aba7
jeremy <jeremy@rebootmarketing.com>
2015-05-18 12:40:09 -0500

    FFP-617 Protein TYP Copy

--- checkout/protein/thankyou.php
+++ checkout/protein/thankyou.php
@@ -3,45 +3,35 @@ $_SESSION["upsell"] = TRUE; //must stay a boolean
 include_once("template-top.php");
 include_once ('template-header.php'); /*Add template-header-nav.php to add top menu*/
 ?>
-<script type="text/javascript">
-	var _vis_opt_revenue = "<?php echo $_SESSION['vwoRevenue'];?>";
-	window._vis_opt_queue = window._vis_opt_queue || [];
-	window._vis_opt_queue.push(function() {_vis_opt_revenue_conversion(_vis_opt_revenue);});
-</script>
+
 <script src="/js/audio.js"></script>
 <div class="container-main">
 	<div class="breadcrumb2">
-	    <a>CHECKOUT</a>
-	    <a class="breadcrumb-center">ORDER CUSTOMIZATION</a>
-	    <a class="current">ORDER CONFIRMATION</a>
+		<a>CHECKOUT</a>
+		<a class="breadcrumb-center">ORDER CUSTOMIZATION</a>
+		<a class="current">ORDER CONFIRMATION</a>
 	</div>
-    <div class="container oto-width">
-    	<div class="johnson-box-02 center-block margin-tb-20">
+	<div class="container oto-width">
+		<div class="johnson-box-02 center-block margin-tb-20">
 			<p><u>Your Order Has Been Received!</u></p>
 			<p>We're Preparing Your Shipment Now.<br>
-		    It Should Arrive In 7-14 Days.</p>
+			It Should Arrive In 7-14 Days.</p>
 		</div>
 		<div class="text-center margin-tb-20">
 			<h1><strong>Thank You For Your Order!</strong></h1>
 		</div>
-        <div class="margin-tb-20">
-        	<p class="read-warning text-center "><strong>NOTE:</strong> Your credit card statement will show a charge
-				from Food4Patriots.com</p>
-        </div>
-        <div class="margin-tb-20">
-			<?php include("snippets/frank-thankyou-msg-coffee.html");?>
-        </div>
-        
+		<div class="margin-tb-20">
+			<p class="read-warning text-center "><strong>NOTE:</strong> Your credit card statement will show a charge from Food4Patriots.com</p>
+		</div>
+		<div class="margin-tb-20">
+			<?php include("snippets/frank-thankyou-msg-protein.html");?>
+		</div>
+
 		<div class="margin-tb-20">
 			<?php
-			$testimonials = array("thomas", "todd", "rich",);
+			$testimonials = array("aeron", "michael", "rick",);
 			foreach ($testimonials as $count => $testimonial) {
 				echo "<div class='testimonial'>";
-				if($count % 2 == 0) {
-					echo "<i class='fa fa-quote-left fa-3x pull-left'></i>";
-				} else {
-					echo "<i class='fa fa-quote-right fa-3x pull-right'></i>";
-				}
 				include("testimonials/" . $testimonial . ".html");
 				echo "</div>";
 			}
@@ -49,11 +39,11 @@ include_once ('template-header.php'); /*Add template-header-nav.php to add top m
 		</div>
 
 		<div class="margin-tb-20">
-		<?php include_once("products/F4P-fruitveggiesnack-pid128.php");?>
+		<?php /*include_once("products/F4P-fruitveggiesnack-pid128.php");*/?>
 		</div>
 
-        <div class="margin-tb-20 text-center">
-        	<h2 class="darkRed">Customer Service Contact Info</h2>
+		<div class="margin-tb-20 text-center">
+			<h2 class="darkRed">Customer Service Contact Info</h2>
 	
 			<p>If you have a problem or question feel free to call 1-800-728-0008<br> or email us at
 				<script type="text/javascript">
@@ -63,8 +53,8 @@ include_once ('template-header.php'); /*Add template-header-nav.php to add top m
 							+ emailE + '</a>'
 					)
 				</script></p>
-        </div>
-        <div class="margin-tb-20">
+		</div>
+		<div class="margin-tb-20">
 			<?php
 			if($_SESSION["orders"]) {
 				echo "<div class='rcBoxR10'>\n";
@@ -93,7 +83,7 @@ include_once ('template-header.php'); /*Add template-header-nav.php to add top m
 			?>
 		</div>
 
-    </div>
+	</div>
 	
 </div>
 <?php
