ada0980da3b4cd4cd320545444e5bf96d4f674c5
jeremy <jeremy@rebootmarketing.com>
2015-05-19 15:48:42 -0500

    FFP-615 Created Sales Page

--- letter/protein/content.php
+++ letter/protein/content.php
@@ -1,36 +1,94 @@
 <div class="container oto-width">
-	<div><h1 class="darkRed text-center">Get The Brand-New 1-Year Food4Patriots Kit For $200.00 Off Plus FREE Shipping &amp; 27 FREE Bonus Gifts!</h1></div>
-	<div class="text-center">
-		<img class="img-responsive center-block" src="/media/images/f4p/f4p-1-year-kit-01.jpg" alt="Food4Patriots 1 Year Kit" style="margin-bottom:20px;">
-		<span class="caption"><i>Save $200.00 when you order a 1 Year Kit,  plus get FREE Shipping, 4 FREE Lifestraw Personal Water Filers, 2 FREE brand-new Survival hard copy books, 4 FREE Liberty Seed Vaults, 4 FREE 11-in-1 Survival Tools and more!</i></span>
+	<div>
+		<h1 class="darkRed text-center">Stay Full and Satisfied with Food4Patriots NEW Meat &amp;  Protein Kit…Good for An Amazing 25 Years!</h1>
 	</div>
-	<div style="margin-top:50px;">
-		<p>Thank you for being a Food4Patriots customer! I hope that you love you love your Food4Patriots kit and have been sleeping easier at night knowing that you are more prepared for whatever may happen.</p>
-		<p>I've got a special "customers only" offer for you...</p>
-		<p>A lot of folks have told me that given the scary state of affairs in this country and the constant threat of natural disasters, they want more food. In fact, we have quite a few folks telling us, &ldquo;I&rsquo;ll take everything you&rsquo;ve got!&rdquo;</p>
-		<p>I want to do everything I can to help you build your food stockpile as quickly and easily as possible, so to thank you for being a customer today, I am offering you an <strong>exclusive $200.00 discount</strong> on a ONE YEAR Food4Patriots kit if you act now. But this special sale offer is ONLY for customers so if you&rsquo;re seeing this, then good news, you qualify!</p>
-		<p>Plus I'll throw in <strong>FREE Shipping and 27 FREE bonus gifts worth $470.00 -- including 4 of the super-popular Lifestraw Personal Water Filters and over 22,000+ heirloom survival seeds</strong> -- just to make this a "no-brainer" for you!</p>
-		<p>Would you like to accelerate your results by getting the 1 Year Food4Patriots Kit at a 1-time discount sale price of $1,997? (That&rsquo;s a $200.00 discount…you&rsquo;ll get 1 year&rsquo;s worth of food for just $1.11 per serving!)</p>
-		<img class="img-responsive center-block" src="/media/images/f4p/f4p-testimonials-10.png" alt="L. Graeser's Testimonial" style="margin-bottom:20px;">
+	<img class="img-responsive center-block" src="/media/images/f4p/f4p-product-img-protein-steak-01.jpg" alt="Food4Patriots 1 Year Kit" style="margin-bottom:20px;">
+	<p><strong>Chances are you&rsquo;ve got a great start on your survival food stockpile… </strong></p>
+	<p><strong>But do you have protein to &ldquo;survive and thrive&rdquo;?</strong></p>
+	<p>In a crisis, your body is under extreme stress.  You need all your strength and then some to cope with all the chaos going on around you, and to keep your loved ones safe. To do that, you&rsquo;ve got to get enough high-quality protein.</p>
+	<p>In fact, protein isn&rsquo;t just a nutritional staple of a good diet; it&rsquo;s a critical component to staying healthy...<strong>especially when the crap hits the fan!</strong></p>
+	<p><strong>Why?</strong></p>
+	<p>Well, for starters, unless you&rsquo;re getting enough, your body will turn to the only other source of protein that&rsquo;s available – by consuming its own muscle tissue.</p>
+	<p><strong>Without enough quality protein, your body literally STARTS EATING ITSELF.</strong></p>
+	<p>Gross, right? </p>
+	<p>Maybe, but 100% true.</p>
+	<p>Even the USDA recognizes the body&rsquo;s need for protein. Their current guidelines recommend a daily <strong>MINIMUM</strong> intake of 50 grams of high-quality protein for the average adult. In a disaster situation, it&rsquo;ll be even higher because you burn more calories than normal.</p>
+
+	<h2 class="darkRed text-center">Lean Protein Is The Cornerstone <br class="hidden-xs">Of Good Health</h2>
+	<p>Protein is literally the “building block” of healthy cells – it helps your body make muscles, regenerate organs, and grow hair. There isn’t a system in your body that doesn’t need lean, high-quality protein to thrive – even more so when you’re facing a crisis.</p>
+
+	<h2 class="darkRed text-center">What Are Some Other Crucial <br class="hidden-xs">Benefits of Protein?</h2>
+	<ul class="fa-ul">
+		<li><i class="fa-li fa fa-check-circle"></i><strong>Keeps hunger at bay.</strong> Protein takes longer to digest, and keeps you feeling fuller, longer.</li>
+		<li><i class="fa-li fa fa-check-circle"></i><strong>Preserves muscle.</strong> Think of protein as the &ldquo;bricklayer&rdquo; of your body. Protein is crucial to building strong muscles, and keeps the body from losing muscle tissue when it&rsquo;s not getting enough.</li>
+		<li><i class="fa-li fa fa-check-circle"></i><strong>Strengthens bones. </strong>Quality protein keeps our bones dense and helps prevent diseases like osteoporosis, which can lead to fractures.<strong></strong></li>
+		<li><i class="fa-li fa fa-check-circle"></i><strong>Mental sharpness. </strong>In a crisis, you&rsquo;ve got to be alert and focused. Protein helps your brain function at peak levels.</li>
+		<li><i class="fa-li fa fa-check-circle"></i><strong>Better sleep. </strong>Adequate protein keeps you active during the day and helps you get a peaceful night&rsquo;s rest.<strong></strong></li>
+	</ul>
+
+	<p>It&rsquo;s obvious that a high-quality source of digestible protein should be a critical part of your overall survival food plan. And that doesn&rsquo;t mean buying up loads of fattening, sugary peanut butter or tasteless cans of tuna…all of which can go bad in just a couple of years.</p>
+	<p>Nope – I&rsquo;ve got a better solution…one that&rsquo;s proven to stay fresh for a staggering 25 years!</p>
+
+	<h2 class="darkRed text-center">In fact, my new “Meat & Protein Kit” was the #1 requested item from Food4Patriots customers…a real “protein boost” to round out and “supercharge” their survival food stockpiles was just the ticket.</h2>
+	<p>And, truth be told, I didn&rsquo;t want to offer anything less than the very best…no cutting corners or using cheap fillers. No sir.</p>
+	<p>I searched for the best quality meat &amp; protein options available. Most didn&rsquo;t pass muster for quality, taste, or long shelf life. </p>
+	<p>After much trial and error, I finally found the exact high-quality protein kit my customers have been clamoring for – one that&rsquo;s delicious, nutritious, and rated good for 25 years! </p>
+	<p>Now just so you know…I&rsquo;m more than a perfectionist when it comes to any of my Food4Patriots products. It&rsquo;s no different with my Meat &amp; Protein Kit…</p>
+	<p>That&rsquo;s why this painstaking process took so long. I was going to &ldquo;do it right&rdquo; or not at all…</p>
+	<p>From the advanced dehydration process and space-age Mylar packaging that seals in nutrients and flavor, to <strong>using a separate FDA approved facility to make them</strong>, I refused to cut corners in any way. It costs me more, but nothing is too good for my customers.</p>
+
+	<h2 class="darkRed text-center">Now, You Can “Fill In The Gaps” Of Your Survival Food Stockpile With Our First Ever Food4Patriots Meat And Protein Kit!</h2>
+	<img class="img-responsive center-block margin-tb-20" src="/media/images/f4p/f4p-product-img-protein-01.jpg" alt="Meat & Protein Kit">
+	<p>Our Meat &amp; Protein Kit comes in the same re-sealable, airtight, space-age Mylar pouches and tucked inside a stackable tote you&rsquo;ve come to expect with our other survival food kits. </p>
+	<p>Keep all your individual protein packs in the tote, or take them on the go – it&rsquo;s your choice.</p>
+	<p>Compact and discreet, you can store them anywhere – your garage, basement, RV, cabin, bug-out bag, or even your car!</p>
+
+	<h2 class="darkRed text-center">Here’s Exactly What You’ll Get:</h2>
+	<div class="container-fluid oto-width margin-tb-20">
+		<div class="row text-center">
+			<div class="col-xs-6 col-sm-4 col-md-4">
+				<img class="img-responsive center-block thumbnail" src="/media/images/f4p/food/protein-beef.jpg" alt="12 servings of freeze-fried beef">
+				<div class="caption">12 servings of freeze-fried beef</div>
+			</div>
+			<div class="col-xs-6 col-sm-4 col-md-4">
+				<img class="img-responsive center-block thumbnail" src="/media/images/f4p/food/protein-chicken.jpg"  alt="12 servings of freeze-dried chicken">
+				<div class="caption">12 servings of freeze-dried chicken</div>
+			</div>
+			<div class="col-xs-6 col-sm-4 col-md-4">
+				<img class="img-responsive center-block thumbnail" src="/media/images/f4p/food/protein-red-beans.jpg"  alt="16 servings of red beans">
+				<div class="caption">16 servings of red beans</div>
+			</div>
+			<div class="col-xs-6 col-sm-4 col-md-4">
+				<img class="img-responsive center-block thumbnail" src="/media/images/f4p/food/protein-pinto-beans.jpg"  alt="20 servings of pinto beans">
+				<div class="caption">20 servings of pinto beans</div>
+			</div>
+			<div class="col-xs-6 col-sm-4 col-md-4">
+				<img class="img-responsive center-block thumbnail" src="/media/images/f4p/food/protein-black-beans.jpg"  alt="20 servings of black beans">
+				<div class="caption">20 servings of black beans</div>
+			</div>
+		</div>
 	</div>
-</div>
+	<p><strong>With our Meat &amp; Protein Kit, the possibilities are anything but bland. You&rsquo;ve got the main ingredients right here for mouth-watering favorites like:</strong></p>
+	<ul>
+		<li>Beef stew</li>
+		<li>Chicken &lsquo;N Dumplings</li>
+		<li>Red Bean Chili</li>
+		<li>Black Bean Burritos</li>
+		<li>Pinto Bean Dip</li>
+	</ul>
+	<p>And many more tasty recipes!</p>
 
-<div class="container oto-width">
-	<h2 class="darkRed text-center">Check Out The Amazing FREE Bonuses You Can ONLY Get With The 1 Year Kit!</h2>
-	<p>You’re going to get the “mother lode” of special bonuses that are ONLY available with the 1 Year Kit!</p>
-</div>
+	<h2 class="darkRed text-center">Lasts 25 Years, But Tastes As Fresh As Tonight’s Dinner</h2>
+	<p>Everything is super-easy to prepare. <u>Just add hot water!</u></p>
+	<p>Native Americans and Pioneers got through the toughest winters and survival situations with the same kind of dried meat you find in our Meat &amp; Protein Kit! </p>
+	<p>Plus, it&rsquo;s very low in fat yet high in life-sustaining protein!</p>
+	<p>You get a whopping 80 total servings of high-quality, lean protein, to keep you fit as a fiddle and ready for whatever Mother Nature or other man-mad catastrophes can throw at you.</p>
 
-<div class="container oto-width">
-	<h2 class="darkRed text-center">You Will Also Get FOUR More Sets Of All Of Your Original FREE Bonuses When You Take Advantage Of This Special Upgrade Offer! </h2>
-	<p>Add to your own stockpile, keep at multiple locations or give to friends and loved ones so they can experience the same peace of mind. Your FREE bonuses include:</p>
-</div>
-
-<div class="container oto-width">
 	<h2 class="darkRed text-center">You Are 100% Protected By My Outrageous Double Guarantee.</h2>
 	<div class="outLineBoxDarkBlue">
 		<p><img src="/media/images/misc/seal-guarantee-satisfaction.jpg" alt="Guarantee #1" class="pull-left img-responsive media margin-t-20">
 		<h3>Guarantee #1:</h3> This is a 100% money back guarantee. No questions asked. If for any reason, you&rsquo;re not
-		satisfied with your Food4Patriots kit, just return it within 60 days of purchase and I&rsquo;ll refund 100% of your purchase
+		satisfied with your Meat & Protein Kit, just return it within 60 days of purchase and I&rsquo;ll refund 100% of your purchase
 		price. If you try it and decide it&rsquo;s not as delicious and nutritious as I promised, you can have your money back for
 		any reason or no reason whatsoever. So there&rsquo;s absolutely no risk for you. You literally can&rsquo;t lose!</p>
 		<div class="clearfix"></div>
@@ -39,31 +97,31 @@
 	<div class="outLineBoxDarkBlue">
 		<p><img src="/media/images/misc/seal-guarantee-money.jpg" alt="Guarantee #2" class="pull-left img-responsive media margin-t-20">
 		<h3>Guarantee #2:</h3> This is an unheard of 300% money back guarantee. It&rsquo;s in addition to guarantee #1. If you open any
-		of your Food4Patriots meals anytime <strong>in the next 25 years</strong> and find that your food has spoiled or gone bad, you
-		can return your entire Food4Patriots stockpile and I will <strong>triple</strong> your money back!</p>
+		of your Meat & Protein Kit anytime <strong>in the next 25 years</strong> and find that your food has spoiled or gone bad, you
+		can return your entire Meat & Protein Kit and I will <strong>triple</strong> your money back!</p>
 		<p>That&rsquo;s how confident I am that this food will remain just as delicious and nutritious for the next 25 years as it is
 			on the day you buy it. Some of my friends said I was crazy to offer this double guarantee, but to be honest I&rsquo;m not
-			really worried about it, because I am so confident you&rsquo;re going to see the value in your Food4Patriots kit as soon
+			really worried about it, because I am so confident you&rsquo;re going to see the value in your Meat & Protein Kit as soon
 			as you have it in your hands.</p>
 	</div>
 
-	<div><img class="img-responsive center-block" src="/media/images/f4p/f4p-testimonials-09.png" width="626" height="269" alt="Stacy's Testimonial"></div>
+	<p><strong>Get Your Kit Now for Just $1.83 Per Serving!</strong></p>
+	<p>For a limited time, you can get our Meat &amp; Protein Kit for the <em>special promotional price</em> of just $147.00! </p>
+	<p>That boils down to only $1.83 per serving. Wow!</p>
+	<p>Be sure to jump on this opportunity while it lasts…unfortunately for you and me, the cost of food is currently skyrocketing from drought and scarcity that&rsquo;s making headlines all across America…so please…claim your kit while there&rsquo;s still time.</p>
+	<p><strong>Click the &ldquo;Add To Cart&rdquo; button now, and I&rsquo;ll rush your kit to your front door right away!</strong></p>
 
-	<h2 class="darkRed title-max-600 center-block text-center">Get On The ‘Fast Track’ - Claim Your 1 Year Food4Patriots Kit For $1000.00 Off Right Now!</h2>
-	<p>Now I understand that the 1 Year Food4Patriots kit is the right choice for folks who are looking for the ultimate in food security. This kit plus all the bonuses is valued at over $3,000, but because you&rsquo;ve already taken the 1st step, and because I appreciate you putting your trust in us by being a customer, you can get your 1 Year Food4Patriots kit today for just $1,997.</p>
-	<p>You get a year&rsquo;s worth of delicious survival food for $1.11 per serving!</p>
-	<p>Plus I'll throw in 27 FREE bonus gifts worth $800+ -- including 4 of the super-popular Lifestraw Personal Water Filters and over 22,000+ heirloom survival seeds -- just to make this a "no-brainer" for you!</p>
-	<p>I was only able to secure a limited quantity of these 1 Year Food4Patriots kits and it&rsquo;s been one of our most frequent requests, so I don&rsquo;t know how long I&rsquo;m going to have them available. To make sure that you don&rsquo;t miss out on getting yours, go ahead and click the big orange &ldquo;Click Here To Accept&rdquo; button below to add the 1 Year Food4Patriots Kit to your order today!</p>
-	<p>The 1 Year Food4Patriots kit will help secure your stockpile faster and protect you and your family from whatever crisis may come. You&rsquo;ll be on the &ldquo;fast track&rdquo; to securing the ultimate food stockpile.</p>
-	<p>This is your last chance for this special 1-time discount, so you need to act now. To get your 1 Year Food4Patriots kit at $1000.00 off, click the big green &ldquo;Add To Cart&rdquo; button below.</p>
-	<div>
-		<div><img class="img-responsive center-block" src="/media/images/f4p/f4p-1-year-kit-01.jpg"  alt="Food4Patriots 1 Year Kit"/></div>
-		<div><img class="img-responsive center-block" src="/media/images/f4p/f4p-1year-value-chart-01.jpg" alt="Value Chart"/></div>
-		<div class="text-center"><h2 id="save" class="darkRed">Act Today And Save Over $1000</h2></div>
-
-		<div style="text-align:center;">
-			<a href="<?php echo $offerUrl; ?>"><img src="/assets/images/buttons/btn-green-add-to-cart-01.jpg" class="img-responsive center-block" alt="Add To Cart"></a>
-		</div>
+	<div class="margin-tb-50">
+		<img class="img-responsive center-block margin-tb-20" src="/media/images/f4p/f4p-product-img-protein-02.jpg" alt="Meat & Protein Kit">
+		<a href="<?php echo $offerUrl; ?>"><img src="/assets/images/buttons/btn-green-add-to-cart-01.jpg" class="img-responsive center-block" alt="Add To Cart"></a>
 	</div>
-</div>
+
+	<p>Please hurry Friend…I don&rsquo;t know how long I&rsquo;m going to be able to offer these Meat &amp; Protein Kits at this rock-bottom price, or <strong><em>at all…</em></strong>with survival food supplies disappearing, I&rsquo;m bound to sell out fast. </p>
+	<p>I&rsquo;d hate to think your family&rsquo;s nutrition might be compromised because you didn&rsquo;t act while you had the chance…</p>
+	<p><strong>So claim your kit NOW by clicking the &ldquo;Add To Cart&rdquo; button, and have true peace of mind that your loved ones WON&rsquo;T starve in the coming crisis…</strong></p>
+
+	<div class="margin-tb-50">
+		<a href="<?php echo $offerUrl; ?>"><img src="/assets/images/buttons/btn-green-add-to-cart-01.jpg" class="img-responsive center-block" alt="Add To Cart"></a>
+	</div>
+
 </div>
