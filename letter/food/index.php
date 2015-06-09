2ddf8bf1a7fcaac381e6152c6c38ad7430a4f2c9
jeremy <jeremy@rebootmarketing.com>
2015-06-09 15:51:27 -0500

    FFP-696 Update Letter Page To Match Platform

---
+++ letter/food/index.php
@@ -0,0 +1,35 @@
+<?php
+// SET PRODUCT ID
+$_SESSION['productId'] = 230; //please keep as an integer
+$_SESSION['quantity'] = 1;
+include_once("Product.php");
+//creates a product object that is available from every template
+$productDataObj = Product::getProduct($_SESSION["productId"]);
+//include template top AFTER the product information is set
+include_once("template-top.php");
+include_once ('template-header.php'); /*Add template-header-nav.php to add top menu*/
+$offerUrl = "/checkout/protein/index.php" . $analyticsObj->queryString;
+
+/*$variantsArray = array (
+	"a", // Short Split Test
+	"b", // Short Split Test
+);
+if($_GET["v"]) {
+	if(in_array(trim($_GET["v"]),$variantsArray)) {
+		$variation = trim($_GET["v"]);
+	}
+}*/
+
+?>
+<link href="/assets/css/styles-letter.css" rel="stylesheet" type="text/css">
+
+<link href='https://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,700,600,300,800' rel='stylesheet' type='text/css'>
+
+		<div id="wrapper">
+			<?php include_once("content.php"); ?>
+		</div>
+	</div>
+<?php
+include_once("template-bottom.php");
+?>
