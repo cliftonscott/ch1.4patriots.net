f139473745113f05fcc41f687bdcca8374e9587d
Brian Gibbins <brian@briangibbins.com>
2015-07-08 16:32:48 -0400

    FFP-738 updates text errors per defect

--- checkout/alt/f4p-1yearkit-discount-offer.php
+++ checkout/alt/f4p-1yearkit-discount-offer.php
@@ -31,12 +31,12 @@ include_once ('template-top.php');
 			<span class="caption"><i>Save $1500.00 when you order a 1-year Kit,  plus get FREE Shipping, 4 FREE Survival Spring Personal Water Filters, 2 FREE brand-new Survival hard copy books, 4 FREE Liberty Seed Vaults, 4 FREE 11-in-1 Survival Tools and more!</i></span>
 		</div>
 		<div style="margin-top:50px;">
-			<p>Thank you for being a Food4Patriots customer! I hope that you love you love your Food4Patriots kit and have been sleeping easier at night knowing that you are more prepared for whatever may happen.</p>
+			<p>Thank you for being a Food4Patriots customer! I hope that you love your Food4Patriots kit and have been sleeping easier at night knowing that you are more prepared for whatever may happen.</p>
 			<p>I've got a special "customers only" offer for you...</p>
 			<p>A lot of folks have told me that given the scary state of affairs in this country and the constant threat of natural disasters, they want more food. In fact, we have quite a few folks telling us, &ldquo;I&rsquo;ll take everything you&rsquo;ve got!&rdquo;</p>
-			<p>I want to do everything I can to help you build your food stockpile as quickly and easily as possible, so to thank you for being a customer today, I am offering you an <strong>exclusive $1500.00 discount</strong> on a ONE YEAR Food4Patriots kit if you act now. But this special sale offer is ONLY for customers so if you&rsquo;re seeing this, then good news, you qualify!</p>
+			<p>I want to do everything I can to help you build your food stockpile as quickly and easily as possible, so to thank you for being a customer today, I am offering you an <strong>exclusive $1500.00 discount</strong> on a 1-year Food4Patriots kit if you act now. But this special sale offer is ONLY for customers so if you&rsquo;re seeing this, then good news, you qualify!</p>
 			<p>Plus I'll throw in <strong>FREE Shipping and 27 FREE bonus gifts worth $470.00 -- including 4 of the super-popular Survival Spring Personal Water Filters and over 22,000+ heirloom survival seeds</strong> -- just to make this a "no-brainer" for you!</p>
-			<p>Would you like to accelerate your results by getting the 1-year Food4Patriots Kit at a 1-time discount sale price of $1,497? (That&rsquo;s a $1500.00 discount…you&rsquo;ll get 1-year&rsquo;s worth of food for just $.83 per serving!)</p>
+			<p>Would you like to accelerate your results by getting the 1-year Food4Patriots Kit at a one-time discount sale price of $1,497? (That&rsquo;s a $1500.00 discount…you&rsquo;ll get 1-year&rsquo;s worth of food for just $.83 per serving!)</p>
 			<img class="img-responsive center-block" src="/media/images/f4p/f4p-testimonials-10.png" alt="L. Graeser's Testimonial" style="margin-bottom:20px;">
 		</div>
 	</div>
@@ -65,10 +65,10 @@ include_once ('template-top.php');
 		<h2 class="darkRed text-center">You Are 100% Protected By My Outrageous Double Guarantee.</h2>
 		<div class="outLineBoxDarkBlue">
 			<p><img src="/media/images/misc/seal-guarantee-satisfaction.jpg" alt="Guarantee #1" class="pull-left img-responsive media margin-t-20">
-			<h3>Guarantee #1:</h3> This is a 100% money back guarantee. No questions asked. If for any reason, you&rsquo;re not
+			<h3>Guarantee #1:</h3> This is a 100% money back guarantee. No questions asked. If for any reason you&rsquo;re not
 			satisfied with your Food4Patriots kit, just return it within 60 days of purchase and I&rsquo;ll refund 100% of your purchase
 			price. If you try it and decide it&rsquo;s not as delicious and nutritious as I promised, you can have your money back for
-			any reason or no reason whatsoever. So there&rsquo;s absolutely no risk for you. You literally can&rsquo;t lose!</p>
+			any reason, or no reason whatsoever. So there&rsquo;s absolutely no risk for you. You literally can&rsquo;t lose!</p>
 			<div class="clearfix"></div>
 		</div>
 
@@ -84,12 +84,10 @@ include_once ('template-top.php');
 				as you have it in your hands.</p>
 		</div>
 
-		<div><img class="img-responsive center-block" src="/media/images/f4p/f4p-testimonials-09.png" width="626" height="269" alt="Stacy's Testimonial"></div>
-
 		<h2 class="darkRed title-max-600 center-block text-center">Get On The ‘Fast Track’ - Claim Your 1-year
 			Food4Patriots Kit For $1500.00 Off Right Now!</h2>
 
-		<p>Now I understand that the 1-year Food4Patriots kit is the right choice for folks who are looking for the ultimate in food security. This kit plus all the bonuses is valued at over $3,000, but because you&rsquo;ve already taken the 1st step, and because I appreciate you putting your trust in us by being a customer, you can get your 1-year Food4Patriots kit today for just $1,497.</p>
+		<p>Now I understand that the 1-year Food4Patriots kit is the right choice for folks who are looking for the ultimate in food security. This kit plus all the bonuses is valued at over $3,000, but because you&rsquo;ve already taken the first step, and because I appreciate you putting your trust in us by being a customer, you can get your 1-year Food4Patriots kit today for just $1,497.</p>
 
 		<p>You get a year&rsquo;s worth of delicious survival food for $.83 per serving!</p>
 
@@ -98,13 +96,13 @@ include_once ('template-top.php');
 
 		<p>I was only able to secure a limited quantity of these 1-year Food4Patriots kits and it&rsquo;s been one of our most frequent requests,
 			so I don&rsquo;t know how long I&rsquo;m going to have them available. To make sure that you don&rsquo;t miss out on getting yours,
-			go ahead and click the big orange &ldquo;Click Here To Accept&rdquo; button below to add the 1-year Food4Patriots Kit to your order
+			go ahead and click the big green &ldquo;Add to Cart&rdquo; button below to add the 1-year Food4Patriots Kit to your order
 			today!</p>
 
 		<p>The 1-year Food4Patriots kit will help secure your stockpile faster and protect you and your family from whatever crisis may come.
 			You&rsquo;ll be on the &ldquo;fast track&rdquo; to securing the ultimate food stockpile.</p>
 
-		<p>This is your last chance for this special 1-time discount, so you need to act now. To get your 1-year
+		<p>This is your last chance for this special one-time discount, so you need to act now. To get your 1-year
 			Food4Patriots kit at $1000.00 off, click the big green &ldquo;Add To Cart&rdquo; button below.</p>
 
 		<div>
@@ -208,7 +206,7 @@ include_once ('template-top.php');
 	</div>
 
 	<div class="guaranteeBox">
-		<p><img src="/assets/images/checkout/satisfaction-seal-02.png" alt="Frank" width="150" height="180" class="img-responsive pull-left"><strong><span class="brightBlue">Guarantee #1:</span></strong> This is a <strong>100% money back guarantee</strong>. No questions asked. If for any reason, you&rsquo;re not satisfied with your Food4Patriots kit, just return it within 60 days of purchase and I&rsquo;ll refund 100% of your purchase.            </p>
+		<p><img src="/assets/images/checkout/satisfaction-seal-02.png" alt="Frank" width="150" height="180" class="img-responsive pull-left"><strong><span class="brightBlue">Guarantee #1:</span></strong> This is a <strong>100% money back guarantee</strong>. No questions asked. If for any reason you&rsquo;re not satisfied with your Food4Patriots kit, just return it within 60 days of purchase and I&rsquo;ll refund 100% of your purchase.            </p>
 		<p>&nbsp;</p>
 		<p><strong><span class="brightBlue">Guarantee #2:&nbsp;</span></strong>This is an unheard of 300% money back guarantee. It&rsquo;s in addition to guarantee #1.&nbsp;If you open any of your Food4Patriots meals anytime&nbsp;<strong>in the next 25 years</strong>&nbsp;and find that your food has spoiled, you can return your entire Food4Patriots stockpile and I will&nbsp;<strong>triple</strong>&nbsp;your money back!</p>
 		<div class="clearfix"></div>
