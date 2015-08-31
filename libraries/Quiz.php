9836fbf3fc8a988c8a8d87927ddc72c005c9beed
Brian Gibbins <brian@briangibbins.com>
2015-08-31 15:16:50 -0400

    FFP-844 updates quiz library with new functions to meet requirements

--- libraries/Quiz.php
+++ libraries/Quiz.php
@@ -11,13 +11,64 @@ class Quiz {
 	static $appMessagesAry = array();
 	static $appErrorsAry = array();
 
+	const QUIZ_TABLE = "quiz";
+	const QUIZ_RESULTS_TABLE = "quizResults";
+
 	public function __construct() {
 
 	}
 
-	function setQuiz() {
+	function saveQuizResults($dataObj) {
+
+		$results = new stdClass();
+		$results->errors = array();
+
+		if(empty($dataObj)) {
+			self::setError("saveQuiz 'dataObj' must contain an object.");
+			$results->errors = "saveQuiz 'dataObj' must contain an object.";
+			$results->success = false;
+			return $results;
+		}
+
+		if(gettype($dataObj) !== "object") {
+			self::setError("saveQuiz 'dataObj' must contain an object.");
+			$results->errors = "saveQuiz 'dataObj' must contain an object.";
+			$results->success = false;
+			return $results;
+		}
+
+		include_once("Db.php");
+		$dbObj = new Db();
+		$db = $dbObj->connect();
+
+		$visitorId = session_id();
+		$quizId = 1;
+
+		$quizData = $dataObj->quizData;
+		foreach($quizData as $q => $a) {
+
+			$sql = "INSERT INTO `" . self::QUIZ_RESULTS_TABLE . "` SET quizId=:quizId, visitorId=:visitorId, question=:question, answer =:answer";
+			$insert = $db->prepare($sql);
+			$insert->bindParam(':quizId', $quizId, PDO::PARAM_INT);
+			$insert->bindParam(':visitorId', $visitorId, PDO::PARAM_STR, 12);
+			$insert->bindParam(':question', $q, PDO::PARAM_STR, 12);
+			$insert->bindParam(':answer', $a, PDO::PARAM_STR, 12);
+
+			if($insert->execute()) {
+				self::setMessage("Inserted into " . self::QUIZ_RESULTS_TABLE . ": " . $quizId);
+				$results->success = true;
+			} else {
+				self::setError("Unable to insert into " . self::QUIZ_RESULTS_TABLE . ".");
+				$results->success = false;
+			}
+		}
+
+		$results->errors = self::getErrors();
+		$results->messages = self::getMessages();
+		return $results;
 
 	}
+
 	function getNextQuizUrl() {
 
 	}
@@ -35,6 +86,7 @@ class Quiz {
 
 		return $pageData;
 	}
+
 
 //ERROR AND MESSAGE HANDLING
 	function setMessage($msg) {
