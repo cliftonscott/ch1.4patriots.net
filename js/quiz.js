5c87ee38599aaa9a6c9cd0837512db3fa55600c2
Brian Gibbins <brian@briangibbins.com>
2015-08-31 16:20:53 -0400

    FFP-849 adds fade in to section change

--- js/quiz.js
+++ js/quiz.js
@@ -1,8 +1,6 @@
 function nextQ(number) {
     close_section = "#section_" + number;
     $(close_section).css("display","none");
-    $(close_section).css("visibility","hidden");
     open_section = "#section_" + (number+1);
-    $(open_section).css("display","block");
-    $(open_section).css("visibility","visible");
+    $(open_section).fadeIn();
 }
