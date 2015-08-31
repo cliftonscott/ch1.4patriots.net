1a7be2a9d700fe32fc62abcf80de01f0ec24822c
Brian Gibbins <brian@briangibbins.com>
2015-08-31 11:04:30 -0400

    FFP-787 sets quizName variable

--- quiz/1/quiz.php
+++ quiz/1/quiz.php
@@ -5,18 +5,22 @@
 </style>
 <?php
 //include template top AFTER the product information is set
+$quizName = "Government Survival Quiz";
 include_once ('template-top.php');
 ?>
 <?php include_once ('template-header.php'); /*Add template-header-nav.php to add top menu*/?>
-	<div class="container-main">
-		<div class="container oto-width">
-			<div class="row">
-				<div class="col-sm-12 col-md-12">
-					<?php include_once("quiz-body.phtml");?>
-				</div>
+<div class="container-main">
+	<div class="container oto-width">
+		<div class="row">
+			<div class="col-sm-12 col-md-12">
+				<?php
+				echo "<h1 class='quizName'>" . $quizName . "</h1>";
+				include_once("quiz-body.phtml");
+				?>
 			</div>
 		</div>
-		<?php
-		include_once("template-bottom.php");
-		?>
 	</div>
+	<?php
+	include_once("template-bottom.php");
+	?>
+</div>
