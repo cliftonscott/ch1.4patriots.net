9b9e9d6bd3f7d4e386f5f83653e081b97e016903
Brian Gibbins <brian@briangibbins.com>
2015-08-31 15:17:27 -0400

    FFP-845 adds quiz processing file

--- quiz/process-quiz.php
+++ quiz/process-quiz.php
@@ -1,6 +1,22 @@
 <?php
-echo "<h3>quiz process file</h3>";
+$quizName = trim($_POST["quizName"]);
+$quizName = filter_var($quizName, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
 foreach ($_POST as $k => $v) {
-	echo $k . ":" . $v . "<br>";
+	if(substr($k,0,8) === "question") {
+		$q = filter_var($k, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+		$a = filter_var($v, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+		$questions[$q] = $a;
+	}
 }
 
+$dataObj = new stdClass();
+$dataObj->visitorId = session_id();
+$dataObj->quizName = $quizName;
+$dataObj->quizData = $questions;
+
+include_once("Quiz.php");
+$quizObj = new Quiz();
+$save = $quizObj->saveQuizResults($dataObj);
+
+header("Location: /checkout/index.php");
+exit;
