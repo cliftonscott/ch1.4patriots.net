1e88ca78419c286ff8e4f4a2fb1bef0fdbb117b6
huskilla <chris.robison@4patriots.com>
2015-09-01 11:00:59 -0500

    FFP-787 Updated text

--- quiz/1/index.php
+++ quiz/1/index.php
@@ -13,7 +13,7 @@ include_once ('template-top.php');
 			<div class="row">
 				<div class="col-sm-12 col-md-12">
 					<div>
-						<p class="text-center"><i>Obama and his government cronies now have more<br class="hidden-xs"> power than ever before...</i></p>
+						<p><i>Obama and his government cronies now have more power than ever before...</i></p>
 						<h1 class="darkRed text-center">Are You And Your Family Prepared<br class="hidden-xs"> To Survive A Government<br class="hidden-xs"> Controlled Society?</h1>
 					</div>
 					<p>527,197 people have taken the <strong><i>Government Survival Quiz.</i></strong> Start the quiz below to see if you & your family are prepared to survive if the government finally gets <strong>complete control</strong> of America...</p>
@@ -25,7 +25,7 @@ include_once ('template-top.php');
 					<p>Most people think they&#39;re prepared for these scenarios because they already have water, food, guns and a back-up generator stored in their house...</p>
 					<p>But the truth is... that&#39;s <strong>not enough</strong> to actually survive when chaos strikes.</p>
 					<p>If you want to be totally self-reliant and able to protect your family when a crisis hits (without having to rely on the Federal government to take care of you) then do yourself a favor and take the FREE, 33 second, <strong>Government Survival Quiz</strong> below...</p>
-					<p>Simply click the orange &#34;Start The Quiz&#34; button below to see if you and your family are truly prepared to survive a government gone wild...</p>
+					<p>Simply click the orange <strong>&#34;Start The Quiz&#34;</strong> button below to see if you and your family are truly prepared to survive a government gone wild...</p>
 					<div id="start-quiz">
 						<a href="quiz.php" class="start-btn" style="margin-top:8px; font-weight:bold;">Start The Quiz</a>
 						<p style="color:#e09800; font-size: 20pt;" >(Click Here to Start The Quiz)</p>
