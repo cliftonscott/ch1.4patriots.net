644b39c9ae6279387b1efe35660bfe4d32a05102
caseywittner <casey.wittner@4patriots.com>
2015-09-01 10:01:04 -0500

    FFP-852 Trivial page changes

--- checkout/alt/f4p-free-food-offer.php
+++ checkout/alt/f4p-free-food-offer.php
@@ -38,10 +38,10 @@ include_once ('template-top.php');
 						<ul>
 							<li>&ldquo;Disaster-Proof packing&rdquo;… a military-grade Mylar pouch keeps out air, moisture and light to keep your food nutritionally sound and tasting great for up to 25 years</li>
 							<li>Instantly protects you and your family from going hungry in a disaster… store in your home, car, office or give to a friend or family member</li>
-							<li>Simple to prepare -- just add boiling water, simmer &amp; serve in about 15 min</li>
-							<li>Rushed right to your door via USPS 1st Class Mail </li>
+							<li>Simple to prepare -- just add boiling water, simmer, and serve in about 15 min</li>
+							<li>Rushed right to your door via USPS First Class Mail </li>
 						</ul>
-						<strong>Regularly priced at $9.95, today it&rsquo;s FREE while supplies (limit 1 per household, just cover postage)&nbsp;&nbsp;--&nbsp;</strong>get yours right now before they&rsquo;re gone! 
+						<strong>Regularly priced at $9.95, today it&rsquo;s FREE while supplies (limit one per household, just cover postage)&nbsp;&nbsp;--&nbsp;</strong>get yours right now before they&rsquo;re gone!
 					</div>
 					<div class="col-lg-12 text-center hidden-xs">
 						<img src="/assets/images/buttons/btn-order-now-green-left-01.png" alt="Frank" class="img-responsive center-block">
@@ -55,7 +55,7 @@ include_once ('template-top.php');
 	</div>
 
 	<div class="guaranteeBox">
-		<p><img src="/assets/images/checkout/satisfaction-seal-02.png" alt="Frank" width="150" height="180" class="img-responsive pull-left"><strong><span class="brightBlue">Guarantee #1:</span></strong> This is a <strong>100% money back guarantee</strong>. No questions asked. If for any reason, you&rsquo;re not satisfied with your Food4Patriots kit, just return it within 60 days of purchase and I&rsquo;ll refund 100% of your purchase.            </p>
+		<p><img src="/assets/images/checkout/satisfaction-seal-02.png" alt="Frank" width="150" height="180" class="img-responsive pull-left"><strong><span class="brightBlue">Guarantee #1:</span></strong> This is a <strong>100% money back guarantee</strong>. No questions asked. If for any reason you&rsquo;re not satisfied with your Food4Patriots kit, just return it within 60 days of purchase and I&rsquo;ll refund 100% of your purchase.            </p>
 		<p>&nbsp;</p>
 		<p><strong><span class="brightBlue">Guarantee #2:&nbsp;</span></strong>This is an unheard of 300% money back guarantee. It&rsquo;s in addition to guarantee #1.&nbsp;If you open any of your Food4Patriots meals anytime&nbsp;<strong>in the next 25 years</strong>&nbsp;and find that your food has spoiled, you can return your entire Food4Patriots stockpile and I will&nbsp;<strong>triple</strong>&nbsp;your money back!</p>
 		<div class="clearfix"></div>
