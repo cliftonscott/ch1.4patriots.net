347eb08ccdf2f9e2410a77738251cf73d9732fd4
jeremy <jeremy@rebootmarketing.com>
2015-09-14 15:46:26 -0500

    FFP-868 -Add Custom Message To Video Lander For Pub

--- video/index.php
+++ video/index.php
@@ -54,6 +54,7 @@ $pubArray = array (
 	"109" => "subscribers of Barton Publishing",
 	"110" => "subscribers of Laissez Faire Today",
 	"111" => "Dr. Sears&#39; Readers",
+	"112" => "subscriber of The Oxford Club",
 );
 if($_GET["pub"]) {
 	if(array_key_exists(trim($_GET["pub"]),$pubArray)) {
