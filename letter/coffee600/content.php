86f5074529437067047cdf5befcb7bf317bffa8e
huskilla <chris.robison@4patriots.com>
2015-10-05 15:02:29 -0500

    FFP-863 Funnel Build

---
+++ letter/coffee600/content.php
@@ -0,0 +1,150 @@
+<div class="container oto-width">
+	<div>
+	<?php if($variation === "a"): ?>
+		<h1 class="darkRed text-center" style="font-size: 29pt;">Who Else Wants More Flavor, Nutrition & Taste... GUARANTEED For 25 Years of Storage?!</h1>
+	<?php elseif($variation === "b"): ?>
+		<h1 class="darkRed text-center">Don't Miss the Chance to Take Your Food4Patriots Kit to the Next Level with These Incredible "Superfoods."</h1>
+	<?php else: ?>
+		<h1 class="darkRed text-center">“Supercharge” Your Survival Food Stockpile With Food4Patriots Fruit, Veggie and Snack Mix!</h1>
+	<?php endif; ?>
+	</div>
+	<img class="img-responsive center-block margin-tb-20" style="width: 100%" src="/media/images/f4p/f4p-fvs-kit-tote-06.jpg" alt="">
+	<p>Dear Patriot,</p>
+	<p><strong>Fruits and veggies are vital to your overall survival food plan.</strong></p>
+	<p>There’s no doubt – in order to get and stay healthy, you’ve got to get multiple servings of fruit and vegetables each day.</p>
+	<p><strong>You’ll especially need them when facing a crisis, when these foods are hard, if not impossible to come by…</strong></p>
+	<p>There’s no other food group that’s as life giving as these orchard and garden delights… chock full of vitamins, minerals, antioxidants, and fiber.</p>
+	<p>Even if you’re more of a “meat and potatoes” kind of person, most everyone enjoys a bowl of sweet, juicy strawberries or garden fresh veggies like broccoli and green beans.</p>
+	<p>Truth is, without an adequate intake of fruits and veggies, your body simply isn’t getting the nutrients it needs to work right.</p>
+	<p>And folks, despite what others may claim, <strong>there’s no pill you can pop that will replace them.</strong></p>
+	<p>Let me explain…</p>
+	<p>Plant-based foods are naturally low in calories but high in nutrition. There’s no other food group (what scientists with fancy degrees call macronutrients) that can protect your body’s cells and keeps things “humming along” better.</p>
+
+	<h2 class="darkRed text-center">Eating Fruits and Vegetables Can Help Maximize the Nutrition of Your Survival Food!</h2>
+	<img class="img-responsive pull-right img-padding-left" src="/media/images/f4p/letter/f4p-letter-fvs-basket.jpg" alt="Food4Patriots 1 Year Kit">
+	<p>When you add high-quality fruits and vegetables to your existing survival food stockpile, you’ll enjoy knowing you’re optimizing the level of nutrients your body needs</p>
+	<p>What Are Some Other Crucial Benefits of fruits and vegetables?</p>
+	<ul class="fa-ul fa-ul-space">
+		<li><i class="fa-li fa fa-check"></i><strong>Better regularity</strong> Fiber keeps your digestive tract healthy and helps move waste out of your system, which helps you feel your best and be “light on your feet” – which is critical when you need to move fast.</li>
+		<li><i class="fa-li fa fa-check"></i><strong>Faster healing and boosted immunity</strong> Our bodies don’t make all the vitamins we need, so we need to eat foods that are rich in them to recover faster from infections and illness, which helps if you’re injured or catch a nasty bug.</li>
+		<li><i class="fa-li fa fa-check"></i><strong>Clear thinking</strong> Whole plant foods contain the nutrients that keep your brain sharp as a tack – especially important in a survival situation.</li>
+		<li><i class="fa-li fa fa-check"></i><strong>Natural anti-inflammatory</strong> Antioxidants in fruit and veggies help protect your ticker and other vital organs by scavenging for free radicals that cause sickness.</li>
+	</ul>
+
+	<p>Okay… so there’s no arguing that you’ve got to have an adequate amount of fruits and vegetables every day for <strong>better</strong> health… but how do you work them into your survival food plan? </p>
+	<p>I mean… fresh fruits and veggies spoil after just a few days, right?</p>
+	<p>How are you supposed to survive on rotting produce?</p>
+	<p>Well, you’re not.</p>
+
+	<h2 class="darkRed text-center">Get All the Plant Based Nutrition You Need With Food4Patriots Fruit, Veggie and Snack Mix – Wholesome, Delicious, and Good for 25 Years</h2>
+	<img class="img-responsive center-block margin-tb-20" style="width: 100%" src="/media/images/f4p/f4p-fvs-kit-tote-02.jpg" alt="">
+	<p>Our Fruit, Veggie and Snack Mix comes in the same re-sealable, airtight, space-age Mylar pouches and tucked inside a stackable tote you’ve come to expect with our other survival food kits.</p>
+	<p>Keep all your individual packs in the tote, or take them on the go – it’s your choice.</p>
+	<p>Compact and discreet, you can store them anywhere – your garage, basement, RV, cabin, bug-out bag, or even your car!</p>
+
+	<h2 class="darkRed text-center">Here's Exactly What You'll Get:</h2>
+	<div class="container-fluid oto-width margin-tb-20">
+		<div class="row text-center">
+			<div class="col-xs-6 col-sm-4 col-md-4">
+				<img class="img-responsive center-block thumbnail" src="/media/images/f4p/food/fvs-corn.jpg"  alt="24 servings of freeze-dried corn">
+				<div class="caption"><strong>24 servings</strong> of freeze-dried corn</div>
+			</div>
+			<div class="col-xs-6 col-sm-4 col-md-4">
+				<img class="img-responsive center-block thumbnail" src="/media/images/f4p/food/fvs-broccoli.jpg"  alt="16 servings of broccoli">
+				<div class="caption"><strong>16 servings</strong> of freeze-dried broccoli</div>
+			</div>
+			<div class="col-xs-6 col-sm-4 col-md-4">
+				<img class="img-responsive center-block thumbnail" src="/media/images/f4p/food/fvs-blueberries.jpg"  alt="8 servings of freeze-dried blueberries">
+				<div class="caption"><strong>8 servings</strong> of freeze-dried blueberries</div>
+			</div>
+			<div class="col-xs-6 col-sm-4 col-md-4">
+				<img class="img-responsive center-block thumbnail" src="/media/images/f4p/food/fvs-greenbeans.jpg"  alt="24 servings of green beans">
+				<div class="caption"><strong>24 servings</strong> of freeze-dried green beans</div>
+			</div>
+			<div class="col-xs-6 col-sm-4 col-md-4">
+				<img class="img-responsive center-block thumbnail" src="/media/images/f4p/food/fvs-strawberries.jpg"  alt="16 servings of freeze-dried strawberries">
+				<div class="caption"><strong>16 servings</strong> of freeze-dried strawberries</div>
+			</div>
+			<div class="col-xs-6 col-sm-4 col-md-4">
+				<img class="img-responsive center-block thumbnail" src="/media/images/f4p/food/fvs-pineapple.jpg"  alt="8 servings of pineapple">
+				<div class="caption"><strong>8 servings</strong> of freeze-dried pineapple</div>
+			</div>
+			<div class="col-xs-6 col-sm-4 col-md-6 col-md-push-1">
+				<img style="width: 200px" class="img-responsive center-block thumbnail" src="/media/images/f4p/food/fvs-banana-chips.jpg"  alt="8 servings of honey-coated banana chips">
+				<div class="caption"><strong>8 servings</strong> of honey-coated banana chips</div>
+			</div>
+			<div class="col-xs-6 col-sm-4 col-md-6 col-md-pull-1">
+				<img style="width: 200px" class="img-responsive center-block thumbnail" src="/media/images/f4p/food/fvs-pudding.jpg"  alt="10 servings of chocolate pudding for dessert!">
+				<div class="caption"><strong>Even 10 servings of chocolate<br/> pudding for dessert!</strong></div>
+			</div>
+		</div>
+	</div>
+	<p><strong>With our Fruit, Veggie and Snack Mix, you can make all kinds of tasty creations like:</strong></p>
+	<ul>
+		<li>Strawberry Oatmeal</li>
+		<li>Healthy Trail Mix</li>
+		<li>Vegetable Medley</li>
+	</ul>
+	<p>And tons more!</p>
+
+	<h2 class="darkRed text-center">Lasts 25 Years, But Tastes As Fresh As If You Just Plucked Them Off A Tree Or Harvested Them Out Of Your Very Own Garden!</h2>
+	<p>Everything is super-easy to prepare. <u>Just add hot water!</u></p>
+	<p>Native Americans and Pioneers got through the toughest winters and survival situations with the same kind of fruits and vegetables you will find in our Fruit, Veggie and Snack Mix!</p>
+	<p>Plus, it&rsquo;s very high in essential vitamins and minerals</p>
+	<p>You get <strong>114 total servings</strong> of delectable, healthy fruits and vegetables to help you survive the crisis we all know is coming.</p>
+
+	<h2 class="darkRed text-center">So, How Much Does This Cost?</h2>
+	<p>It’s no secret that fruits and vegetables are essential to any diet.</p>
+	<p>And when a natural disaster happens, there will not be opportunities to go to the nearest store and pick up some fruits and vegetables.</p>
+	<p>Well, you know me well enough by now that I’ve make it my mission to help get as many Food4Patriots kits as I can into the hands of my fellow patriots and, because I have been blessed with the ability to buy in bulk, I’m able to cut out the middle man and give these kits away for pennies on the dollar of what they’d normally cost.</p>
+	<p>And for a limited time, you can get our Fruit, Veggie and Snack Mix for the <strong>special promotional price of just $97.00!</strong></p>
+	<p><strong>That’s Just $1.18 Per Serving!</strong></p>
+	<p>Normally, this kit would cost around $147, but for a limited time, I want to make it available to anyone that wants to ensure their family has enough vitamin enriched foods in their stockpile.</p>
+	<p>Be sure to jump on this opportunity folks… food prices are going through the roof right now, and I’m struggling to keep these kits in stock.</p>
+	<p>Get your Fruit, Veggie and Snack Mix at this special introductory price while it is still available…</strong></p>
+	<p>I’m not sure how long we will be able to offer this important kit for $97 or $1.18 per serving.</p>
+	<p><strong>Click the <strong>"Add To Cart"</strong> button below now, and I'll rush your kit to your front door right away!</strong></p>
+
+
+	<div class="margin-tb-50 center-block text-center">
+		<img class="img-responsive center-block margin-tb-20" style="width: 100%" src="/media/images/f4p/f4p-fvs-kit-tote-03.jpg" alt="">
+		<a href="<?php echo $offerUrl; ?>"><img src="/assets/images/buttons/btn-orange-add-cart-fruitveggie.jpg" class="img-responsive center-block" alt="Add To Cart"><strong>Add to Cart - $97</strong></a>
+	</div>
+
+	<p>And just to make this a complete no-brainer for you...</p>
+
+	<h2 class="darkRed text-center">You Are 100% Protected By My Outrageous Double Guarantee.</h2>
+	<div class="outLineBoxDarkBlue">
+		<p><img src="/media/images/misc/seal-guarantee-satisfaction.jpg" alt="Guarantee #1" class="pull-left img-responsive media margin-t-20">
+		<h3>Guarantee #1:</h3> This is a 100% money back guarantee. No questions asked. If for any reason you’re not satisfied with your Fruit, Veggie and Snack Mix, just return it within 60 days of purchase and I’ll refund 100% of your purchase price. If you try it and decide it’s not as delicious and nutritious as I promised, you can have your money back </p>
+		<div class="clearfix"></div>
+	</div>
+
+	<div class="outLineBoxDarkBlue">
+		<p><img src="/media/images/misc/seal-guarantee-money.jpg" alt="Guarantee #2" class="pull-left img-responsive media margin-t-20">
+		<h3>Guarantee #2:</h3> This is an unheard of 300% money back guarantee. It’s in addition to guarantee #1. If you open any of your Fruit, Veggie and Snack Mix anytime <strong>in the next 25 years</strong> and find that your food has spoiled or gone bad, you can return your entire Fruit, Veggie and Snack Mix and I will <strong>triple</strong> your money back!</p>
+		<p>That’s how confident I am that this food will remain just as delicious and nutritious for the next 25 years as it is on the day you buy it. Some of my friends said I was crazy to offer this double guarantee, but to be honest I’m not really worried about it, because I am so confident you’re going to see the value in your Fruit, Veggie and Snack Mix as soon as you have it in your hands.</p>
+	</div>
+
+	<h2 class="darkRed text-center">Here’s What Patriots Just Like You Have To Say About Our Fruit, Veggie and Snack Mix…</h2>
+	<img class="img-responsive center-block margin-tb-20" src="/media/images/f4p/f4p-fvs-testimonials-01.jpg" alt="Testimonial">
+	<img class="img-responsive center-block margin-tb-20" src="/media/images/f4p/f4p-fvs-testimonials-02.jpg" alt="Testimonial">
+	<img class="img-responsive center-block margin-tb-20" src="/media/images/f4p/f4p-fvs-testimonials-03.jpg" alt="Testimonial">
+
+	<p><strong><i>It’s all over the news… the worst drought to hit the US since the Dustbowl of the Great Depression is driving the cost of raw ingredients sky-high.</i></strong></p>
+	<p>Most folks recognize the need to “fill in the gaps” of their survival food stockpile to make sure their family has the most nutritious food available in a disaster.</p>
+	<p>Click the <strong>“Add to Cart”</strong> button below now, while I still have these in stock.</p>
+
+	<p>Hey listen… let’s get REAL… you don’t want to compromise on your family’s health or well-being, right?</p>
+	<p>I hate to be the bearer of bad news, but I just don’t know how long I’m going to be able to offer these Fruit, Veggie and Snack Mix at this low price, or even <strong><i>at all… </i></strong></p>
+	<p>Survival food supplies are vanishing before our eyes.</p>
+	<p>So claim your kit NOW by clicking the <strong>“Add to Cart”</strong> button, and have true peace of mind your loved ones WON’T starve in the coming crisis…</p>
+	<img class="img-responsive center-block margin-tb-20" style="width: 100%" src="/media/images/f4p/f4p-fvs-kit-tote-03.jpg" alt="">
+	<div class="margin-tb-50 center-block text-center">
+		<a href="<?php echo $offerUrl; ?>"><img src="/assets/images/buttons/btn-orange-add-cart-fruitveggie.jpg" class="img-responsive center-block" alt="Add To Cart"><strong>Add to Cart - $97</strong></a>
+	</div>
+
+	<img class="img-responsive center-block margin-tb-20" src="/media/images/f4p/f4p-testimonials-07b.jpg" alt="Testimonial">
+	<img class="img-responsive center-block margin-tb-20" src="/media/images/f4p/f4p-testimonials-26.jpg" alt="Testimonial">
+
+</div>
