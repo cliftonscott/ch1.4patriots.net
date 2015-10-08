c92b650654a5d3f99103fc0d1b68d9fac9360e96
tammygibbins <tammy.gibbins@4patriots.com>
2015-10-08 12:02:23 -0400

    FFP-681 congruency changes

--- checkout/protein/oto/f4p-4week-kit-discount.php
+++ checkout/protein/oto/f4p-4week-kit-discount.php
@@ -87,7 +87,7 @@ if($isUpgrade) {
 		<p>That&rsquo;s $50.00 – a massive 25% savings – off the already-discounted price. </p>
 		<p>I was only able to secure a limited quantity of these 4-Week Food4Patriots kits and it&rsquo;s been our most popular upgrade, so I don&rsquo;t know how long I&rsquo;m going to have them available. To make sure that you don&rsquo;t miss out on getting yours, go ahead and click the big orange &ldquo;Click To Accept&rdquo; button below to add the 4-Week Food4Patriots to your order today!</p>
 		<p>The 4-Week Food4Patriots kit will help secure your stockpile faster and protect you and your family from whatever crisis may come. You&rsquo;ll be on the &ldquo;fast track&rdquo; to securing your food stockpile.</p>
-		<p><?php echo $firstName;?>, this is your last chance for this special 1-time discount, so you need to act now. To get the 4-Week Food4Patriots kit at $50.00 less than everybody else pays (that&rsquo;s 25% off), click the big orange &ldquo;Click To Accept&rdquo; button below.</p>
+		<p><?php echo $firstName;?>, this is your last chance for this special one-time discount, so you need to act now. To get the 4-Week Food4Patriots kit at $50.00 less than everybody else pays (that&rsquo;s 25% off), click the big orange &ldquo;Click To Accept&rdquo; button below.</p>
 		<div>
 <?php
 if($isUpgrade) {
