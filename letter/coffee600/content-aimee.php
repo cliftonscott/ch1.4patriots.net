25b6e8b0007b054aec50b53c5b0ae64db741734f
huskilla <chris.robison@4patriots.com>
2015-10-09 12:49:52 -0500

    FFP-930 Updated content

--- letter/coffee600/content-aimee.php
+++ letter/coffee600/content-aimee.php
@@ -195,15 +195,7 @@ if(!$pageGreeting) {
 							<tr>
 								<td><span style="text-align:center;padding-top:10px;padding-bottom:10px;">
 								<select name="quantity" id="quantity" style="width:50px;margin-top:3px;margin-bottom:3px;" onchange="productChange();">
-									<?php
-									if(!$maxQuantity) {
-										$maxQuantity = 5;
-									}
-									for ($i=1; $i<=$maxQuantity; $i++)
-									{
-										echo "<option>". $i . "</option>";
-									}
-									?>
+
 								</select>
 								</span></td>
 							</tr>
