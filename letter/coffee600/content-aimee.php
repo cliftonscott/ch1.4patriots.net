ea186bccf81097471e9684520c4d8792cc9e48d3
huskilla <chris.robison@4patriots.com>
2015-10-09 13:29:56 -0500

    FFP-930 Updated content

--- letter/coffee600/content-aimee.php
+++ letter/coffee600/content-aimee.php
@@ -169,7 +169,7 @@ if(!$pageGreeting) {
 			<p>Get your additional supply right away, Click the <strong>“Add to Cart”</strong> button below now:</p>
 		</div>
 	</div>
-	<div style="margin-top: -100px" class="container oto-width">
+	<div style="margin-top: -75px" class="container oto-width">
 		<div class="margin-tb-50 center-block text-center">
 			<img class="img-responsive center-block margin-tb-20" style="width: 100%" src="/media/images/f4p/f4p-coffee-kit-04.jpg" alt="">
 			<a href="<?php echo $offerUrl; ?>"><img src="/assets/images/buttons/btn-orange-add-cart-02.jpg" class="img-responsive center-block" alt="Add To Cart"></a>
