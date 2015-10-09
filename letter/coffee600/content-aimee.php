ec2f99cfa35a51ebab05076fa44985782c52b23d
huskilla <chris.robison@4patriots.com>
2015-10-09 12:47:24 -0500

    FFP-930 Updated content

--- letter/coffee600/content-aimee.php
+++ letter/coffee600/content-aimee.php
@@ -190,7 +190,17 @@ if(!$pageGreeting) {
 						<input type="image" src="/assets/images/buttons/btn-orange-add-cart-02.jpg" name="submit" class="img-responsive center-block 1yearbuy"/>
 					</div>
 
-
+					<div>
+						<table  style="margin-right:auto;margin-left:auto;" border="0" cellspacing="0" cellpadding="0">
+							<tr>
+								<td><div style="text-align: left;margin-right:10px;"><strong>Quantity:</strong></div></td>
+								<td><span style="text-align:center;padding-top:10px;padding-bottom:10px;">
+								<select name="quantity" id="quantity" style="width:50px;margin-top:3px;margin-bottom:3px;" onchange="productChange();">
+								</select>
+								</span></td>
+							</tr>
+						</table>
+					</div>
 
 					<div style="position:relative;text-align:left;margin-top:10px;max-width:600px;margin-right:auto;margin-left:auto;">
 						<div style="float:left;margin-right:5px;">
