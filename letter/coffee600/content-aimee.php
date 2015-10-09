d1d7615e8f8e29b3583cf3db4024e98568dfd3f3
huskilla <chris.robison@4patriots.com>
2015-10-09 12:56:22 -0500

    FFP-930 Updated content

--- letter/coffee600/content-aimee.php
+++ letter/coffee600/content-aimee.php
@@ -170,48 +170,21 @@ if(!$pageGreeting) {
 		</div>
 	</div>
 
-	<div class="container oto-width">
-		<div>
-			<div>
-				<img class="img-responsive center-block small" src="/media/images/f4p/f4p-coffee-kit-04.jpg"  alt="Food4Patriots Coffee Kit"/>
-			</div>
-			<a id="accept"></a>
-			<?php
-			if($isUpgrade) {
-				?>
-				<div style="text-align:center;">
-					<a href="/order/<?php echo $productDataObj->productId;?>"><img src="/assets/images/buttons/btn-orange-add-cart-02.jpg" name="submit" class="img-responsive center-block"></a>
-				</div>
-			<?php
-			} else {
-				?>
-				<form action="/checkout/process.php" method="post" accept-charset="utf-8" id="optin-form">
-					<div style="text-align:center;">
-						<input type="image" src="/assets/images/buttons/btn-orange-add-cart-02.jpg" name="submit" class="img-responsive center-block 1yearbuy"/>
-					</div>
-					<?php
-					if(!$maxQuantity) {
-						$maxQuantity = 5;
-					}
-					for ($i=1; $i<=$maxQuantity; $i++)
-					{
-						echo "<option>". $i . "</option>";
-					}
-					?>
 
-					<div style="position:relative;text-align:left;margin-top:10px;max-width:600px;margin-right:auto;margin-left:auto;">
-						<div style="float:left;margin-right:5px;">
-							<img src="/assets/images/misc/yes-01.jpg" width="74" height="34" alt="Yes">
-						</div>
-						<div id="terms"></div>
-						<script>
-							productChange();
-						</script>
-					</div>
-				</form>
-			<?php
-			}
-			?>
+			<div class="margin-tb-50 center-block text-center">
+				<img class="img-responsive center-block margin-tb-20" style="width: 100%" src="/media/images/f4p/f4p-coffee-kit-04.jpg" alt="">
+				<a href="<?php echo $offerUrl; ?>"><img src="/assets/images/buttons/btn-orange-add-cart-02.jpg" class="img-responsive center-block" alt="Add To Cart"><strong>Add to Cart - $97</strong></a>
+			</div>
+			<div style="position:relative;text-align:left;margin-top:10px;max-width:600px;margin-right:auto;margin-left:auto;">
+				<div style="float:left;margin-right:5px;">
+					<img src="/assets/images/misc/yes-01.jpg" width="74" height="34" alt="Yes">
+				</div>
+				<div id="terms"></div>
+				<script>
+					productChange();
+				</script>
+			</div>
+
 			<div class="outLineBoxDarkBlueCoffee">
 				<div>
 					<img src="/media/images/misc/seal-guarantee-satisfaction.jpg" alt="Guarantee #1" class="pull-left img-responsive media" style="padding-right:10px;">
