3d87eaf7ef1b53b9cb5b2c30656cfa7055bc646e
jeremy <jeremy@rebootmarketing.com>
2015-10-12 16:58:41 -0500

    FFP-457 Checkout V2 Updates

--- js/audio-gb-v2.js
+++ js/audio-gb-v2.js
@@ -4,16 +4,16 @@ function playAudio(what) {
 function stopAudio(what) {
 	document.getElementById(what+"AudioSrc").pause();
 	document.getElementById("GBCheckoutAudioControl").src = "/media/images/checkout-v2/gb-button.png";
-	document.getElementById("frankCheckoutAudioControl").src = "/assets/images/misc/speaker_off.gif";
+	document.getElementById("frankCheckoutAudioControl").src = "/assets/images/misc/speaker_off_white.gif";
 }
 function toggleAudio(what) {
 	if(document.getElementById(what+"AudioSrc").paused) {
 		document.getElementById(what+"AudioSrc").play();
 		document.getElementById("GBCheckoutAudioControl").src = "/media/images/checkout-v2/gb-button-animation.gif";
-		document.getElementById("frankCheckoutAudioControl").src = "/assets/images/misc/speaker_on.gif";
+		document.getElementById("frankCheckoutAudioControl").src = "/assets/images/misc/speaker_on_white.gif";
 	} else {
 		document.getElementById(what+"AudioSrc").pause();
 		document.getElementById("GBCheckoutAudioControl").src = "/media/images/checkout-v2/gb-button.png";
-		document.getElementById("frankCheckoutAudioControl").src = "/assets/images/misc/speaker_off.gif";
+		document.getElementById("frankCheckoutAudioControl").src = "/assets/images/misc/speaker_off_white.gif";
 	}
 }
