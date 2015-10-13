77a0b717d6506cfdf6055fbb7f5d18c025d701de
huskilla <chris.robison@4patriots.com>
2015-10-13 13:55:11 -0500

    FFP-931 Updated content

--- quiz/1/index.php
+++ quiz/1/index.php
@@ -12,11 +12,13 @@ include_once ('template-top.php');
 			<div class="row">
 				<div class="col-sm-12 col-md-12">
 					<div>
-						<h1 class="darkRed text-center">Are You And Your Family Prepared<br class="hidden-xs"> To Survive A Government<br class="hidden-xs"> Controlled Society?</h1>
+						<h1 class="darkRed text-center">Are You And Your Family <u>Prepared</u><br class="hidden-xs"> To Survive A Government<br class="hidden-xs"> Controlled Society?</h1>
 					</div>
 					<p>527,197 people have taken the <strong><i>Government Survival Quiz.</i></strong> Start the quiz below to see if you and your family are prepared to survive if the government finally gets <strong>complete control</strong> of America...</p>
 					<div id="start-quiz">
-						<a href="quiz.php"><button class="start-btn" style="margin-top:8px; font-weight:bold;">Start The Quiz</button><br><p class="click-here" style="font-size: 20pt; padding-bottom: 10px;">(Click Here to Start The Quiz)</p></a>
+						<img style="width: 100px;" src="/media/images/misc/arrow-l-animated.gif">
+						<a href="quiz.php"><button class="start-btn" style="margin-top:8px; margin-bottom:20px; font-weight:bold;">Start The Quiz</button></a>
+						<img style="width: 100px;"  src="/media/images/misc/arrow-l-animated.gif" class="img-flip">
 					</div>
 					<p>In this FREE, <strong>33 second</strong> quiz you&#39;ll get to see if you and your family are truly prepared to survive a major crisis such as a government takeover, economic collapse or even martial law.</p>
 					<p>Most people think they&#39;re prepared for these scenarios because they already have water, food, guns and a back-up generator stored in their house...</p>
@@ -24,7 +26,9 @@ include_once ('template-top.php');
 					<p>If you want to be totally self-reliant and able to protect your family when a crisis hits (without having to rely on the Federal Government to take care of you) then do yourself a favor and take the FREE, 33 second, <strong>Government Survival Quiz</strong> below...</p>
 					<p>Simply click the orange <strong>&#34;Start The Quiz&#34;</strong> button below to see if you and your family are truly prepared to survive a government gone wild...</p>
 					<div id="start-quiz">
-						<a href="quiz.php"><button class="start-btn" style="margin-top:8px; font-weight:bold;">Start The Quiz</button><br><p class="click-here" style="font-size: 20pt; padding-bottom: 10px;">(Click Here to Start The Quiz)</p></a>
+						<img style="width: 100px;" src="/media/images/misc/arrow-l-animated.gif">
+						<a href="quiz.php"><button class="start-btn" style="margin-top:8px; margin-bottom:20px; font-weight:bold;">Start The Quiz</button></a>
+						<img style="width: 100px;"  src="/media/images/misc/arrow-l-animated.gif" class="img-flip">
 					</div>
 					<p><i>This quiz is 100% FREE and takes only 33 seconds to complete. All answers are kept completely private and secure.</i></p>
 					<img style="padding-top: 10px;" src="/media/images/misc/quiz-as-seen-advertised-on-01-grayscale.jpg" class="img-responsive center-block">
