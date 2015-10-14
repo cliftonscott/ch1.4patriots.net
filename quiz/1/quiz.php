3e0c4d6610e7a6e3194db99ca137507e361caffb
jeremy <jeremy@rebootmarketing.com>
2015-10-14 17:04:33 -0500

    FFP-993 Quiz Split Test Version

--- quiz/1/quiz.php
+++ quiz/1/quiz.php
@@ -8,6 +8,43 @@
 $quizName = "Government Survival Quiz";
 include_once ('template-top.php');
 ?>
+<?php if ($_SESSION["variation"] === "2"): ?>
+	<style>
+		p{
+			font-size: 14pt;
+		}
+		#background-img{
+			background: url(/assets/images/video-lander/img-background-police.jpg) no-repeat center center fixed;
+			-webkit-background-size: cover;
+			-moz-background-size: cover;
+			-o-background-size: cover;
+			background-size: cover;
+		}
+		.navbar {
+			margin-top: 25px;
+			background-color: transparent;
+			border-radius: 0px;
+			margin-bottom: 0px;
+			max-width: 958px;
+			margin-right: auto;
+			margin-left: auto;
+			border: none;
+		}
+
+		.navbar-default {
+			background-image: none;
+			border-bottom-color: transparent;
+			border: none;
+		}
+		.container-main {
+			border-radius: 10px;
+		}
+		h1{
+			font-family: 'Oswald', sans-serif;
+		}
+	</style>
+	<link href='https://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+<?php endif; ?>
 <div class="container-main">
 	<div class="container oto-width">
 		<div class="row">
--- templates/template-bottom.php
+++ templates/template-bottom.php
@@ -13,6 +13,8 @@ if(!empty($template["formType"])) {
 $suppressOlark = array (
 	"/video/index.php",
 	"/video/t1/index.php",
+	"/quiz/1/index.php",
+	"/quiz/1/quiz.php",
 );
 if(!in_array($_SERVER["PHP_SELF"], $suppressOlark)) {
 	include_once("chat-olark.php");
