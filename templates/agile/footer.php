97d2279e06683b9a365919d85b1e53a307a07fb8
Brad Forbes <bradbforbes@gmail.com>
2015-10-23 11:09:40 -0500

    FFP-937 Update Olark to only load on checkout page

--- templates/agile/footer.php
+++ templates/agile/footer.php
@@ -1,15 +1,9 @@
 <?php include_once("ga-data-layer.php"); ?>
 
 <?php
-$suppressOlark = array (
-	"/video/index.php",
-	"/video/t1/index.php",
-	"/quiz/1/index.php",
-	"/quiz/1/quiz.php",
-);
-if(!in_array($_SERVER["PHP_SELF"], $suppressOlark)) {
+if ($page == "checkout"):
 	include_once("chat-olark.php");
-}
+endif;
 ?>
 
 </html>
