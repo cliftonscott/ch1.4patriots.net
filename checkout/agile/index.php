9129d48ed31982227f3353e8e16b60fd519e73da
Brad Forbes <bradbforbes@gmail.com>
2015-10-23 14:29:49 -0500

    FFP-937 Fix timer countdown starting time

--- checkout/agile/index.php
+++ checkout/agile/index.php
@@ -324,6 +324,74 @@ margin-top: 109px;" src="/assets/images/misc/speaker_off.gif" width="36" height=
 
 <script>
 
+	/*
+	 |--------------------------------------------------------------------------
+	 | Floating Timer
+	 |--------------------------------------------------------------------------
+	 |
+	 | This establishes the behavior of the floating timer.
+	 |
+	 */
+
+	var sec = 0;	// set the seconds - set back to 40
+	var min = <?php echo $template["floatingTimer"];?>;	// set from within the template
+
+	function countDown() {
+		console.log('min >');console.log(min);
+		sec--;
+		if (sec == -01) {
+			sec = 59;
+			min = min - 1;
+		} else {
+			min = min;
+		}
+		if (sec<=9) {
+			sec = "0" + sec;
+		}
+
+		time = (min<=9 ? "0" + min : min) + ":" + sec;
+
+		if (document.getElementById) {
+			document.getElementById('theTime').innerHTML = time;
+		}
+
+		SD=window.setTimeout("countDown();", 1000);
+
+		if (min == '00' && sec == '00') {
+			sec = "00";
+			window.clearTimeout(SD);
+			exitconfirm();
+		}
+	}
+
+	countDown();
+
+	function exitconfirm() {
+		$('#csrModal').modal('show');
+	}
+
+	floatingMenu.add('floatingTimer', {
+		// Represents distance from left or right browser window
+		// border depending upon property used. Only one should be
+		// specified.
+		targetLeft: 5,
+		//targetRight: 10,
+
+		// Represents distance from top or bottom browser window
+		// border depending upon property used. Only one should be
+		// specified.
+		//targetTop: 10,
+		targetBottom: 5,
+
+		// Uncomment one of those if you need centering on
+		// X- or Y- axis.
+		// centerX: true,
+		// centerY: true,
+
+		// Remove this one if you don't want snap effect
+		snap: true
+	});
+
 	window.onbeforeunload = grayOut;
 	function grayOut(){
 		var ldiv = document.getElementById('LoadingDiv');
@@ -511,75 +579,6 @@ margin-top: 109px;" src="/assets/images/misc/speaker_off.gif" width="36" height=
 				if(isMobile() === false) {
 					toggleAudio('frankCheckout');
 				}
-			});
-
-
-			/*
-			 |--------------------------------------------------------------------------
-			 | Floating Timer
-			 |--------------------------------------------------------------------------
-			 |
-			 | This establishes the behavior of the floating timer.
-			 |
-			 */
-
-			var sec = 0;	// set the seconds - set back to 40
-			var min = <?php echo $template["floatingTimer"];?>;	// set from within the template
-
-				function countDown() {
-					console.log('min >');console.log(min);
-					sec--;
-					if (sec == -01) {
-						sec = 59;
-						min = min - 1;
-					} else {
-						min = min;
-					}
-					if (sec<=9) {
-						sec = "0" + sec;
-					}
-
-					time = (min<=9 ? "0" + min : min) + ":" + sec;
-
-					if (document.getElementById) {
-						document.getElementById('theTime').innerHTML = time;
-					}
-
-					SD=window.setTimeout("countDown();", 1000);
-
-					if (min == '00' && sec == '00') {
-						sec = "00";
-						window.clearTimeout(SD);
-						exitconfirm();
-					}
-				}
-
-			countDown();
-
-			function exitconfirm() {
-				$('#csrModal').modal('show');
-			}
-
-			floatingMenu.add('floatingTimer', {
-				// Represents distance from left or right browser window
-				// border depending upon property used. Only one should be
-				// specified.
-				targetLeft: 5,
-				//targetRight: 10,
-
-				// Represents distance from top or bottom browser window
-				// border depending upon property used. Only one should be
-				// specified.
-				//targetTop: 10,
-				targetBottom: 5,
-
-				// Uncomment one of those if you need centering on
-				// X- or Y- axis.
-				// centerX: true,
-				// centerY: true,
-
-				// Remove this one if you don't want snap effect
-				snap: true
 			});
 
 		});
