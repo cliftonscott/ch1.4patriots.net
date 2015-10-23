0d12aee064bc686f955f7a9de8ccb26e420bed8c
Brad Forbes <bradbforbes@gmail.com>
2015-10-23 09:53:16 -0500

    FFP-937 Add back in Olark loading

--- templates/agile/footer.php
+++ templates/agile/footer.php
@@ -1,3 +1,15 @@
 <?php include_once("ga-data-layer.php"); ?>
 
+<?php
+$suppressOlark = array (
+	"/video/index.php",
+	"/video/t1/index.php",
+	"/quiz/1/index.php",
+	"/quiz/1/quiz.php",
+);
+if(!in_array($_SERVER["PHP_SELF"], $suppressOlark)) {
+	include_once("chat-olark.php");
+}
+?>
+
 </html>
