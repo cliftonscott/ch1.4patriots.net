a79f55f7e799f4611620c5a839a1c1a70cca3f39
tammygibbins <tammy.gibbins@4patriots.com>
2015-12-04 10:02:50 -0500

    FFP-952 uncapitalized words

--- letter/fruitveggie/content.php
+++ letter/fruitveggie/content.php
@@ -23,7 +23,7 @@
 	<h2 class="darkRed text-center">Eating Fruits and Vegetables Can Help Maximize the Nutrition of Your Survival Food!</h2>
 	<img class="img-responsive pull-right img-padding-left" src="/media/images/f4p/letter/f4p-letter-fvs-basket.jpg" alt="Food4Patriots 1 Year Kit">
 	<p>When you add high-quality fruits and vegetables to your existing survival food stockpile, you’ll enjoy knowing you’re optimizing the level of nutrients your body needs.</p>
-	<p>What Are Some Other Crucial Benefits of fruits and vegetables?</p>
+	<p>What are some other crucial benefits of fruits and vegetables?</p>
 	<ul class="fa-ul fa-ul-space">
 		<li><i class="fa-li fa fa-check"></i><strong>Better regularity</strong> Fiber keeps your digestive tract healthy and helps move waste out of your system, which helps you feel your best and be “light on your feet” – which is critical when you need to move fast.</li>
 		<li><i class="fa-li fa fa-check"></i><strong>Faster healing and boosted immunity</strong> Our bodies don’t make all the vitamins we need, so we need to eat foods that are rich in them to recover faster from infections and illness, which helps if you’re injured or catch a nasty bug.</li>
