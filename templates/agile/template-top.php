423e419b96e549f993145f1f7cce55f3a99e8e9f
jeremy <jeremy@rebootmarketing.com>
2016-03-08 13:37:58 -0600

    FFP-1318 JV-66 F4P Letter - Mobile Geo Targeting

--- templates/agile/template-top.php
+++ templates/agile/template-top.php
@@ -120,7 +120,9 @@ if(!empty($customerDataObj->shippingCity)) {
 		include_once("exit-pop-video.php");
 	}
 	?>
-
+	<?php if (JV::in("66-geo")): /*JV-66 MOBILE ONLY GEOTARGETING*/?>
+	<script type='text/javascript' src='https://www.geolify.com/geojavascript.php?id=12428'></script>
+	<?php endif ?>
 </head>
 <body>
 <?php include_once("ga-data-layer.php"); ?>
