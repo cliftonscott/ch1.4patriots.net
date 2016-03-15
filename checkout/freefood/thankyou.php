ac1502d5176357a0037b445ca3cdf39000173026
huskilla <chris.robison@4patriots.com>
2016-03-15 10:40:01 -0500

    FFP-1277 Updated content

--- checkout/freefood/thankyou.php
+++ checkout/freefood/thankyou.php
@@ -62,22 +62,22 @@ include_once ('template-header.php'); /*Add template-header-nav.php to add top m
 					<p>Here are all your Food4Patriots bonus manuals. </p>
 					<div>
 						<div class="row">
-							<div class="col-sm-12 col-md-4 text-center"><a target="_blank" href="/media/pdf/F4P-10-Items-After-Crisis.pdf"><img height="231" src="/media/images/f4p/f4p-eBook-10-items-after-crisis.jpg" alt="" /></a></div>
+							<div class="col-sm-12 col-md-4 text-center"><a target="_blank" href="/media/pdf/F4P-10-Items-After-Crisis.pdf"><img height="231" src="/media/images/bonuses/f4p-10-items-after-crisis.jpg" alt="" /></a></div>
 							<div class="col-sm-12 col-md-8 margin-tb-50"><a target="_blank" href="/media/pdf/F4P-10-Items-After-Crisis.pdf">Right Click Here for Top 10 Items Sold Out After Crisis</a><br />
 								This is manual shows you everything you need to stock to survive a disaster.</div>
 						</div>
 						<div class="row">
-							<div class="col-sm-12 col-md-4 text-center"><a target="_blank" href="/media/pdf/F4P-Cut-Grocery-Bills-Half.pdf"><img src="/media/images/f4p/f4p-eBook-cut-grocery-bills-half.jpg" alt=""></a></div>
+							<div class="col-sm-12 col-md-4 text-center"><a target="_blank" href="/media/pdf/F4P-Cut-Grocery-Bills-Half.pdf"><img src="/media/images/bonuses/f4p-cutting-grocery-bill-in-half.jpg" alt=""></a></div>
 							<div class="col-sm-12 col-md-8 margin-tb-50"><a target="_blank" href="/media/pdf/F4P-Cut-Grocery-Bills-Half.pdf">Right Click here for How To Cut Your Grocery Bills In Half Guide</a><br />
 								Here's the step-by-step manual showing you how to save on your grocery bill.</div>
 						</div>
 						<div class="row">
-							<div class="col-sm-12 col-md-4 text-center"><a target="_blank" href="/media/pdf/F4P-Survival-Garden-Guide.pdf"><img height="231" src="/media/images/f4p/f4p-eBook-the-survival-garden-guide.jpg" alt="" /></a></div>
+							<div class="col-sm-12 col-md-4 text-center"><a target="_blank" href="/media/pdf/F4P-Survival-Garden-Guide.pdf"><img height="231" src="/media/images/bonuses/f4p-survival-garden-guide.jpg" alt="" /></a></div>
 							<div class="col-sm-12 col-md-8 margin-tb-50"><a target="_blank" href="/media/pdf/F4P-Survival-Garden-Guide.pdf">Right Click Here For The Survival Garden Guide</a><br />
 								This is the manual showing you the critical steps necessary to plant and grow your survival garden.</div>
 						</div>
 						<div class="row">
-							<div class="col-sm-12 col-md-4 text-center"><a target="_blank" href="/media/pdf/F4P-Water-Survival-Guide.pdf"><img src="/media/images/f4p/f4p-eBook-the-water-survival-guide.jpg" alt=""></a></div>
+							<div class="col-sm-12 col-md-4 text-center"><a target="_blank" href="/media/pdf/F4P-Water-Survival-Guide.pdf"><img src="/media/images/bonuses/f4p-the-water-survival-guide.jpg" alt=""></a></div>
 							<div class="col-sm-12 col-md-8 margin-tb-50"><a target="_blank" href="/media/pdf/F4P-Water-Survival-Guide.pdf"> Right Click Here For Water Survival Manual</a><br />
 								This is the manual showing you the critical steps necessary to survive a water crisis.</div>
 						</div>
--- templates/recommendations/thankyou-ads.php
+++ templates/recommendations/thankyou-ads.php
@@ -4,7 +4,7 @@ $typAds[2] = "https://securetrack1.com/base.php?c=107&key=98081de8a415bfe1da0f08
 $typAds[3] = "http://jump.patriothealthinstitute.com/aff_c?offer_id=48&aff_id=14&aff_sub=f4ptypagetext";
 ?>
 <h2 class="darkRed text-center">Customers Who Bought Food4Patriots Also Bought These Products</h2>
-<p>Based on your purchase, you may also be interested in:</p>
+<p style="text-align: left">Based on your purchase, you may also be interested in:</p>
 <!-- AD POSITION 1 -->
 <p class="text-center" style="margin-bottom:40px;">
 	<iframe src="//reboot.sitescout.com/disp?pid=1E9023ACCD" width='600' height='360' marginwidth='0' marginheight='0' scrollbars='0' scrolling='no' frameborder='0' bordercolor='#000000' vspace='0' hspace='0'></iframe></p>
