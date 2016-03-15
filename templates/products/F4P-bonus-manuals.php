7c9aab85d9136f04265a61319ffc865f55d17d40
caseywittner <casey.wittner@4patriots.com>
2016-03-15 17:34:49 -0500

    FFP-1299 TYP Cleanup

--- templates/products/F4P-bonus-manuals.php
+++ templates/products/F4P-bonus-manuals.php
@@ -3,25 +3,20 @@
 		<p>Here are all your Food4Patriots bonus manuals. </p>
 	<div>
 		<div class="row">
-			<div class="col-sm-12 col-md-4 text-center"><a target="_blank" href="/media/pdf/F4P-10-Items-After-Crisis.pdf"><img height="231" src="/media/images/f4p/f4p-eBook-10-items-after-crisis.jpg" alt="" /></a></div>
-			<div class="col-sm-12 col-md-8 margin-tb-50"><a target="_blank" href="/media/pdf/F4P-10-Items-After-Crisis.pdf">Right Click Here for Top 10 Items Sold Out After Crisis</a><br />
+			<div class="col-sm-12 col-md-2 text-center"><a target="_blank" href="/media/pdf/F4P-10-Items-After-Crisis.pdf"><img class="img-responsive center-block" src="/media/images/f4p/f4p-bonus-top-10-sold-out-86x99.png" alt="" /></a></div>
+			<div class="col-sm-12 col-md-4 margin-b-20 right-border"><a target="_blank" href="/media/pdf/F4P-10-Items-After-Crisis.pdf">Top 10 Items Sold Out After Crisis</a><br />
 				This is manual shows you everything you need to stock to survive a disaster.</div>
-		</div>
-		<div class="row">
-			<div class="col-sm-12 col-md-4 text-center"><a target="_blank" href="/media/pdf/F4P-Cut-Grocery-Bills-Half.pdf"><img src="/media/images/f4p/f4p-eBook-cut-grocery-bills-half.jpg" alt=""></a></div>
-			<div class="col-sm-12 col-md-8 margin-tb-50"><a target="_blank" href="/media/pdf/F4P-Cut-Grocery-Bills-Half.pdf">Right Click here for How To Cut Your Grocery Bills In Half Guide</a><br />
+			<div class="col-sm-12 col-md-2 text-center"><a target="_blank" href="/media/pdf/F4P-Cut-Grocery-Bills-Half.pdf"><img class="img-responsive center-block" src="/media/images/f4p/f4p-bonus-grocery-bills-half-86x99.png" alt=""></a></div>
+			<div class="col-sm-12 col-md-4 margin-b-20"><a target="_blank" href="/media/pdf/F4P-Cut-Grocery-Bills-Half.pdf">How To Cut Your Grocery Bills In Half Guide</a><br />
 				Here's the step-by-step manual showing you how to save on your grocery bill.</div>
 		</div>
 		<div class="row">
-			<div class="col-sm-12 col-md-4 text-center"><a target="_blank" href="/media/pdf/F4P-Survival-Garden-Guide.pdf"><img height="231" src="/media/images/f4p/f4p-eBook-the-survival-garden-guide.jpg" alt="" /></a></div>
-			<div class="col-sm-12 col-md-8 margin-tb-50"><a target="_blank" href="/media/pdf/F4P-Survival-Garden-Guide.pdf">Right Click Here For The Survival Garden Guide</a><br />
+			<div class="col-sm-12 col-md-2 text-center"><a target="_blank" href="/media/pdf/F4P-Survival-Garden-Guide.pdf"><img class="img-responsive center-block" src="/media/images/f4p/f4p-bonus-survival-garden-guide-86x99.png" alt="" /></a></div>
+			<div class="col-sm-12 col-md-4 margin-b-20 right-border"><a target="_blank" href="/media/pdf/F4P-Survival-Garden-Guide.pdf">The Survival Garden Guide</a><br />
 				This is the manual showing you the critical steps necessary to plant and grow your survival garden.</div>
-		</div>
-		<div class="row">
-			<div class="col-sm-12 col-md-4 text-center"><a target="_blank" href="/media/pdf/F4P-Water-Survival-Guide.pdf"><img src="/media/images/f4p/f4p-eBook-the-water-survival-guide.jpg" alt=""></a></div>
-			<div class="col-sm-12 col-md-8 margin-tb-50"><a target="_blank" href="/media/pdf/F4P-Water-Survival-Guide.pdf"> Right Click Here For Water Survival Manual</a><br />
+			<div class="col-sm-12 col-md-2 text-center"><a target="_blank" href="/media/pdf/F4P-Water-Survival-Guide.pdf"><img class="img-responsive center-block" src="/media/images/f4p/f4p-bonus-water-survival-guide-86x99.png" alt=""></a></div>
+			<div class="col-sm-12 col-md-4 margin-b-20"><a target="_blank" href="/media/pdf/F4P-Water-Survival-Guide.pdf">The Water Survival Guide</a><br />
 				This is the manual showing you the critical steps necessary to survive a water crisis.</div>
 		</div>
 	</div>
 </div>
-
