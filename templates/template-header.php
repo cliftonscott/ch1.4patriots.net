9569060fccfd2de653dbf7f4c3dc1dffdd355840
Michael Belcher <mbelcher@Michaels-Work-Macbook.local>
2016-03-16 15:02:54 -0500

    FFP-1276 update to template-header for new phone number

--- templates/template-header.php
+++ templates/template-header.php
@@ -1,34 +1,34 @@
 <div class="navbar navbar-inverse navbar-static-top">
-			<?php if($template["formType"] === "customerForm" && (!isset($template["unclickableCsrModal"]) || !$template["unclickableCsrModal"])) { echo "<div class='hidden-xs' style='float:right; cursor:pointer;'><a href='#csr' onclick='showCsrModal();'><img src='/assets/images/misc/customer-service-rep-01.png' alt='Have Questions? Give Us A Call' class='img-responsive'/></a></div>";} ?>
-			<?php if($template["formType"] === "customerForm" && (isset($template["unclickableCsrModal"]) && $template["unclickableCsrModal"] === true)) { echo "<div class='hidden-xs' style='float:right;'><img src='/assets/images/misc/customer-service-rep-01.png' alt='Have Questions? Give Us A Call' class='img-responsive'/></div>";} ?>
+	<?php if($template["formType"] === "customerForm" && (!isset($template["unclickableCsrModal"]) || !$template["unclickableCsrModal"])) { echo "<div class='hidden-xs' style='float:right; cursor:pointer;'><a href='#csr' onclick='showCsrModal();'><img src='/assets/images/misc/customer-service-rep-01.png' alt='Have Questions? Give Us A Call' class='img-responsive'/></a></div>";} ?>
+	<?php if($template["formType"] === "customerForm" && (isset($template["unclickableCsrModal"]) && $template["unclickableCsrModal"] === true)) { echo "<div class='hidden-xs' style='float:right;'><img src='/assets/images/misc/customer-service-rep-01.png' alt='Have Questions? Give Us A Call' class='img-responsive'/></div>";} ?>
 	<?php
 	if($template["floatingTimer"] > 0) {
 		include_once("snippets/timer-box.html");
 	}
 	?>
 	<div class="navbar-header">
-<?php
-	$logoLink = array (
-		"/checkout/alt/f4p-discount-offer.php",
-		"/checkout/alt/f4p-free-food-offer.php",
-	);
+		<?php
+		$logoLink = array (
+			"/checkout/alt/f4p-discount-offer.php",
+			"/checkout/alt/f4p-free-food-offer.php",
+		);
 
-if(substr_count($_SERVER["PHP_SELF"],"/") == 1) {
-	$logoImage = "<a href='/index.php'>";
-	$logoImage.= "<img src='/assets/images/logo-small.png' width='384' height='103' alt='power4patriots' class='img-responsive'/>";
-	$logoImage.= "</a>";
-}
-elseif(in_array($_SERVER["PHP_SELF"], $logoLink)) {
-	$logoImage = "<a href='/index.php'>";
-	$logoImage.= "<img src='/assets/images/logo-small.png' width='384' height='103' alt='power4patriots' class='img-responsive'/>";
-	$logoImage.= "</a>";
-}
-else {
-	$logoImage = "<img src='/assets/images/logo-small.png' width='384' height='103' alt='power4patriots' class='img-responsive'/>";
-}
-?>
+		if(substr_count($_SERVER["PHP_SELF"],"/") == 1) {
+			$logoImage = "<a href='/index.php'>";
+			$logoImage.= "<img src='/assets/images/logo-small.png' width='384' height='103' alt='power4patriots' class='img-responsive'/>";
+			$logoImage.= "</a>";
+		}
+		elseif(in_array($_SERVER["PHP_SELF"], $logoLink)) {
+			$logoImage = "<a href='/index.php'>";
+			$logoImage.= "<img src='/assets/images/logo-small.png' width='384' height='103' alt='power4patriots' class='img-responsive'/>";
+			$logoImage.= "</a>";
+		}
+		else {
+			$logoImage = "<img src='/assets/images/logo-small.png' width='384' height='103' alt='power4patriots' class='img-responsive'/>";
+		}
+		?>
 		<div class="navbar-brand"><?php echo $logoImage;?></div>
-		</div>
+	</div>
 
 </div>
 </div><!-- /container -->
@@ -36,8 +36,15 @@ else {
 <div class="navbar-phone-contain">
 	<div class="container nopadding">
 		<div class="navbar-phone">
-			<div id="phone-txt"><span class="glyphicon glyphicon-earphone"></span> Phone: 1-800-728-0008</div>
-			<div id="phone-button"><button type="button" class="btn btn-primary"><a href="tel:1-800-728-0008"><span class="glyphicon glyphicon-earphone"></span> Phone: 1-800-728-0008</a></button></div>
+			<?php
+			if ($productId = 17) {
+				echo("<div id=\"phone-txt\"><span class=\"glyphicon glyphicon-earphone\"></span> Phone: 1-800-555-5555</div>
+				<div id=\"phone-button\"><button type=\"button\" class=\"btn btn-primary\"><a href=\"tel:1-800-728-0008\"><span class=\"glyphicon glyphicon-earphone\"></span> Phone: 1-800-555-5555</a></button></div>");
+			}
+			else {
+				echo("<div id=\"phone-txt\"><span class=\"glyphicon glyphicon-earphone\"></span> Phone: 1-800-728-0008</div>
+				<div id=\"phone-button\"><button type=\"button\" class=\"btn btn-primary\"><a href=\"tel:1-800-728-0008\"><span class=\"glyphicon glyphicon-earphone\"></span> Phone: 1-800-728-0008</a></button></div>");
+			}  ?>
 		</div>
 	</div>
 </div><!-- /navbar phone -->
