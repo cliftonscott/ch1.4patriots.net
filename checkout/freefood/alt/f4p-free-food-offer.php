15efdec927d5f841b99c91d52f7491a5f2cb0a16
huskilla <chris.robison@4patriots.com>
2016-03-16 15:20:52 -0500

    FFP-1274 content update

--- checkout/freefood/alt/f4p-free-food-offer.php
+++ checkout/freefood/alt/f4p-free-food-offer.php
@@ -35,16 +35,17 @@ include_once ('template-top.php');
 						<div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><iframe src="//fast.wistia.net/embed/iframe/f1958k8cul?videoFoam=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="100%" height="100%"></iframe></div></div>
 					</div>
 					<div class="col-lg-12 margin-b-10">
-						<p>This isn&rsquo;t ordinary food... this is delicious, nutritious, good for 25 years &ldquo;super survival food&rdquo; that could literally save your life in a disaster! <br>
-						<br>
-						Get 4 full servings of <em>Frank's 5-Star Minestrone</em> rated for up to 25 years of long-term storage <strong>FREE</strong> (just cover postage) while supplies last! Here&rsquo;s exactly what you&rsquo;ll get:</p>
+						<p>This isn&rsquo;t ordinary food... it&rsquo;s delicious, nutritious, &ldquo;super survival food&rdquo; that could save your life in a disaster!</p>
+						<p>All Food4Patriots survival food is made in the USA from the finest ingredients and is rated for 25 years of storage. And it&rsquo;s yours<strong> FREE</strong> (just cover postage) while supplies last!</p>
+						<p>Get 16 servings of family-favorite dishes: Liberty Bell Potato Cheddar Soup, Blue Ribbon Creamy Chicken Rice, Travelers Stew, and Granny's Homestyle Potato Soup.&nbsp;</p>
+						<p>Here&rsquo;s exactly what you&rsquo;ll get:</p>
 						<ul>
-							<li>&ldquo;Disaster-Proof packing&rdquo;… a military-grade Mylar pouch keeps out air, moisture and light to keep your food nutritionally sound and tasting great for up to 25 years</li>
-							<li>Instantly protects you and your family from going hungry in a disaster… store in your home, car, office or give to a friend or family member</li>
-							<li>Simple to prepare -- just add boiling water, simmer, and serve in about 15 min</li>
-							<li>Rushed right to your door via USPS First Class Mail </li>
+							<li>&ldquo;Disaster-Proof&rdquo; packaging: Mylar pouches keep out air, moisture and light to keep your food fresh and delicious for up to 25 years</li>
+							<li>Instantly protects you and your family from going hungry in a disaster</li>
+							<li>Simple to prepare: just add boiling water, simmer, and serve</li>
+							<li>Rushed to your door via USPS First Class Mail</li>
 						</ul>
-						<strong>Regularly priced at $9.95, today it&rsquo;s FREE while supplies (limit one per household, just cover postage)&nbsp;&nbsp;--&nbsp;</strong>get yours right now before they&rsquo;re gone!
+						<p><strong>Regularly priced at $27.00 plus shipping, today it&rsquo;s FREE while supplies last </strong>(limit one per household, just cover postage)<strong>&nbsp;--&nbsp;</strong>get yours now before it&rsquo;s gone!</p>
 					</div>
 					<div class="col-lg-12 text-center hidden-xs">
 						<img src="/assets/images/buttons/btn-order-now-green-left-01.png" alt="Frank" class="img-responsive center-block">
@@ -58,7 +59,7 @@ include_once ('template-top.php');
 	</div>
 
 	<div class="guaranteeBox">
-		<p><img src="/assets/images/checkout/satisfaction-seal-02.png" alt="Frank" width="150" height="180" class="img-responsive pull-left"><strong><span class="brightBlue">Guarantee #1:</span></strong> This is a <strong>100% money back guarantee</strong>. No questions asked. If for any reason you&rsquo;re not satisfied with your Food4Patriots kit, just return it within 60 days of purchase and I&rsquo;ll refund 100% of your purchase.            </p>
+		<p><img src="/assets/images/checkout/satisfaction-seal-02.png" alt="Frank" width="150" height="180" class="img-responsive pull-left"><strong><span class="brightBlue">Guarantee #1:</span></strong> This is a <strong>100% money back guarantee</strong>. No questions asked. If for any reason you’re not satisfied with your Food4Patriots kit, just return it within 365 days of purchase and I’ll refund 100% of your purchase.</p>
 		<p>&nbsp;</p>
 		<p><strong><span class="brightBlue">Guarantee #2:&nbsp;</span></strong>This is an unheard of 300% money back guarantee. It&rsquo;s in addition to guarantee #1.&nbsp;If you open any of your Food4Patriots meals anytime&nbsp;<strong>in the next 25 years</strong>&nbsp;and find that your food has spoiled, you can return your entire Food4Patriots stockpile and I will&nbsp;<strong>triple</strong>&nbsp;your money back!</p>
 		<div class="clearfix"></div>
