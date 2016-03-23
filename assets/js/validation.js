5171fe8f2ca3c0583161275f3547562ec97e2dc1
huskilla <chris.robison@4patriots.com>
2016-03-23 11:47:41 -0500

    FFP-1275 form-val

---
+++ assets/js/validation.js
@@ -0,0 +1,74 @@
+var Validation = function () {
+
+    return {
+        
+        //Validation
+        initValidation: function () {
+	        $("#sky-form1").validate({
+	            // Rules for form validation
+	            rules:
+	            {
+	                "required":
+	                {
+	                    required: true
+	                },
+					"billing-zip":
+					{
+						required: true,
+						rangelength: [5, 7]
+					},
+					"firstName":
+	                {
+	                    required: true
+	                },
+					"firstNamecard":
+					{
+						required: true
+					},
+					"lastName":
+	                {
+	                    required: true
+	                },
+					"billing-address":
+					{
+	                    required: true
+	                },
+					"billing-city":
+					{
+	                    required: true
+	                },
+					"billing-country":
+					{
+	                    required: true
+	                },
+					"billing-state":
+					{
+	                    required: true
+	                },
+	                "email":
+	                {
+	                    required: true,
+	                    email: true
+	                },
+					"creditCardNumber":
+	                {
+	                    required: true,
+	                    rangelength: [12, 16]
+	                },
+					"card-cvv2":
+	                {
+	                    required: true,
+	                    rangelength: [3, 4]
+	                },
+	            },
+	                                
+	            // Do not change code below
+	            errorPlacement: function(error, element)
+	            {
+	                error.insertAfter(element.parent());
+	            }
+	        });
+        }
+
+    };
+}();
