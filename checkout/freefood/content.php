16e146c9df4c1fbe88e047b3f626def8e9ffc12a
huskilla <chris.robison@4patriots.com>
2016-03-23 22:40:42 -0500

    FFP-1274 Content Update

--- checkout/freefood/content.php
+++ checkout/freefood/content.php
@@ -15,23 +15,24 @@ $submitButtonSource = "/assets/images/buttons/btn-orange-click-continue-01.png";
 		<div class="row">
 			<div class="col-sm-6 col-sm-push-6 col-md-7 col-md-push-5 nopadding">
 				<div style="padding-top: 0px;">
-					<div class="row">
+					<div style="padding: 10px" class="row">
 						<div class="col-lg-12">
-							<h2 class="red21 text-center nomargin"><strong>WARNING: 73-Hour Survival Kits Are Almost Gone...</strong></h2>
+							<h2 style="font-size: 19px" class="red21 text-center nomargin"><strong>WARNING: 73-Hour Survival Kits Are Almost Gone...</strong></h2>
 						</div>
 						<div class="col-lg-12 text-center center-block hidden-xs">
 							<div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><iframe src="//fast.wistia.net/embed/iframe/f1958k8cul?videoFoam=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="100%" height="100%"></iframe></div></div>
 						</div>
 						<div class="col-lg-12 margin-b-10">
-							<p>All Food4Patriots survival food is made in the USA from the finest ingredients and is rated for 25 years of storage. And it’s yours for just $27 (plus the cost of postage) while supplies last!</p>
+							<p>This isn&rsquo;t ordinary food... it&rsquo;s delicious, nutritious, &ldquo;super survival food&rdquo; that could save your life in a disaster!</p>
+							<p>&nbsp;All Food4Patriots survival food is made in the USA from the finest ingredients and is rated for 25 years of storage. And it&rsquo;s yours<strong> FREE</strong> (just cover postage) while supplies last!&nbsp;</p>
 							<p>Get 16 servings of family-favorite dishes: Liberty Bell Potato Cheddar Soup, Blue Ribbon Creamy Chicken Rice, Travelers Stew, and Granny's Homestyle Potato Soup. Here&rsquo;s exactly what you&rsquo;ll get:</p>
 							<ul>
-								<li>Instantly protects you and your family from going hungry in a disaster</li>
+								<li>Instantly protects you and your family from going hungry</li>
 								<li>Simple to prepare: just add boiling water, simmer, and serve</li>
-								<li>Space-age Mylar pouches keep out air, moisture and light to keep your food fresh and delicious</li>
+								<li>Space-age Mylar pouches keep out air, moisture and light to keep your food fresh and delicious for up to 25 years</li>
 								<li>Rushed to your door via USPS First Class Mail</li>
 							</ul>
-							<p>For just $27.00 plus shipping,while supplies last (limit one per household), you can get 72-hours worth of food preparedness!</p>
+							<p><strong>Regularly priced at $27.00 plus shipping, today it&rsquo;s <strong class="darkRed">FREE</strong> while supplies last </strong>(limit one per household, just cover postage).</p>
 						</div>
 						<div class="col-lg-12 text-center hidden-xs">
 							<img src="/assets/images/buttons/btn-order-now-green-left-01.png" alt="Frank" class="img-responsive center-block">
