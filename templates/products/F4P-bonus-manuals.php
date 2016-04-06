6bf4cdeac2f3102510fd7d4395d93ee219300991
caseywittner <casey.wittner@4patriots.com>
2016-04-06 10:39:17 -0500

    FFP-1303 Remove the "right mouse click" text and replace link w/ a link that provides a direct download

--- templates/products/F4P-bonus-manuals.php
+++ templates/products/F4P-bonus-manuals.php
@@ -1,21 +1,22 @@
+<?php $suppressLoadingDiv = true; ?>
 <div class="bonus-materials">
 	<h3>Food4Patriots Bonus Manuals</h3>
-		<p>Here are all your Food4Patriots bonus manuals. </p>
+	<p>Here are all your Food4Patriots bonus manuals. </p>
 	<div>
 		<div class="row">
-			<div class="col-sm-12 col-md-2 text-center"><a target="_blank" href="/templates/products/bonusDownloads.php?v=10items"><img class="img-responsive center-block" src="/media/images/f4p/f4p-bonus-top-10-sold-out-86x99.png" alt="" /></a></div>
-			<div class="col-sm-12 col-md-4 margin-b-20 right-border"><a target="_blank" href="/templates/products/bonusDownloads.php?v=10items">Top 10 Items Sold Out <br class="hidden-xs">After Crisis</a><br />
+			<div class="col-sm-12 col-md-2 text-center"><a href="/templates/products/bonusDownloads.php?v=10items"><img class="img-responsive center-block" src="/media/images/f4p/f4p-bonus-top-10-sold-out-86x99.png" alt="" /></a></div>
+			<div class="col-sm-12 col-md-4 margin-b-20 right-border"><a href="/templates/products/bonusDownloads.php?v=10items">Top 10 Items Sold Out <br class="hidden-xs">After Crisis</a><br />
 				This is manual shows you everything you need to stock to survive a disaster.</div>
-			<div class="col-sm-12 col-md-2 text-center"><a target="_blank" href="/templates/products/bonusDownloads.php?v=groceryBills"><img class="img-responsive center-block" src="/media/images/f4p/f4p-bonus-grocery-bills-half-86x99.png" alt=""></a></div>
-			<div class="col-sm-12 col-md-4 margin-b-20"><a target="_blank" href="/templates/products/bonusDownloads.php?v=groceryBills">How To Cut Your Grocery Bills In Half Guide</a><br />
+			<div class="col-sm-12 col-md-2 text-center"><a href="/templates/products/bonusDownloads.php?v=groceryBills"><img class="img-responsive center-block" src="/media/images/f4p/f4p-bonus-grocery-bills-half-86x99.png" alt=""></a></div>
+			<div class="col-sm-12 col-md-4 margin-b-20"><a href="/templates/products/bonusDownloads.php?v=groceryBills">How To Cut Your Grocery Bills In Half Guide</a><br />
 				Here's the step-by-step manual showing you how to save on your grocery bill.</div>
 		</div>
 		<div class="row">
-			<div class="col-sm-12 col-md-2 text-center"><a target="_blank" href="/templates/products/bonusDownloads.php?v=gardenGuide"><img class="img-responsive center-block" src="/media/images/f4p/f4p-bonus-survival-garden-guide-86x99.png" alt="" /></a></div>
-			<div class="col-sm-12 col-md-4 margin-b-20 right-border"><a target="_blank" href="/templates/products/bonusDownloads.php?v=gardenGuide">The Survival Garden Guide</a><br />
+			<div class="col-sm-12 col-md-2 text-center"><a href="/templates/products/bonusDownloads.php?v=gardenGuide"><img class="img-responsive center-block" src="/media/images/f4p/f4p-bonus-survival-garden-guide-86x99.png" alt="" /></a></div>
+			<div class="col-sm-12 col-md-4 margin-b-20 right-border"><a href="/templates/products/bonusDownloads.php?v=gardenGuide">The Survival Garden Guide</a><br />
 				This is the manual showing you the critical steps necessary to plant and grow your survival garden.</div>
-			<div class="col-sm-12 col-md-2 text-center"><a target="_blank" href="/templates/products/bonusDownloads.php?v=waterGuide"><img class="img-responsive center-block" src="/media/images/f4p/f4p-bonus-water-survival-guide-86x99.png" alt=""></a></div>
-			<div class="col-sm-12 col-md-4 margin-b-20"><a target="_blank" href="/templates/products/bonusDownloads.php?v=waterGuide">The Water Survival Guide</a><br />
+			<div class="col-sm-12 col-md-2 text-center"><a href="/templates/products/bonusDownloads.php?v=waterGuide"><img class="img-responsive center-block" src="/media/images/f4p/f4p-bonus-water-survival-guide-86x99.png" alt=""></a></div>
+			<div class="col-sm-12 col-md-4 margin-b-20"><a href="/templates/products/bonusDownloads.php?v=waterGuide">The Water Survival Guide</a><br />
 				This is the manual showing you the critical steps necessary to survive a water crisis.</div>
 		</div>
 	</div>
