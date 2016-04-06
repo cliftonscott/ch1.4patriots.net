4c1dfe81c34baff421256e64f3871f4bcf32ebf3
Michael Belcher <mbelcher@Michaels-Work-Macbook.local>
2016-04-06 10:39:14 -0500

    FFP-1338 Coffee6000 OTO 1-year page

--- checkout/coffee600/oto/f4p-1year-kit.php
+++ checkout/coffee600/oto/f4p-1year-kit.php
@@ -171,7 +171,7 @@
 		<div class="container oto-width">
 
 
-			<h2 class="darkRed text-center">You Are 100% Protected By My Outrageous Double Guarantee.</h2>
+			<h2 class="darkRed text-center">You Are 100% Protected By My Outrageous Double Guarantee</h2>
 			<div class="outLineBoxDarkBlue">
 				<p><img src="/media/images/misc/seal-guarantee-satisfaction.jpg" alt="Guarantee #1" class="pull-left img-responsive media margin-t-20">
 				<h3>Guarantee #1:</h3> This is a 100% money back guarantee. No questions asked. If for any reason you&rsquo;re not
@@ -198,17 +198,13 @@
 			<h2 class="darkRed title-max-600 center-block text-center"><?php echo $firstName;?>, Get On The ‘Fast Track’ - Claim Your 1-Year
 				Food4Patriots Kit For $1000.00 Off Right Now!</h2>
 
-			<p>Now I understand that the 1-year kit is the right choice for folks who are looking for the ultimate in food security. This kit plus all the bonuses is valued at over $3,000, but because you&rsquo;ve already taken the first step, and because I appreciate you putting your trust in us by being a customer, you can get your 1-year kit today for just $1,997.</p>
+			<p>Now I understand that the 1-year kit is the right choice for folks who are looking for the ultimate in food security. This kit plus all the bonuses is valued at over $2,900, but because you&rsquo;ve already taken the first step, and because I appreciate you putting your trust in us by being a customer, you can get your 1-year kit today for just $1,997.</p>
 
 			<p>You get a year&rsquo;s worth of delicious survival food for $1.11 per serving!</p>
 
-			<p>Plus I'll throw in 31 FREE bonus gifts worth $700+ -- including 4 of the super-popular Survival Spring Personal Water Filters and over
-				22,000+ heirloom survival seeds -- just to make this a "no-brainer" for you!</p>
+			<p>Plus I'll throw in 31 FREE bonus gifts worth $700+ -- including 4 of the super-popular Survival Spring Personal Water Filters and over 22,000+ heirloom survival seeds -- just to make this a "no-brainer" for you!</p>
 
-			<p>I was only able to secure a limited quantity of these 1-year kit and it&rsquo;s been one of our most frequent requests,
-				so I don&rsquo;t know how long I&rsquo;m going to have them available. To make sure that you don&rsquo;t miss out on getting yours,
-				go ahead and click the big orange <strong>&ldquo;Click To Accept&rdquo;</strong> button below to add the 1-year kit to your order
-				today!</p>
+			<p>I was only able to secure a limited quantity of these 1-year kit and it&rsquo;s been one of our most frequent requests, so I don&rsquo;t know how long I&rsquo;m going to have them available. To make sure that you don&rsquo;t miss out on getting yours, go ahead and click the big orange <strong>&ldquo;Click To Accept&rdquo;</strong> button below to add the 1-year kit to your order today!</p>
 
 			<p>The 1-year kit will help secure your stockpile faster and protect you and your family from whatever crisis may come.
 				You&rsquo;ll be on the &ldquo;fast track&rdquo; to securing the ultimate food stockpile.</p>
@@ -218,7 +214,9 @@
 			<div>
 				<!--	<div><img class="img-responsive center-block" src="/media/images/f4p/f4p-1-year-kit-01.jpg"  alt="Food4Patriots 1-Year Kit"/></div>-->
 				<img class="img-responsive center-block small" src="/media/images/f4p/f4p-1year-totes-bonuses-badges-700x561.jpg"  alt="Food4Patriots 1-Year Kit"/>
-				<div><img class="img-responsive center-block" src="/media/images/f4p/f4p-1year-value-chart-01.jpg" alt="Value Chart"/></div>
+				<div class="col-md-12">
+					<?php include("f4p-1year-value-chart.html");?>
+				</div>
 				<div class="text-center"><h2 id="save" class="darkRed">Act Today And Save Over $1000</h2></div>
 				<a id="accept"></a>
 				<?php
