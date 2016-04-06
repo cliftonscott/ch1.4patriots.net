49e7204dfc7aed6e42fe5420b42186fa780bc82b
Michael Belcher <mbelcher@Michaels-Work-Macbook.local>
2016-04-06 10:32:40 -0500

    FFP-1363 Coffee OTO 1-year page

--- templates/products/offers/f4p-1year-kit.php
+++ templates/products/offers/f4p-1year-kit.php
@@ -173,7 +173,7 @@
 
 		<h2 class="darkRed title-max-600 center-block text-center"><?php echo $view->customer->firstName;;?>, Get On The ‘Fast Track’ - Claim Your 1-Year Food4Patriots Kit For $1000.00 Off Right Now!</h2>
 
-		<p>Now I understand that the 1-Year Food4Patriots kit is the right choice for folks who are looking for the ultimate in food security. This kit plus all the bonuses is valued at over $3,000, but because you&rsquo;ve already taken the 1st step, and because I appreciate you putting your trust in us by being a customer, you can get your 1-Year Food4Patriots kit today for just $1,997.</p>
+		<p>Now I understand that the 1-Year Food4Patriots kit is the right choice for folks who are looking for the ultimate in food security. This kit plus all the bonuses is valued at over $2,900, but because you&rsquo;ve already taken the 1st step, and because I appreciate you putting your trust in us by being a customer, you can get your 1-Year Food4Patriots kit today for just $1,997.</p>
 
 		<p>You get a year&rsquo;s worth of delicious survival food for $1.11 per serving!</p>
 
@@ -184,13 +184,13 @@
 		<p>The 1-Year Food4Patriots kit will help secure your stockpile faster and protect you and your family from whatever crisis may come. You&rsquo;ll be on the &ldquo;fast track&rdquo; to securing the ultimate food stockpile.</p>
 
 		<p><?php echo $firstName;?>, this is your last chance for this special 1-time discount, so you need to act now. To get your 1-Year Food4Patriots kit at $1000.00 off, click the big orange <strong>&ldquo;Click To Accept&rdquo;</strong> button below.</p>
-
 		<div>
 			<!--				<div><img class="img-responsive center-block" src="/media/images/f4p/f4p-1-year-kit-01.jpg"  alt="Food4Patriots 1-Year Kit"/></div>-->
 			<img class="img-responsive center-block small" src="/media/images/f4p/f4p-1year-totes-bonuses-badges-700x561.jpg"  alt="Food4Patriots 1-Year Kit"/>
-			<div><img class="img-responsive center-block" src="/media/images/f4p/f4p-1year-value-chart-01.jpg" alt="Value Chart"/></div>
+			<div class="col-md-12">
+				<?php include("f4p-1year-value-chart.html");?>
+			</div>
 			<div class="text-center"><h2 id="save" class="darkRed">Act Today And Save Over $1000</h2></div>
-
 			<a id="accept"></a>
 			<?php
 				if($isUpgrade) {
